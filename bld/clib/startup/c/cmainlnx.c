7d906efc99584cb981d2942d8d4078286f10ab49
Jiri Malak <malak.jiri@gmail.com>
2012-08-29 15:33:26 +0200

    add forgoten change missing from old change 37206

--- bld/clib/startup/c/cmainlnx.c
+++ bld/clib/startup/c/cmainlnx.c
@@ -58,7 +58,7 @@ void __cdecl _LinuxMain( int argc, char **argv, char **arge )
     _STACKLOW           = _curbrk;
     _argc               = argc;
     _argv               = argv;
-    environ             = arge;
+    _Envptr             = arge;
     _LpPgmName          = argv[0];
 
     __InitRtns( INIT_PRIORITY_THREAD );
