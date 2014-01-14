586bcc27f919f72a4eb0e457048ac74933afbc4f
Jiri Malak <malak.jiri@gmail.com>
2014-01-14 09:35:45 +0100

    add more tests for near/far/huge mismatch

--- bld/plustest/regress/diagnose/source/diag0081.c
+++ bld/plustest/regress/diagnose/source/diag0081.c
@@ -22,6 +22,9 @@ struct Z
 };
 
 
+Y __huge yh;
+Y __huge yh3[3];
+
 Y __far yf;
 Y __far yf3[3];
 
@@ -34,20 +37,30 @@ Z __huge *HugeMem;
 Z __far FarArr[3];
 Z __far *FarMem;
 
-void fun( Y __far & y_far, Y __near & y_near )
+void fun( Y __huge & y_huge, Y __far & y_far, Y __near & y_near )
 {
-    Y zn( y_far  );
+    Y zn( y_huge  );
+    Y zn2( y_far  );
     Y zf( y_near );
+    Y zf2( y_huge );
+    Y zh( y_near );
+    Y zh2( y_far );
     Z localvar;
     int i = 1;
 
+    zn = y_huge;
     zn = y_far;
     zn = y_near;
     y_far = y_near;
+    y_far = y_huge;
+    y_huge = y_near;
+    y_huge = y_far;
 
+    delete &y_huge;
     delete &y_far;
     delete &y_near;
 
+    y_huge.foo();
     y_far.foo();
     y_near.foo();
 
