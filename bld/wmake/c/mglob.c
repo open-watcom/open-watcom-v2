05003589282dc3a7a2206342d30df9dce5866e3f
Jiri Malak <malak.jiri@gmail.com>
2014-05-27 02:12:20 +0200

    enable bootstrap by Intel compiler on Windows

--- bld/wmake/c/mglob.c
+++ bld/wmake/c/mglob.c
@@ -70,8 +70,12 @@ const char FAR *BuiltIns = {
 #ifdef DLLS_IMPLEMENTED
     "__LOADDLL__=\n"
 #endif
-#if defined( __WATCOMC__ ) && defined( BOOTSTRAP )
+#if defined( BOOTSTRAP )
+#if defined( __WATCOMC__ )
     "__WATCOM__=\n"
+#elif defined( __INTEL_COMPILER )
+    "__INTEL__=\n"
+#endif
 #endif
 
 #if defined( __DOS__ )
