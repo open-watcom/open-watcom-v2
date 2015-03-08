32687a71dffbb3aecc1276a732e674773bc23e7c
Jiri Malak <malak.jiri@gmail.com>
2015-03-08 02:18:05 +0100

    fix problem with command line length

--- bld/wgml/test/test.cmd
+++ bld/wgml/test/test.cmd
@@ -29,43 +29,53 @@ rem NT/XP? or OS/2 or eCS?
 
 :NOOS2
    if [%Systemroot%] == [] goto unknown
-   set wgml_dir=nt386
-   set wg4_dir=dos
+   set WGML_DIR=nt386
+   set WG4_DIR=dos
    goto bld1
 
 :NOWIN
 :CONT3
-   set wgml_dir=os2386
-   set wg4_dir=os2
+   set WGML_DIR=os2386
+   set WG4_DIR=os2
 
 :BLD1
 
-set options=set date 'April 28, 2009' set time '21:11:02' wscript dev ps pass 1 verbose warn stat incl index
-rem set options=wscript dev ps pass 1 verbose warn stat incl index
+echo ( set date 'April 28, 2009' >wgml.opt
+echo ( set time '21:11:02' >>wgml.opt
+echo ( wscript >>wgml.opt
+echo ( dev ps >>wgml.opt
+echo ( pass 1 >>wgml.opt
+echo ( verbose >>wgml.opt
+echo ( warning >>wgml.opt
+echo ( statistics >>wgml.opt
+echo ( inclist >>wgml.opt
+echo ( index >>wgml.opt
 
 set GMLLIB=%OWDOCSDIR%\gml\syslib;%OWDOCSDIR%\doc\whelp;%OWDOCSDIR%\doc\hlp
 set GMLINC=%OWDOCSDIR%\doc\devguide;%OWDOCSDIR%\doc\gml;%OWDOCSDIR%\doc\whelp;%OWDOCSDIR%\doc\hlp
 
-If [%OWDOSBOX%] == [] %OWDOCSDIR%\gml\%wg4_dir%\wgml %1.gml ( %options% out %1.ops %2 %3 %4 %5 %6 %7 %8 %9 >%1.old
-If [%OWDOSBOX%] == [] goto pause1
+if [%OWDOSBOX%] == [] goto NATIVE1
 REM create DOSBOX batch script for run wgml on 64-bit Windows under DOSBOX
-   echo d: > wgml.bat
-   echo set GMLINC=c:\doc\devguide;c:\doc\gml;c:\doc\whelp;c:\doc\hlp >> wgml.bat
-   echo set GMLLIB=c:\gml\syslib;c:\doc\whelp;c:\doc\hlp;.\testlib >> wgml.bat
-   echo c:\gml\dos\wgml.exe %1.gml ( %options% out %1.ops %2 %3 %4 %5 %6 %7 %8 %9 ^>%1.old >> wgml.bat
-   echo exit >> wgml.bat
-   %OWDOSBOX% -noautoexec -c "mount c %OWDOCSDIR%" -c "mount d ." -c "mount e %OWSRCDIR%" -c "d:wgml.bat" -noconsole
+    echo d: > wgml.bat
+    echo set GMLINC=c:\doc\devguide;c:\doc\gml;c:\doc\whelp;c:\doc\hlp >> wgml.bat
+    echo set GMLLIB=c:\gml\syslib;c:\doc\whelp;c:\doc\hlp;.\testlib >> wgml.bat
+    echo c:\gml\dos\wgml.exe %1.gml ( file wgml.opt out %1.ops %2 %3 %4 %5 %6 %7 %8 %9 ^>%1.old >> wgml.bat
+    echo exit >> wgml.bat
+    %OWDOSBOX% -noautoexec -c "mount c %OWDOCSDIR%" -c "mount d ." -c "mount e %OWSRCDIR%" -c "d:wgml.bat" -noconsole
+    goto PAUSE1
+:NATIVE1   
+    %OWDOCSDIR%\gml\%WG4_DIR%\wgml %1.gml ( file wgml.opt out %1.ops %2 %3 %4 %5 %6 %7 %8 %9 >%1.old
 :PAUSE1
-   pause
-   %OWSRCDIR%\wgml\%wgml_dir%\wgml %1.gml    ( %options% out %1.nps %2 %3 %4 %5 %6 %7 %8 %9 >%1.ntr
-   %OWSRCDIR%\wgml\%wgml_dir%\wgml %1.gml -r ( %options% out %1.nps %2 %3 %4 %5 %6 %7 %8 %9 >%1.new
-   pause
-   wdw %OWSRCDIR%\wgml\%wgml_dir%\wgml %1.gml -r ( %options% out %1.nps %2 %3 %4 %5 %6 %7 %8 %9
-   goto eof
+    pause
+    %OWSRCDIR%\wgml\%WGML_DIR%\wgml %1.gml    ( file wgml.opt out %1.nps %2 %3 %4 %5 %6 %7 %8 %9 >%1.ntr
+    %OWSRCDIR%\wgml\%WGML_DIR%\wgml %1.gml -r ( file wgml.opt out %1.nps %2 %3 %4 %5 %6 %7 %8 %9 >%1.new
+    pause
+    wdw %OWSRCDIR%\wgml\%WGML_DIR%\wgml %1.gml -r ( file wgml.opt out %1.nps %2 %3 %4 %5 %6 %7 %8 %9
+    goto eof
 
 :PARMMISS
-   echo Parameter missing
-   echo usage: test gmlfile
+    echo Parameter missing
+    echo usage: test gmlfile
 
 :EOF
-   endlocal
+    endlocal
