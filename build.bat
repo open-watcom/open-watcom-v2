f9a7bc34a7650882127a412fc12d5b9869586c4f
Jiri Malak <malak.jiri@gmail.com>
2015-05-27 22:28:41 +0200

    correct build.bat for DOS

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
 REM Script to build the Open Watcom tools
 set OWBUILDER_BOOTX_OUTPUT=%OWROOT%\bootx.log
+if not '%OS%' == 'Windows_NT' goto skip_errout
+set OWBUILDER_REDIR_ERROUT=2^>^&1
+:skip_errout
 if exist %OWBUILDER_BOOTX_OUTPUT% del %OWBUILDER_BOOTX_OUTPUT%
 cd %OWSRCDIR%\wmake
 if not exist %OWOBJDIR% mkdir %OWOBJDIR%
@@ -8,22 +11,22 @@ cd %OWOBJDIR%
 if exist %OWBINDIR%\wmake.exe del %OWBINDIR%\wmake.exe
 if '%OWTOOLS%' == 'VISUALC' goto visualc_tools
 if '%OWTOOLS%' == 'INTEL' goto intel_tools
-wmake -f ..\wmake clean >>%OWBUILDER_BOOTX_OUTPUT% 2>&1
-wmake -f ..\wmake >>%OWBUILDER_BOOTX_OUTPUT% 2>&1
+wmake -f ..\wmake clean >>%OWBUILDER_BOOTX_OUTPUT% %OWBUILDER_REDIR_ERROUT%
+wmake -f ..\wmake >>%OWBUILDER_BOOTX_OUTPUT% %OWBUILDER_REDIR_ERROUT%
 if errorlevel == 1 goto error_exit
 goto make_builder
 :intel_tools
 :visualc_tools
-nmake -f ..\nmake clean >>%OWBUILDER_BOOTX_OUTPUT% 2>&1
-nmake -f ..\nmake >>%OWBUILDER_BOOTX_OUTPUT% 2>&1
+nmake -f ..\nmake clean >>%OWBUILDER_BOOTX_OUTPUT% %OWBUILDER_REDIR_ERROUT%
+nmake -f ..\nmake >>%OWBUILDER_BOOTX_OUTPUT% %OWBUILDER_REDIR_ERROUT%
 if errorlevel == 1 goto error_exit
 :make_builder
 cd %OWSRCDIR%\builder
 if not exist %OWOBJDIR% mkdir %OWOBJDIR%
 cd %OWOBJDIR%
 if exist %OWBINDIR%\builder.exe del %OWBINDIR%\builder.exe
-%OWBINDIR%\wmake -f ..\binmake clean >>%OWBUILDER_BOOTX_OUTPUT% 2>&1
-%OWBINDIR%\wmake -f ..\binmake bootstrap=1 builder.exe >>%OWBUILDER_BOOTX_OUTPUT% 2>&1
+%OWBINDIR%\wmake -f ..\binmake clean >>%OWBUILDER_BOOTX_OUTPUT% %OWBUILDER_REDIR_ERROUT%
+%OWBINDIR%\wmake -f ..\binmake bootstrap=1 builder.exe >>%OWBUILDER_BOOTX_OUTPUT% %OWBUILDER_REDIR_ERROUT%
 if errorlevel == 1 goto error_exit
 cd %OWSRCDIR%
 builder boot
