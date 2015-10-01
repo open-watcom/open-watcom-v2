fe624c194ce129284779a01639c032aeadccf2c9
Jiri Malak <malak.jiri@gmail.com>
2015-10-01 06:57:28 +0200

    move substr.c to Fortran library tree

--- bld/f77/f77lib/c/substr.c
+++ bld/f77/f77lib/c/substr.c
@@ -29,6 +29,7 @@
 ****************************************************************************/
 
 #include "ftnstd.h"
+#include "ftextfun.h"
 
 bool    DoSubstring( intstar4 first, intstar4 last, int len ) {
 // Do substring operation for EQUIVALENCE or DATA statement and
