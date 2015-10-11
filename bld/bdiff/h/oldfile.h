6b2b3fca5fa46bef73d815c0962a03ea47722a00
Jiri Malak <malak.jiri@gmail.com>
2015-10-12 01:12:58 +0200

    add new header files to add global prototypes instead local declaration

---
+++ bld/bdiff/h/oldfile.h
@@ -0,0 +1,44 @@
+/****************************************************************************
+*
+*                            Open Watcom Project
+*
+*    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
+*
+*  ========================================================================
+*
+*    This file contains Original Code and/or Modifications of Original
+*    Code as defined in and that are subject to the Sybase Open Watcom
+*    Public License version 1.0 (the 'License'). You may not use this file
+*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
+*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
+*    provided with the Original Code and Modifications, and is also
+*    available at www.sybase.com/developer/opensource.
+*
+*    The Original Code and all software distributed under the License are
+*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
+*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
+*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
+*    NON-INFRINGEMENT. Please see the License for the specific language
+*    governing rights and limitations under the License.
+*
+*  ========================================================================
+*
+* Description:  WHEN YOU FIGURE OUT WHAT THIS FILE DOES, PLEASE
+*               DESCRIBE IT HERE!
+*
+****************************************************************************/
+
+
+#if defined( BDIFF )
+extern byte             *OldFile;
+#else
+extern MY_FILE          OldFile;
+#endif
+
+extern char             *SetOld( char *name );
+extern char             *FindOld( char *name );
+extern foff             CheckSumOld( foff new_size );
+extern PATCH_RET_CODE   OpenOld( foff len, int prompt, foff new_size, foff new_sum );
+extern byte             InOld( foff offset );
+extern PATCH_RET_CODE   CloseOld( int havenew, int dobackup );
