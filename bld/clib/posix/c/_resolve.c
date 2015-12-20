09e69f1fcf535ea795deb35af75cf4a13a03aa91
Jiri Malak <malak.jiri@gmail.com>
2015-12-20 15:46:58 +0100

    correct porototypes to use char pointer type where appropriate a little optimize code

--- bld/clib/posix/c/_resolve.c
+++ bld/clib/posix/c/_resolve.c
@@ -87,7 +87,7 @@ struct __dns_response_data
 
 struct __dns_resource_record
 {
-    unsigned char               *name;
+    char                        *name;
     struct __dns_response_data  *resource;
     unsigned char               *rdata;
 };
@@ -95,100 +95,76 @@ struct __dns_resource_record
 
 struct __dns_query
 {
-    unsigned char               *name;
+    char                        *name;
     struct __dns_question       *ques;
 };
 
-static int _from_dns_name_format( unsigned char *dest, const unsigned char *reader, 
-                                  const unsigned char *buffer )
+static int _from_dns_name_format( char *dest, const char *reader, const char *buffer )
 {
     int     p, jumped, offset;
-    int     i , j;
+    int     c;
     int     count;
 
     count = 1;
-    p = 0;
     jumped = 0;
 
-    dest[0]='\0';
+    /* read the names in 3www6google3com format and convert them to dot name format */
+    p = 0;
+    while( (c = *(unsigned char *)reader) != '\0' ) {
 
-    /* read the names in 3www6google3com format */
-    while( *reader != '\0' ) {
+        if( c >= 0xC0 ) {
 
-        if( *reader >= 192 ) {
-
-            /* 49152 = 11000000 00000000 ;) */
-            offset = *reader * 256 + *(reader + 1) - 49152; 
-
-            reader = buffer + offset - 1;
-            jumped = 1; /* Stop counting... */
+            offset = ( c - 0xC0 ) * 256 + *(unsigned char *)(reader + 1); 
+            reader = buffer + offset;
+            /* If we havent jumped to another location then we can count up */
+            if( jumped == 0 ) {
+                count++;
+                jumped = 1; /* Stop counting... */
+            }
 
         } else {
 
-            dest[p++]=*reader;
-
-        }
-
-        reader = reader+1;
-
-        /* If we havent jumped to another location then we can count up */
-        if( jumped == 0 ) {
-            count++;
+            /* If we havent jumped to another location then we can count up */
+            if( jumped == 0 )
+                count += c + 1;
+            /* copy name */
+            while( c-- > 0 ) {
+                dest[p++] = *reader++;
+            }
+            /* add dot after name */
+            dest[p++] = '.';
         }
     }
-
-    dest[p] = '\0';
-
-    if( jumped == 1 )
-        count++; 
-
-    for( i = 0; i < (int)strlen( (char *)dest ); i++ ) {
-
-        p = dest[i];
-        for( j = 0; j < (int)p; j++ ) {
-            dest[i] = dest[i + 1];
-            i++;
-        }
-
-        dest[i] = '.';
-
-    }
-
-    dest[i - 1] = '\0'; 
+    if( p > 0 )     /* remove last dot */
+        --p;
+    dest[p] = '\0'; /* terminate string */
 
     return( count );
 }
 
-static int _to_dns_name_format( char *dest, const char *src )
+static size_t _to_dns_name_format( char *dest, const char *src )
 {
     const char  *dot;
     const char  *segment;
-    int         i, j, p;
-
-    *dest = '\0';
+    size_t      i, j, p;
 
     p = 0;
     dot = src;
     segment = src;
     do {
-
         dot = strchr( segment, '.' );
         if( dot == NULL ) {
             i = strlen( segment );
         } else {
             i = dot - segment;
         }
-
         dest[p++] = i;
         for( j = 0; j < i; j++ )
             dest[p++] = segment[j];
-
         if( dot != NULL ) {
             segment = dot + 1;
         }
-
     } while( dot != NULL && *segment != '\0' );
-
     dest[p++] = '\0';
     return( p );
 }
@@ -215,9 +191,9 @@ static char **_add_string_to_list( char **addr_list, char *text )
 
 static char **_add_address_to_list( char **addr_list, struct in_addr addr )
 {
-    char    *allocated_address;
+    char *allocated_address;
 
-    allocated_address = (char *)malloc( 64 * sizeof( char ) );
+    allocated_address = malloc( 64 );
     if( allocated_address != NULL )
         strcpy( allocated_address, inet_ntoa( addr ) );
 
@@ -226,13 +202,14 @@ static char **_add_address_to_list( char **addr_list, struct in_addr addr )
 
 int _dns_query( const char *name, int query_type, in_addr_t dnsaddr, struct hostent *res )
 {
-    unsigned char   *buf, *query_name, *reader;
+    char            *buf, *query_name, *reader;
     int             query_socket;
     int             i, j;
     int             name_length;
     int             result;
     size_t          query_size;
     int             ret;
+    int             n_answers;
 
     struct __dns_resource_record *answers; //the replies from the DNS server
     struct sockaddr_in      dest;
@@ -249,7 +226,7 @@ int _dns_query( const char *name, int query_type, in_addr_t dnsaddr, struct host
     if( name == NULL )
         return( -EINVAL );
 
-    buf = (unsigned char *)malloc( DNS_BUFFER_SIZE * sizeof( unsigned char ) );
+    buf = malloc( DNS_BUFFER_SIZE );
     if( buf == NULL )
         return( -ENOMEM );
 
@@ -263,15 +240,15 @@ int _dns_query( const char *name, int query_type, in_addr_t dnsaddr, struct host
 
     //Set the DNS structure to standard queries
     dns = (struct __dns_header *)buf;
-    memset( dns, 0, sizeof( struct __dns_header ) );
+    memset( dns, 0, sizeof( *dns ) );
 
-    dns->id = (uint16_t)htons( getpid() );
+    dns->id = htons( getpid() );
     dns->recursion = 1;      /* request recursion */
     dns->n_questions = htons( 1 );
-    query_size = sizeof( struct __dns_header );
+    query_size = sizeof( *dns );
 
-    query_name = (unsigned char*)&buf[(int)query_size];
-    query_size += _to_dns_name_format( (char *)query_name, (const char *)name );
+    query_name = &buf[query_size];
+    query_size += _to_dns_name_format( query_name, name );
 
     qinfo = (struct __dns_question *)&buf[query_size];
 
@@ -279,14 +256,9 @@ int _dns_query( const char *name, int query_type, in_addr_t dnsaddr, struct host
     qinfo->qclass = htons( 1 ); 
     qinfo->qtype = htons( query_type ); 
     qinfo->qclass = htons( 1 ); 
-    query_size += sizeof( struct __dns_question );
+    query_size += sizeof( *qinfo );
 
-    result = sendto( query_socket,
-                    (char *)buf,
-                    query_size,
-                    0,
-                    (struct sockaddr *)&dest,
-                    sizeof( dest ) );
+    result = sendto( query_socket, buf, query_size, 0, (struct sockaddr *)&dest, sizeof( dest ) );
 
     if( result < 0 ) {
         ret = -ENOMSG;
@@ -294,12 +266,7 @@ int _dns_query( const char *name, int query_type, in_addr_t dnsaddr, struct host
     }
 
     i = sizeof( dest );
-    result = recvfrom( query_socket,
-                      (char *)buf,
-                      DNS_BUFFER_SIZE,
-                      0,
-                      (struct sockaddr *)&dest,
-                      (socklen_t *)&i );
+    result = recvfrom( query_socket, buf, DNS_BUFFER_SIZE, 0, (struct sockaddr *)&dest, (socklen_t *)&i );
     if( result < 0 ) {
         ret = -ENOMSG;
         goto dns_cleanup;
@@ -308,22 +275,23 @@ int _dns_query( const char *name, int query_type, in_addr_t dnsaddr, struct host
     dns = (struct __dns_header *)buf;
     reader = &buf[query_size];
 
-    if( ntohs( dns->n_answers ) == 0 ) {
+    n_answers = ntohs( dns->n_answers );
+    if( n_answers == 0 ) {
         ret = -ENOENT;
         goto dns_cleanup;
     }
 
-    answers = (struct __dns_resource_record *)malloc( ntohs( dns->n_answers ) * sizeof( struct __dns_resource_record ) );
+    answers = malloc( n_answers * sizeof( *answers ) );
     if( answers == NULL )
         goto dns_cleanup;
-    memset( answers, 0, ntohs( dns->n_answers ) * sizeof( struct __dns_resource_record ) );
+    memset( answers, 0, n_answers * sizeof( *answers ) );
 
     /* If we've reached this point, we have answers (or none, but
      * a valid lack of answers), so begin filling in the result
      * structure appropriately
      */
     if( query_type == DNSQ_TYPE_A ) {
-        res->h_name = (char *)malloc( ( strlen( name ) + 1 ) * sizeof( char ) );
+        res->h_name = malloc( strlen( name ) + 1 );
         if( res->h_name != NULL ) {
             strcpy( res->h_name, name );
         }
@@ -340,10 +308,10 @@ int _dns_query( const char *name, int query_type, in_addr_t dnsaddr, struct host
     /* We only support IPv4 right now. */
     res->h_addrtype = AF_INET;
 
-    for( i = 0; i < ntohs( dns->n_answers ); i++ ) {
+    for( i = 0; i < n_answers; i++ ) {
         int     rdata_length;
 
-        answers[i].name = (unsigned char *)malloc( 256 * sizeof( unsigned char ) );
+        answers[i].name = malloc( 256 );
         name_length = _from_dns_name_format( answers[i].name, reader, buf );
         reader += name_length;
 
@@ -352,47 +320,35 @@ int _dns_query( const char *name, int query_type, in_addr_t dnsaddr, struct host
         rdata_length = ntohs( *(uint16_t *)reader );
         reader += sizeof( uint16_t * );
 
-        if( ntohs( answers[i].resource->type ) == DNSQ_TYPE_A ) { /* IPv4 encountered */
+        if( ntohs( answers[i].resource->type ) == DNSQ_TYPE_A) { /* IPv4 encountered */
 
-            answers[i].rdata = (unsigned char *)malloc( ( rdata_length + 1 ) * sizeof( char ) );
+            answers[i].rdata = malloc( rdata_length + 1 );
             if( answers[i].rdata != NULL ) {
                 for( j = 0; j < rdata_length; j++ ) {
                     answers[i].rdata[j] = reader[j];
                 }
+                answers[i].rdata[rdata_length] = '\0';
             }
-
-            answers[i].rdata[rdata_length] = '\0';
-
             reader += rdata_length;
 
             lptr = (struct in_addr *)answers[i].rdata;
 
             res->h_addr_list = _add_address_to_list( res->h_addr_list, *lptr );
             ret = 1;
-
         } else {
-
             /* Answer should just be a string */
-            answers[i].rdata = (unsigned char *)malloc( 256 * sizeof( unsigned char ) );
+            answers[i].rdata = malloc( 256 );
             if( answers[i].rdata != NULL ) {
                 name_length = _from_dns_name_format( answers[i].name, reader, buf );
-
                 if( query_type == ntohs( answers[i].resource->type ) ) {
-
                     if( res->h_name == NULL ) {
-
                         res->h_name = answers[i].name;
                         answers[i].name = NULL;
-
                     } else {
-
                         res->h_aliases = _add_string_to_list( res->h_aliases, answers[i].name );
                         answers[i].name = NULL;
-
                     }
-
                 }
-
                 reader += name_length;
             }
         }
@@ -401,16 +357,15 @@ int _dns_query( const char *name, int query_type, in_addr_t dnsaddr, struct host
 dns_cleanup:
 
     if( answers != NULL && dns != NULL ) {
-        for( i = 0; i < ntohs( dns->n_answers ); i++ ) {
+        for( i = 0; i < n_answers; i++ ) {
             if( answers[i].name != NULL )
                 free( answers[i].name );
-            if( answers[i].rdata != NULL ) {
+            if( answers[i].rdata != NULL) {
                 free( answers[i].rdata );
             }
         }
         free( answers );
     }
-
     free( buf );
 
     return( ret );
