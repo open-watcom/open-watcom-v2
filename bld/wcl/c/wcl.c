open-watcom/open-watcom-v2

commit f76af83c2e9cd10b34e6964ab3c71c60ba4277f0

fix mistake in wcl tool command line processing (non working -mt option)

bld/wcl/c/wcl.c
@@ -822,9 +822,7 @@ static int Parse( const char *cmd )
                     p = CC_Opts + strlen( CC_Opts );
                     *p++ = ' ';
                     *p++ = '-';
-                    len = end - cmd;
-                    memcpy( p, cmd, len );    /* keep original case */
-                    p[len] = '\0';
+                    strcpy( p, Word );      /* keep original case */
                 }
             }
             cmd = end;

commit ac26553c9354ff606657d53d99eeb10bd4a95ef9

fix mistake for -fo option processing introduced by f76af83c2e9cd10b34e6964ab3c71c60ba4277f0 commit

bld/wcl/c/wcl.c
@@ -626,9 +626,9 @@ static int Parse( const char *cmd )
                         p = Word + 2;
                         if( *p == '=' || *p == '#' )
                             ++p;
-                        NormalizeFName( Word, MAX_CMD, p );
+                        NormalizeFName( p, MAX_CMD, p );
                         MemFree( Obj_Name );
-                        Obj_Name = MemStrDup( Word );
+                        Obj_Name = MemStrDup( p );
                         break;
 #if defined( WCLI86 ) || defined( WCL386 )
                     case 'p':           /* floating-point option */