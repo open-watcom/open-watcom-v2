566e235d4777f81098ff60ab84d8ef6183c354c7
Jiri Malak <malak.jiri@gmail.com>
2016-10-20 02:30:34 +0200

    fix 64-bit Windows build

--- bld/clib/h/iomode.h
+++ bld/clib/h/iomode.h
@@ -60,11 +60,11 @@ extern  HANDLE      *__OSHandles;
 extern  unsigned    __NHandles;
 extern  unsigned    __NFiles;              /* maximum # of files we can open */
 
-extern  unsigned    __GetIOMode( int __handle );
-extern  int         __SetIOMode( int __handle, unsigned __value );
-extern  void        __SetIOMode_nogrow( int __handle, unsigned __value );
-extern  void        __ChkTTYIOMode( int __handle );
-extern  int         __set_binary( int __handle );
+extern  unsigned    __GetIOMode( int handle );
+extern  int         __SetIOMode( int handle, unsigned value );
+extern  void        __SetIOMode_nogrow( int handle, unsigned value );
+extern  void        __ChkTTYIOMode( int handle );
+extern  int         __set_binary( int handle );
 
 #endif
 
