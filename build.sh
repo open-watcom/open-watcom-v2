90d613f5cb3b2e3c1c505eb31425677ab4be1b08
Jonathan Campbell <jonathan@castus.tv>
2016-10-29 01:22:16 -0700

    restore build.sh

--- build.sh
+++ build.sh
@@ -22,32 +22,37 @@ output_redirect()
     "$@" >>$OWBUILDER_BOOTX_OUTPUT 2>&1
 }
 
-rm -f $OWBUILDER_BOOTX_OUTPUT
-cd $OWSRCDIR/wmake
-if [ ! -d $OWOBJDIR ]; then mkdir $OWOBJDIR; fi
-cd $OWOBJDIR
-rm -f $OWBINDIR/wmake
+die() {
+    echo "Error: $1"
+    exit 1
+}
+
+rm -f $OWBUILDER_BOOTX_OUTPUT || die "Failed to remove $OWBUILDER_BOOTX_OUTPUT"
+cd $OWSRCDIR/wmake || die "Failed to enter $OWSRCDIR/wmake"
+if [ ! -d $OWOBJDIR ]; then mkdir -p $OWOBJDIR || die "Failed to mkdir $OWOBJDIR"; fi
+cd $OWOBJDIR || doe "Failed to enter $OWOWBDIR"
+rm -f $OWBINDIR/wmake || die "Failed to remove $OWBINDIR/wmake"
 if [ "$OWTOOLS" = "WATCOM" ]; then
-    output_redirect wmake -f ../wmake clean
-    output_redirect wmake -f ../wmake
+    output_redirect wmake -f ../wmake clean || die 'Failed to wmake wmake clean'
+    output_redirect wmake -f ../wmake || die 'Failed to wmake wmake'
 else
     case `uname` in
         FreeBSD)
-            output_redirect make -f ../posmake clean
-            output_redirect make -f ../posmake TARGETDEF=-D__BSD__
+            output_redirect make -f ../posmake clean || die "Failed to make wmake clean"
+            output_redirect make -f ../posmake TARGETDEF=-D__BSD__ || die "Failed to make wmake"
             ;;
         Darwin)
             output_redirect make -f ../posmake clean
             output_redirect make -f ../posmake TARGETDEF=-D__OSX__
             ;;
         Haiku)
-            output_redirect make -f ../posmake clean
-            output_redirect make -f ../posmake TARGETDEF=-D__HAIKU__
+            output_redirect make -f ../posmake clean || die "Failed to make wmake clean"
+            output_redirect make -f ../posmake TARGETDEF=-D__HAIKU__ || die "Failed to make wmake"
             ;;
 #        Linux)
         *)
-            output_redirect make -f ../posmake clean
-            output_redirect make -f ../posmake TARGETDEF=-D__LINUX__
+            output_redirect make -f ../posmake clean || die "Failed to make wmake clean"
+            output_redirect make -f ../posmake TARGETDEF=-D__LINUX__ || die "Failed to make wmake"
             ;;
     esac
 fi
@@ -69,7 +74,16 @@ else
     else
         builder $BUILDER_ARG
         RC=$?
+        if [ $RC -ne 0 ]; then
+            echo "builder bootstrap build error"
+        else
+            builder rel
+            RC=$?
+	fi
     fi
 fi
 cd $OWROOT
+if [ $RC -eq 0 ]; then
+    echo "Now you can run 'cd bld && builder rel' to produce the final binaries"
+fi
 exit $RC
