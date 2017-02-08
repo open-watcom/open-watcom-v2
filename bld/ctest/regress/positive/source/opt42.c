f442d191e8efe5bd6a3684ccab48db43ed66bdf8
Jiri Malak <malak.jiri@gmail.com>
2017-02-08 23:01:25 +0100

    do C compiler tests more portable use keywords instead of macros for modifiers

--- bld/ctest/regress/positive/source/opt42.c
+++ bld/ctest/regress/positive/source/opt42.c
@@ -18,10 +18,10 @@ ALWAYS_PASS
 #error  Must use small model, not flat
 #endif
 
-signed   long long far __based(__segname("FARAWAY")) s_store1;
-signed   long long far __based(__segname("FARAWAY")) s_store2;
-unsigned long long far __based(__segname("FARAWAY")) u_store1;
-unsigned long long far __based(__segname("FARAWAY")) u_store2;
+signed   long long __far __based(__segname("FARAWAY")) s_store1;
+signed   long long __far __based(__segname("FARAWAY")) s_store2;
+unsigned long long __far __based(__segname("FARAWAY")) u_store1;
+unsigned long long __far __based(__segname("FARAWAY")) u_store2;
 
 /*
  * Ugly hack used to prevent access via DS. Do not know how to place data
@@ -35,49 +35,49 @@ void restore_ds(void);
 #pragma aux restore_ds  = "pop  ds";
 
 #define testdata(type, d1, d2, d3, d4)                                           \
-signed   type far __based(__segname("FARAWAY")) s_##type = d1;                   \
-unsigned type far __based(__segname("FARAWAY")) u_##type = d2;                   \
-signed   type far __based(__segname("FARAWAY")) sneg_##type = d3;                \
-unsigned type far __based(__segname("FARAWAY")) ubig_##type = d4;                \
-signed   long long  s_expand_##type(signed   type far *p)  { return *p; }        \
-unsigned long long  u_expand_##type(unsigned type far *p)  { return *p; }        \
-void store_s_s_##type(signed   long long far *p, signed   type n) { *p = n; }    \
-void store_s_u_##type(signed   long long far *p, unsigned type n) { *p = n; }    \
-void store_u_s_##type(unsigned long long far *p, signed   type n) { *p = n; }    \
-void store_u_u_##type(unsigned long long far *p, unsigned type n) { *p = n; }    \
+signed   type __far __based(__segname("FARAWAY")) s_##type = d1;                 \
+unsigned type __far __based(__segname("FARAWAY")) u_##type = d2;                 \
+signed   type __far __based(__segname("FARAWAY")) sneg_##type = d3;              \
+unsigned type __far __based(__segname("FARAWAY")) ubig_##type = d4;              \
+signed   long long  s_expand_##type(signed   type __far *p)  { return *p; }      \
+unsigned long long  u_expand_##type(unsigned type __far *p)  { return *p; }      \
+void store_s_s_##type(signed   long long __far *p, signed   type n) { *p = n; }  \
+void store_s_u_##type(signed   long long __far *p, unsigned type n) { *p = n; }  \
+void store_u_s_##type(unsigned long long __far *p, signed   type n) { *p = n; }  \
+void store_u_u_##type(unsigned long long __far *p, unsigned type n) { *p = n; }  \
 int  verify_expand_##type(void)                                                  \
 {                                                                                \
-        int errors;                                                              \
-        clear_ds();                                                              \
-        errors  = s_expand_##type(&s_##type) != d1;                              \
-        errors += u_expand_##type(&u_##type) != d2;                              \
-        errors += s_expand_##type(&sneg_##type) != d3;                           \
-        errors += u_expand_##type(&ubig_##type) != d4;                           \
-        restore_ds();                                                            \
-        return errors;                                                           \
+    int errors;                                                                  \
+    clear_ds();                                                                  \
+    errors  = s_expand_##type(&s_##type) != d1;                                  \
+    errors += u_expand_##type(&u_##type) != d2;                                  \
+    errors += s_expand_##type(&sneg_##type) != d3;                               \
+    errors += u_expand_##type(&ubig_##type) != d4;                               \
+    restore_ds();                                                                \
+    return errors;                                                               \
 }                                                                                \
 int  verify_store_##type(void)                                                   \
 {                                                                                \
-        int errors = 0;                                                          \
+    int errors = 0;                                                              \
                                                                                  \
-        s_store1 = 0x8877665544332211;                                           \
-        s_store2 = 0x9988776655443322;                                           \
-        u_store1 = 0xFFEEDDCCAABB9988;                                           \
-        u_store2 = 0xEEDDCCAABB998877;                                           \
+    s_store1 = 0x8877665544332211;                                               \
+    s_store2 = 0x9988776655443322;                                               \
+    u_store1 = 0xFFEEDDCCAABB9988;                                               \
+    u_store2 = 0xEEDDCCAABB998877;                                               \
                                                                                  \
-        clear_ds();                                                              \
-        store_s_s_##type(&s_store1, d1);                                         \
-        store_s_u_##type(&s_store2, d2);                                         \
-        store_u_s_##type(&u_store1, d1);                                         \
-        store_u_u_##type(&u_store2, d2);                                         \
-        restore_ds();                                                            \
+    clear_ds();                                                                  \
+    store_s_s_##type(&s_store1, d1);                                             \
+    store_s_u_##type(&s_store2, d2);                                             \
+    store_u_s_##type(&u_store1, d1);                                             \
+    store_u_u_##type(&u_store2, d2);                                             \
+    restore_ds();                                                                \
                                                                                  \
-        if (s_store1 != d1)  errors++;                                          \
-        if (s_store2 != d2)  errors++;                                          \
-        if (u_store1 != d1)  errors++;                                          \
-        if (u_store2 != d2)  errors++;                                          \
+    if (s_store1 != d1)  errors++;                                               \
+    if (s_store2 != d2)  errors++;                                               \
+    if (u_store1 != d1)  errors++;                                               \
+    if (u_store2 != d2)  errors++;                                               \
                                                                                  \
-        return errors;                                                           \
+    return errors;                                                               \
 }
 
 #pragma off(check_stack);   /* I zeroed DS, do not call __STK  */
@@ -97,21 +97,21 @@ testdata(__int64, 'I', 'J', -1, 0xDEADC0FFEEFEED)
 
 int main(void)
 {
-	expand(char);
-	expand(short);
-	expand(int);
-	expand(long);
-	expand(__int64);
+    expand(char);
+    expand(short);
+    expand(int);
+    expand(long);
+    expand(__int64);
 
-	store(char);
-	store(short);
-	store(int);
-	store(long);
-	store(__int64);
+    store(char);
+    store(short);
+    store(int);
+    store(long);
+    store(__int64);
 
-	_PASS;
+    _PASS;
 
-	return 0;
+    return 0;
 }
 
 #endif
