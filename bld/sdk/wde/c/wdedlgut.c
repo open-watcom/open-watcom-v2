b3edbf1bfd49a54fd0f5c1c26cd26a3b38e4ce0c
Jiri Malak <malak.jiri@gmail.com>
2017-03-27 22:49:28 +0200

    add new code for in-memory dialog data manipulation function

---
+++ bld/sdk/wde/c/wdedlgut.c
@@ -0,0 +1,141 @@
+/****************************************************************************
+*
+*                            Open Watcom Project
+*
+* Copyright (c) 2017-2017 The Open Watcom Contributors. All Rights Reserved.
+*
+*  ========================================================================
+*
+*    This file contains Original Code and/or Modifications of Original
+*    Code as defined in and that are subject to the Sybase Open Watcom
+*    Public License version 1.0 (the 'License'). You may not use this file
+*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
+*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
+*    provided with the Original Code and Modifications, and is also
+*    available at www.sybase.com/developer/opensource.
+*
+*    The Original Code and all software distributed under the License are
+*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
+*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
+*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
+*    NON-INFRINGEMENT. Please see the License for the specific language
+*    governing rights and limitations under the License.
+*
+*  ========================================================================
+*
+* Description:  In-memory Dialog manipulation functions
+*
+****************************************************************************/
+
+
+#include "wdeglbl.h"
+#include "windlg.h"
+#include "wdedlgut.h"
+
+
+/*
+ * DlgStringLength - get length of string
+ */
+size_t DlgStringLength( const char _ISFAR *str )
+{
+#if defined( __WINDOWS__ )
+    return( SLEN( str ) );
+#else
+    if( str == NULL ) {
+        return( SLEN( str ) );
+    } else {
+        int len;
+        len = MultiByteToWideChar( CP_OEMCP, MB_ERR_INVALID_CHARS, str, -1, NULL, 0 );
+        if( len == 0 || len == ERROR_NO_UNICODE_TRANSLATION ) {
+            return( SLEN( str ) );
+        }
+        return( len * sizeof( WCHAR ) );
+    }
+#endif
+
+} /* DlgStringLength */
+
+/*
+ * DlgCopyString - copy from string to memory
+ */
+char _ISFAR *DlgCopyString( char _ISFAR *mem, const char _ISFAR *str, int len )
+{
+#if defined( __WINDOWS__ )
+    if( mem != NULL && str != NULL ) {
+        _FARmemcpy( mem, str, len );
+    }
+    return( mem + len );
+#else
+    int i;
+
+    if( mem != NULL && str != NULL ) {
+        for( i = 0; i < len / 2; i++ ) {
+            *(short *)mem = *(unsigned char *)str;
+            mem += 2;
+            str++;
+        }
+        return( mem );
+    } else {
+        return( mem + len );
+    }
+#endif
+
+} /* DlgCopyString */
+
+/*
+ * DlgCopyMBString - copy from string to memory
+ */
+char _ISFAR *DlgCopyMBString( char _ISFAR *mem, const char _ISFAR *str, int len )
+{
+#if defined( __WINDOWS__ )
+    return( DlgCopyString( mem, str, len ) );
+#else
+    if( mem != NULL && str != NULL ) {
+        int     len2;
+        len2 = MultiByteToWideChar( CP_OEMCP, MB_ERR_INVALID_CHARS, str, -1, (LPWSTR)mem, len );
+        len2 *= sizeof( WCHAR );
+        if( len2 != len ) {
+            return( DlgCopyString( mem, str, len ) );
+        }
+        return( mem + len );
+    } else {
+        return( DlgCopyString( mem, str, len ) );
+    }
+#endif
+
+} /* DlgCopyMBString */
+
+
+/*
+ * DlgCopyWord - copy from Word to memory
+ */
+char _ISFAR *DlgCopyWord( char _ISFAR *mem, WORD word )
+{
+    *mem++ = word;
+    *mem++ = word >> 8;
+    return( mem );
+
+} /* DlgCopyWord */
+
+unsigned char DlgGetClassOrdinal( const char *class )
+{
+    unsigned char   class_ordinal;
+
+    if( stricmp( class, "combobox" ) == 0 ) {
+        class_ordinal = 0x85;
+    } else if( stricmp( class, "scrollbar" ) == 0 ) {
+        class_ordinal = 0x84;
+    } else if( stricmp( class, "listbox" ) == 0 ) {
+        class_ordinal = 0x83;
+    } else if( stricmp( class, "static" ) == 0 ) {
+        class_ordinal = 0x82;
+    } else if( stricmp( class, "edit" ) == 0 ) {
+        class_ordinal = 0x81;
+    } else if( stricmp( class, "button" ) == 0 ) {
+        class_ordinal = 0x80;
+    } else {
+        class_ordinal = 0;
+    }
+    return( class_ordinal );
+}
