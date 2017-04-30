7ea83f3ea9f013aef138b90added8b070bd4aa8d
Jiri Malak <malak.jiri@gmail.com>
2017-04-30 16:29:16 +0200

    a few Alpha fixes

--- bld/clib/alpha/h/fpcraxp.h
+++ bld/clib/alpha/h/fpcraxp.h
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-*    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
+* Copyright (c) 2017-2017 The Open Watcom Contributors. All Rights Reserved.
 *
 *  ========================================================================
 *
@@ -24,38 +24,10 @@
 *
 *  ========================================================================
 *
-* Description:  Clear Alpha FP exceptions.
+* Description:  Interface to assembly code
 *
 ****************************************************************************/
 
 
-#include "variety.h"
-#include "rtdata.h"
-#include "fpcraxp.h"
-#include "clearfpe.h"
-
-
-/*
- * Alpha FPCR Exception Bits
- * -------------------------
- *
- * Bit 63 : Summary Bit (OR of Bits 57 - 52)
- * Bit 57 : Integer Overflow (IOV) Exception
- * Bit 56 : Inexact Result (INE) Exception
- * Bit 55 : Underflow (UNF) Exception
- * Bit 54 : Overflow (OVF) Exception
- * Bit 53 : Division by Zero (DZE) Exception
- * Bit 52 : Invalid Operation (INV) Exception
- *
- * Since we are using only the highword of the FPCR, these bits will map
- * to 31 and 25-20 respectively.
- */
-
-void _ClearFPE(void)
-{
-    unsigned long fpcr;
-
-    fpcr = _GetFPCR();
-    fpcr &= 0x7c0fffffL;
-    _SetFPCR(fpcr);
-} /* _ClearFPE() */
+extern unsigned long _GetFPCR( void );
+extern void          _SetFPCR( unsigned long );
