cac3ccaf551554a9d22f951bd4d611ae01794cff
Jiri Malak <malak.jiri@gmail.com>
2017-05-11 22:09:57 +0200

    add executable flag

--- gitget.sh
+++ gitget.sh
@@ -15,6 +15,7 @@ git clone --quiet --branch=master https://${GITHUB_TOKEN}@github.com/open-watcom
 #
 # compress GitHub repository to hold only a few last builds
 #
+depth=0
 cd ../travis-ci-ow-builds
 depth=$(`git rev-list HEAD --count`)
 if [ $depth -gt 10 ]
