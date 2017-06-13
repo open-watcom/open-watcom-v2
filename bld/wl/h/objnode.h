6fbad91ba59182e42716e446722e1196273eb1d1
Jiri Malak <malak.jiri@gmail.com>
2017-06-13 10:43:32 +0200

    reformat source file, apply OW rules

--- bld/wl/h/objnode.h
+++ bld/wl/h/objnode.h
@@ -33,27 +33,25 @@
 typedef struct nodearray        NODEARRAY;
 
 extern void             InitNodes( void );
-extern void *           FindNode( NODEARRAY *, unsigned );
+extern void             *FindNode( NODEARRAY *, unsigned );
 extern unsigned         GetNumNodes( NODEARRAY * );
-extern void *           AllocNode( NODEARRAY * );
-extern void *           AllocNodeIdx( NODEARRAY *, unsigned );
-extern mod_entry *      NewModEntry( void );
+extern void             *AllocNode( NODEARRAY * );
+extern void             *AllocNodeIdx( NODEARRAY *, unsigned );
+extern mod_entry        *NewModEntry( void );
 extern void             FreeModEntry( mod_entry * );
 extern void             FreeNodes( NODEARRAY * );
-extern void             IterateNodelist( NODEARRAY *, void (*)(void *,void *),
-                                         void *);
+extern void             IterateNodelist( NODEARRAY *, void (*)(void *,void *), void *);
 extern void             BurnNodes( void );
 extern void             ReleaseNames( void );
 extern void             CollapseLazyExtdefs( void );
-extern segnode *        FindSegdata( seg_leader * );
-extern extnode *        FindExtHandle( void * );
-extern segdata *        AllocSegData( void );
+extern segnode          *FindSegdata( seg_leader * );
+extern extnode          *FindExtHandle( void * );
+extern segdata          *AllocSegData( void );
 extern void             FreeSegData( void * );
-extern list_of_names *  MakeListName( char *, size_t );
+extern list_of_names    *MakeListName( char *, size_t );
 extern unsigned long    BadObjFormat( void );
 
-
-extern NODEARRAY *      ExtNodes;           // ptr to obj file import list
-extern NODEARRAY *      SegNodes;           // ptr to obj file segment list
-extern NODEARRAY *      GrpNodes;           // ptr to obj file group list
-extern NODEARRAY *      NameNodes;          // ptr to obj file lname list
+extern NODEARRAY        *ExtNodes;           // ptr to obj file import list
+extern NODEARRAY        *SegNodes;           // ptr to obj file segment list
+extern NODEARRAY        *GrpNodes;           // ptr to obj file group list
+extern NODEARRAY        *NameNodes;          // ptr to obj file lname list
