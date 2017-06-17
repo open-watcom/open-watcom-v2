7b1050aee8c42cae42fb3d76c18a2722724dee42
Jiri Malak <malak.jiri@gmail.com>
2017-06-17 14:57:19 +0200

    set OWVERBOSE=1 for OSX build

--- gitupds.sh
+++ gitupds.sh
@@ -9,11 +9,8 @@ cd ../travis-ci-ow-builds
 #
 # commit updated files to GitHub repository
 #
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]
-then
-  if [ "$TRAVIS_OS_NAME" = "osx" ]
-  then
-  else
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     git add -f .
     git commit --quiet -m "Travis CI build $TRAVIS_BUILD_NUMBER (success) - OW distribution"
     git push --quiet -f origin
