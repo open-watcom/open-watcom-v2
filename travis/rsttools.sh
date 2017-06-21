06b43671605a8b22fd9b1b7daf6888dd2fbe4edb
Jiri Malak <malak.jiri@gmail.com>
2017-06-21 09:18:29 +0200

    correct travis scripts

--- travis/rsttools.sh
+++ travis/rsttools.sh
@@ -5,10 +5,10 @@
 #
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    . travis/dropbox.sh download OSX/bin/ build/bin/
-    . travis/dropbox.sh download OSX/watcom/ bld/watcom/binbuild/
+    travis/dropbox.sh download OSX/bin/ build/bin/
+    travis/dropbox.sh download OSX/watcom/ bld/watcom/binbuild/
 else
-    . travis/dropbox.sh download Linux/bin/ build/bin/
-    . travis/dropbox.sh download Linux/watcom/ bld/watcom/binbuild/
+    travis/dropbox.sh download Linux/bin/ build/bin/
+    travis/dropbox.sh download Linux/watcom/ bld/watcom/binbuild/
 fi
 chmod +x build/bin/*
--- travis/savtools.sh
+++ travis/savtools.sh
@@ -5,11 +5,11 @@
 #
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    . travis/dropbox.sh upload build/bin/ OSX/bin/
-    . travis/dropbox.sh upload bld/watcom/binbuild/clibext.lib OSX/watcom/
-    . travis/dropbox.sh upload bld/watcom/binbuild/clibexts.lib OSX/watcom/
+    travis/dropbox.sh upload build/bin/ OSX/bin/
+    travis/dropbox.sh upload bld/watcom/binbuild/clibext.lib OSX/watcom/
+    travis/dropbox.sh upload bld/watcom/binbuild/clibexts.lib OSX/watcom/
 else
-    . travis/dropbox.sh upload build/bin/ Linux/bin/
-    . travis/dropbox.sh upload bld/watcom/binbuild/clibext.lib Linux/watcom/
-    . travis/dropbox.sh upload bld/watcom/binbuild/clibexts.lib Linux/watcom/
+    travis/dropbox.sh upload build/bin/ Linux/bin/
+    travis/dropbox.sh upload bld/watcom/binbuild/clibext.lib Linux/watcom/
+    travis/dropbox.sh upload bld/watcom/binbuild/clibexts.lib Linux/watcom/
 fi
