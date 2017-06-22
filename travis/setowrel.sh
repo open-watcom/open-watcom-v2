ba65053aa1c52e20e1bf728fc4e79ff0f83b1a55
Jiri Malak <malak.jiri@gmail.com>
2017-06-22 23:04:55 +0200

    correct commit text for Travis OW build commit

--- travis/setowrel.sh
+++ travis/setowrel.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+# *****************************************************************
+# setowrel.sh - set OW release directory for output OW build
+# *****************************************************************
+#
+
 mkdir -p ../travis-ci-ow-builds
 cd ../travis-ci-ow-builds
 export OWRELROOT=`pwd`
