e6b2263a0006608df38aedef9a05b0db10bc46d8
Jiri Malak <malak.jiri@gmail.com>
2017-06-22 13:06:04 +0200

    correct Travis pull request build

--- travis/build.sh
+++ travis/build.sh
@@ -8,45 +8,64 @@
 #   - correct setup for all OW build environment variables
 #
 
-build_proc()
+bootstrap_proc()
 {
-    if [ "$1" = "boot" ]; then
+    #
+    # build new verison of wmake for host system
+    #
+    cd $OWSRCDIR/wmake
+    mkdir $OWOBJDIR
+    cd $OWOBJDIR
+    rm -f $OWBINDIR/wmake
+    case `uname` in
+        Darwin)
+            make -f ../posmake clean
+            make -f ../posmake TARGETDEF=-D__OSX__
+            ;;
+        *)
+            make -f ../posmake clean
+            make -f ../posmake TARGETDEF=-D__LINUX__
+            ;;
+    esac
+    RC=$?
+    if [ $RC -eq 0 ]; then
         #
-        # build new verison of wmake for host system
+        # build new verison of builder for host system
         #
-        cd $OWSRCDIR/wmake
+        cd $OWSRCDIR/builder
         mkdir $OWOBJDIR
         cd $OWOBJDIR
-        rm -f $OWBINDIR/wmake
-        case `uname` in
-            Darwin)
-                make -f ../posmake clean
-                make -f ../posmake TARGETDEF=-D__OSX__
-                ;;
-            *)
-                make -f ../posmake clean
-                make -f ../posmake TARGETDEF=-D__LINUX__
-                ;;
-        esac
+        rm -f $OWBINDIR/builder
+        $OWBINDIR/wmake -f ../binmake clean
+        $OWBINDIR/wmake -f ../binmake bootstrap=1 builder.exe
         RC=$?
         if [ $RC -eq 0 ]; then
-            #
-            # build new verison of builder for host system
-            #
-            cd $OWSRCDIR/builder
-            mkdir $OWOBJDIR
-            cd $OWOBJDIR
-            rm -f $OWBINDIR/builder
-            $OWBINDIR/wmake -f ../binmake clean
-            $OWBINDIR/wmake -f ../binmake bootstrap=1 builder.exe
             cd $OWSRCDIR
             builder boot
             RC=$?
         fi
+    fi
+}
+
+build_proc()
+{
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+        if [ "$1" = "boot" ]; then
+            bootstrap_proc
+        else
+            cd $OWSRCDIR
+            builder $1
+            RC=$?
+        fi
     else
-        cd $OWSRCDIR
-        builder $1
-        RC=$?
+        if [ "$1" = "rel" ]; then
+            bootstrap_proc
+            if [ $RC -eq 0 ]; then
+                cd $OWSRCDIR
+                builder rel
+                RC=$?
+            fi
+        fi
     fi
     cd $OWROOT
     return $RC
--- travis/rsttools.sh
+++ travis/rsttools.sh
@@ -4,11 +4,13 @@
 # *****************************************************************
 #
 
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    travis/dropbox.sh download OSX/bin/ build/bin/
-    travis/dropbox.sh download OSX/watcom/ bld/watcom/binbuild/
-else
-    travis/dropbox.sh download Linux/bin/ build/bin/
-    travis/dropbox.sh download Linux/watcom/ bld/watcom/binbuild/
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+        travis/dropbox.sh download OSX/bin/ build/bin/
+        travis/dropbox.sh download OSX/watcom/ bld/watcom/binbuild/
+    else
+        travis/dropbox.sh download Linux/bin/ build/bin/
+        travis/dropbox.sh download Linux/watcom/ bld/watcom/binbuild/
+    fi
+    chmod +x build/bin/*
 fi
-chmod +x build/bin/*
