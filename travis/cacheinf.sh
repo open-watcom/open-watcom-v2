37c8cc3cb2bdc3b8ee6fcdb8799fc87095cee01a
Jiri Malak <malak.jiri@gmail.com>
2017-06-23 17:10:04 +0200

    change travis setup

--- travis/cacheinf.sh
+++ travis/cacheinf.sh
@@ -1,7 +1,5 @@
 echo "*****************"
 #    ls -l ${TRAVIS_BUILD_DIR}/build/bin
-echo "*****************"
-#    ls -l ${HOME}/mycache
 echo "**** ls .casher ****"
     ls -l ${HOME}/.casher
 echo "**** paths ****"
@@ -14,4 +12,8 @@ echo "**** tar.err.log ****"
     cat ${HOME}/.casher/tar.err.log
 echo "**** tar.log ****"
     cat ${HOME}/.casher/tar.log
+echo "**** push.err.log ****"
+    cat ${HOME}/.casher/push.err.log
+echo "**** push.log ****"
+    cat ${HOME}/.casher/push.log
 echo "*****************"
