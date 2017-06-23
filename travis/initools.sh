8da51c4d8c6b7be61b0f354aff57427cf6d36682
Jiri Malak <malak.jiri@gmail.com>
2017-06-23 15:12:14 +0200

    display travis cache internal data

--- travis/initools.sh
+++ travis/initools.sh
@@ -7,6 +7,8 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     ls -l ${TRAVIS_BUILD_DIR}/build/bin/*
     ls -l ${HOME}/mycache/*
+    ls -l ${HOME}/.casher/*
+    cat ${HOME}/.casher/paths
     echo "initools.sh - done"
 else
     echo "initools.sh - skipped"
