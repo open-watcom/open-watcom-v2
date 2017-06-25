4f34de535c5f4bc3a179f108c1d4a944b0fd9bf1
Jiri Malak <malak.jiri@gmail.com>
2017-06-25 02:44:01 +0200

    fix problem with travis cache If sudo changed then cache has different signature

--- travis/gitupds.sh
+++ travis/gitupds.sh
@@ -7,34 +7,31 @@
 #
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-    case "$OWTRAVISJOB" in
-        BUILD|DOCPDF)
-            if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
-                if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-                    #
-                    # commit updated files to GitHub repository
-                    #
-                    cd $OWRELROOT
-                    git add -f .
-                    if [ "$OWTRAVISJOB" = "DOCPDF" ]; then
-                        git commit --quiet -m "Travis CI build $TRAVIS_JOB_NUMBER - Documentation"
-                    else
-                        git commit --quiet -m "Travis CI build $TRAVIS_JOB_NUMBER - OW distribution"
-                    fi
-                    git push --quiet -f origin
-                    cd $TRAVIS_BUILD_DIR
-                    echo "gitupds.sh - done"
+    if [ "$OWTRAVISJOB" = "BUILD" ] || [ "$OWTRAVISJOB" = "DOCPDF" ]; then
+        if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
+            if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+                #
+                # commit updated files to GitHub repository
+                #
+                cd $OWRELROOT
+                git add -f .
+                if [ "$OWTRAVISJOB" = "DOCPDF" ]; then
+                    git commit --quiet -m "Travis CI build $TRAVIS_JOB_NUMBER - Documentation"
                 else
-                    echo "gitupds.sh - skipped"
+                    git commit --quiet -m "Travis CI build $TRAVIS_JOB_NUMBER - OW distribution"
                 fi
+                git push --quiet -f origin
+                cd $TRAVIS_BUILD_DIR
+                echo "gitupds.sh - done"
             else
                 echo "gitupds.sh - skipped"
             fi
-            ;;
-        *)
+        else
             echo "gitupds.sh - skipped"
-            ;;
-    esac
+        fi
+    else
+        echo "gitupds.sh - skipped"
+    fi
 elif [ "$COVERITY_SCAN_BRANCH" = 1 ]; then
     echo "gitupds.sh - skipped"
 else
