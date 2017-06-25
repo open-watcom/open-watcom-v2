dcce610b6a413fb1a521d727dd077e18b314be31
Jiri Malak <malak.jiri@gmail.com>
2017-06-25 18:01:25 +0200

    cleanup travis code

--- travis/tfini.sh
+++ travis/tfini.sh
@@ -11,32 +11,32 @@ rm -rf  bld/watcom/binbuild/*
 
 echo_msg="tfini.sh - skipped"
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-if [ "$COVERITY_SCAN_BRANCH" = 1 ]; then
-    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
-        #
-        # configure Git client
-        #
-        git config --global user.email "travis@travis-ci.org"
-        git config --global user.name "Travis CI"
-        git config --global push.default simple
-        #
-        cd ..
-        rm -rf $TRAVIS_BUILD_DIR
-        git clone $GITQUIET https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git $TRAVIS_BUILD_DIR
-        cd $TRAVIS_BUILD_DIR
-        #
-        git checkout coverity_scan
-        git merge master
-        git add -A
-        git commit $GITQUIET -am "Travis CI update from master branch"
-        git push $GITQUIET -f origin coverity_scan
-        #
-        echo_msg="tfini.sh - done"
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    if [ "$TRAVIS_BRANCH" != "master" ]; then
+        if [ "$COVERITY_SCAN_BRANCH" = 1 ]; then
+            if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+                #
+                # configure Git client
+                #
+                git config --global user.email "travis@travis-ci.org"
+                git config --global user.name "Travis CI"
+                git config --global push.default simple
+                #
+                cd ..
+                rm -rf $TRAVIS_BUILD_DIR
+                git clone $GITQUIET https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git $TRAVIS_BUILD_DIR
+                cd $TRAVIS_BUILD_DIR
+                #
+                git checkout coverity_scan
+                git merge master
+                git add -A
+                git commit $GITQUIET -am "Travis CI update from master branch"
+                git push $GITQUIET -f origin coverity_scan
+                #
+                echo_msg="tfini.sh - done"
+            fi
+        fi
     fi
-    fi
-fi
 fi
 
 echo "$echo_msg"
--- travis/tinit.sh
+++ travis/tinit.sh
@@ -10,39 +10,39 @@ echo_msg="githubin.sh - skipped"
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     if [ "$TRAVIS_BRANCH" = "master" ]; then
-    if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
-        #
-        # configure Git client
-        #
-        git config --global user.email "travis@travis-ci.org"
-        git config --global user.name "Travis CI"
-        git config --global push.default simple
-        #
-        # clone GitHub repository
-        #
-        git clone $GITQUIET --branch=master https://${GITHUB_TOKEN}@github.com/${OWTRAVIS_REPO_SLUG}.git $OWTRAVIS_BUILD_DIR
-        #
-        # compress GitHub repository to hold only a few last builds
-        #
-        cd $OWTRAVIS_BUILD_DIR
-        depth=`git rev-list HEAD --count`
-        if [ $depth -gt 12 ]; then
-            echo "githubin.sh - start compression"
-            git checkout --orphan temp1
-            git add -A
-            git commit -am "Initial commit"
-            git branch -D master
-            git branch -m master
-            git push -f origin master
-            git branch --set-upstream-to=origin/master master
-            echo "githubin.sh - end compression"
+        if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
+            #
+            # configure Git client
+            #
+            git config --global user.email "travis@travis-ci.org"
+            git config --global user.name "Travis CI"
+            git config --global push.default simple
+            #
+            # clone GitHub repository
+            #
+            git clone $GITQUIET --branch=master https://${GITHUB_TOKEN}@github.com/${OWTRAVIS_REPO_SLUG}.git $OWTRAVIS_BUILD_DIR
+            #
+            # compress GitHub repository to hold only a few last builds
+            #
+            cd $OWTRAVIS_BUILD_DIR
+            depth=`git rev-list HEAD --count`
+            if [ $depth -gt 12 ]; then
+                echo "githubin.sh - start compression"
+                git checkout --orphan temp1
+                git add -A
+                git commit -am "Initial commit"
+                git branch -D master
+                git branch -m master
+                git push -f origin master
+                git branch --set-upstream-to=origin/master master
+                echo "githubin.sh - end compression"
+            fi
+            cd $TRAVIS_BUILD_DIR
+            echo_msg="githubin.sh - done"
         fi
-        cd $TRAVIS_BUILD_DIR
-        echo_msg="githubin.sh - done"
+#    elif [ "$COVERITY_SCAN_BRANCH" = 1 ]; then
+#    else
     fi
-    fi
-#elif [ "$COVERITY_SCAN_BRANCH" = 1 ]; then
-#else
 fi
 
 echo "$echo_msg"
