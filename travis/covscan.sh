d5f687a270c74b14903c548ee806e7c532652e84
Jiri Malak <malak.jiri@gmail.com>
2017-06-27 00:36:23 +0200

    add script for handling coverity scan

---
+++ travis/covscan.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set +x
+
+coverity_load_proc()
+{
+    PLATFORM=`uname`
+    TOOL_ARCHIVE=/tmp/cov-analysis-${PLATFORM}.tgz
+    TOOL_BASE=/tmp/coverity-scan-analysis
+
+    wget -nv -O $TOOL_ARCHIVE https://scan.coverity.com/download/${PLATFORM} --post-data "project=$TRAVIS_REPO_SLUG&token=$COVERITY_SCAN_TOKEN"
+
+    mkdir -p $TOOL_BASE
+    tar xzf $TOOL_ARCHIVE -C $TOOL_BASE
+    
+    TOOL_DIR=`find $TOOL_BASE -type d -name 'cov-analysis*'`
+    export PATH=$TOOL_DIR/bin:$PATH
+    return 0
+}
+
+coverity_run_proc()
+{
+    pwd
+    #cov-build --dir cov-int build.sh
+    #cov-import-scm --dir cov-int --scm git --log cov-int/scm_log.txt 2>&1
+    return 0
+}
+
+coverity_upload_proc()
+{
+    return 0
+}
+
+coverity_load_proc
+coverity_run_proc
+coverity_upload_proc
