aa0d4290215050e9100f0f82b8ee7c222b53bbc4
Jiri Malak <malak.jiri@gmail.com>
2017-08-13 06:51:38 +0200

    cleanup awk code

--- bld/awk/c/missin95.c
+++ bld/awk/c/missin95.c
@@ -3,8 +3,9 @@
    if this won't load, use the return NULL statements. */
 
 #include <stdio.h>
+#include "awk.h"
 
-FILE *popen(char *s, char *m) {
+FILE *popen( const char *s, const char *m ) {
 #ifdef __DOS__
     s=s;m=m;
     return( NULL );
@@ -13,7 +14,7 @@ FILE *popen(char *s, char *m) {
 #endif
 }
 
-int pclose(FILE *f) {
+int pclose( FILE *f ) {
 #ifdef __DOS__
     f=f;
     return( 0 );
