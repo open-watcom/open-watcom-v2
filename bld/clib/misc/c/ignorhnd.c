66bbf4c18fb8689dd6c8e416e12f7cdeb684cb38
Jiri Malak <malak.jiri@gmail.com>
2017-09-18 07:11:22 +0200

    correct prototypes

--- bld/clib/misc/c/ignorhnd.c
+++ bld/clib/misc/c/ignorhnd.c
@@ -2,6 +2,7 @@
 *
 *                            Open Watcom Project
 *
+* Copyright (c) 2002-2017 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -33,7 +34,7 @@
 #include "saferlib.h"
 
 
-_WCRTLINK void ignore_handler_s( const char * __restrict msg,
+_WCHANDLER void ignore_handler_s( const char * __restrict msg,
                                  void * __restrict ptr, errno_t error )
 /*********************************************************************/
 {
