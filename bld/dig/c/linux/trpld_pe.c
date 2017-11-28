900ddabb6ea1fa1621cebb60c2b35a17beaff7e8
Jiri Malak <malak.jiri@gmail.com>
2017-11-28 16:55:09 +0100

    cleanup trap PE loader code

--- bld/dig/c/linux/trpld_pe.c
+++ bld/dig/c/linux/trpld_pe.c
@@ -110,7 +110,7 @@ char *LoadTrap( const char *parms, char *buff, trap_version *trap_ver )
         sprintf( buff, TC_ERR_CANT_LOAD_TRAP, parms );
         return( buff );
     }
-    TrapFile = PE_loadLibraryHandle( -1, trap_name );
+    TrapFile = PE_loadLibraryFile( fp );
     DIGLoader( Close )( fp );
     if( TrapFile == NULL ) {
         sprintf( buff, TC_ERR_CANT_LOAD_TRAP, trap_name );
