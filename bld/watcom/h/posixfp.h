048c2adebc1ab81f115a0352cb064f7209c323b2
Jiri Malak <malak.jiri@gmail.com>
2017-12-09 20:21:30 +0100

    correct file handles conversion macros

--- bld/watcom/h/posixfp.h
+++ bld/watcom/h/posixfp.h
@@ -4,9 +4,9 @@
  * to enable simple test for NULL for both handles
  */
 #if defined( _WIN64 )
-#define POSIX2FP(ph)    ((FILE *)((unsigned __int64)(ph) + 1))
-#define FP2POSIX(fp)    ((int)((unsigned __int64)(fp) - 1))
+#define POSIX2FP(ph)    ((FILE *)(unsigned __int64)((ph) + 1))
+#define FP2POSIX(fp)    (((int)(unsigned __int64)(fp)) - 1)
 #else
-#define POSIX2FP(ph)    ((FILE *)((unsigned long)(ph) + 1))
-#define FP2POSIX(fp)    ((int)((unsigned long)(fp) - 1))
+#define POSIX2FP(ph)    ((FILE *)(unsigned long)((ph) + 1))
+#define FP2POSIX(fp)    (((int)(unsigned long)(fp)) - 1)
 #endif
