60dd3043e4e9ad34040a959e69539490ba0c61cd
Jiri Malak <malak.jiri@gmail.com>
2017-12-19 00:35:42 +0100

    cleanup code

--- bld/lib_misc/h/exitwmsg.h
+++ bld/lib_misc/h/exitwmsg.h
@@ -42,7 +42,6 @@
 // - note there is no #pragma aborts so that debugger can trace out
 extern _WCRTLINK _WCNORETURN void __exit_with_msg( char _WCI86FAR *, unsigned );
 extern _WCRTLINK _WCNORETURN void __fatal_runtime_error( char _WCI86FAR *, unsigned );
-extern _WCRTLINK _WCNORETURN void _Not_Enough_Memory( void );
 extern _WCRTLINK _WCNORETURN void __exit( unsigned );
 
 // ASM interface
