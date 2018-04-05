45d906077a52a90d35f974604609c7eaf951f8cf
Jiri Malak <malak.jiri@gmail.com>
2018-04-05 17:38:24 +0200

    fix for issue #429 fix order of tests to fix array overflow

--- bld/wv/rfx/rfxacc.c
+++ bld/wv/rfx/rfxacc.c
@@ -236,13 +236,15 @@ static void mylocaltime( unsigned long date_time, int *time, int *date )
         }
     }
     if( ( ( num_yr_since_1970 - 2 ) % 4 ) == 0 ) {
-        for( month = 2; month <= 12; ++day_since_jan[month], ++month ) {
-            ;
+        for( month = 2; month <= 12; ++month ) {
+            ++day_since_jan[month];
         }
     }
     year = num_yr_since_1970 - 10;
-    for( month = 1; ( day > day_since_jan[month] && month <= 12 ); month++ ) {
-        ;
+    for( month = 1; month <= 12; month++ ) {
+        if( day <= day_since_jan[month] ) {
+            break;
+        }
     }
     day -= day_since_jan[month - 1];
     date_time %= 86400;
