5eabf776929d93fe3a6cace1adf88f32658929f4
Jiri Malak <malak.jiri@gmail.com>
2018-08-27 17:09:36 +0200

    fix build by Visual C++

--- bld/wipfc/icu/common/unicode/ptypes.h
+++ bld/wipfc/icu/common/unicode/ptypes.h
@@ -127,7 +127,12 @@ typedef unsigned int uint32_t;
 
 #endif /* U_HAVE_STDINT_H / U_HAVE_INTTYPES_H */
 
-#if defined( __cplusplus ) && __cplusplus < 201103L
+
+#if defined( _MSC_VER )
+#if _MSC_VER < 1600
+typedef uint16_t char16_t;
+#endif
+#elif !defined( __cplusplus ) || __cplusplus < 201100L
 typedef uint16_t char16_t;
 #endif
 
