c001917fd374a3794970feee3d4fbd0f49cfb8f7
Jiri Malak <malak.jiri@gmail.com>
2018-11-02 02:23:59 +0100

    cleanup regression tests

--- bld/wmake/regress/fortest/test.bat
+++ bld/wmake/regress/fortest/test.bat
@@ -1,28 +1,33 @@
 @echo %verbose% off
+
 set ERRORS=0
+
 echo # ===========================
 echo # Start FORTEST
 echo # ===========================
+
 if .%1 == . goto usage
+set PRG=%1
+set ERRLOG=%2
 
 set TEST=01
 call :header
 if exist for01.out del for01.out
-%1 -h -f for01 > for01.out 2>&1
+%PRG% -h -f for01 > for01.out 2>&1
 diff for01.out for01.chk
 call :result
 
 set TEST=02
 call :header
 if exist for02.out del for02.out
-%1 -h -f for02 > for02.out 2>&1
+%PRG% -h -f for02 > for02.out 2>&1
 diff for02.out for02.chk
 call :result
 
 set TEST=03
 call :header
 if exist for03.out del for03.out
-%1 -h -f for03 > for03.out 2>&1
+%PRG% -h -f for03 > for03.out 2>&1
 diff for03.out for03.chk
 call :result
 
@@ -35,28 +40,28 @@ dir /b /-o       >> tmp04.chk
 type for04b.chk  >> tmp04.chk
 dir /b /-o for?? >> tmp04.chk
 type for04c.chk  >> tmp04.chk
-%1 -h -f for04 > for04.out 2>&1
+%PRG% -h -f for04 > for04.out 2>&1
 diff for04.out tmp04.chk
 call :result
 
 set TEST=05
 call :header
 if exist for05.out del for05.out
-%1 -h -f for05 > for05.out 2>&1
+%PRG% -h -f for05 > for05.out 2>&1
 diff for05.out for05.chk
 call :result
 
 set TEST=06
 call :header
 if exist for06.out del for06.out
-%1 -h -f for06 > for06.out 2>&1
+%PRG% -h -f for06 > for06.out 2>&1
 diff for06.out for06.chk
 call :result
 
 set TEST=07
 call :header
 if exist for07.out del for07.out
-%1 -h -f for07 > for07.out 2>&1
+%PRG% -h -f for07 > for07.out 2>&1
 diff for07.out for07.chk
 call :result
 
@@ -73,7 +78,7 @@ echo a >> tmp08.chk
 echo b >> tmp08.chk
 ..\cmds\prntdir "echo c" >> tmp08.chk
 echo c >> tmp08.chk
-%1 -h -f for08 > for08.out 2>&1
+%PRG% -h -f for08 > for08.out 2>&1
 diff -b for08.out tmp08.chk
 call :result
 
@@ -82,21 +87,21 @@ if %ERRORS% == 0 del *.lst
 goto end
 
 :usage
-echo usage: %0 prgname errorfile
-goto end
+    echo usage: %0 prgname errorfile
+    goto end
 
 :header
-echo # ---------------------------
-echo # FOR Loop Test %TEST%
-echo # ---------------------------
-goto end
+    echo # ---------------------------
+    echo # For Loop Test %TEST%
+    echo # ---------------------------
+    goto end
 
 :result
 if errorlevel 1 goto resulterr
-    @echo #        Test %TEST% successful
+    echo #        Test successful
     goto end
 :resulterr
-    @echo ## FORTEST ## >> %2
-    @echo # Error: Test %TEST% unsuccessful!!! | tee -a %2
+    echo ## FORTEST ## >> %ERRLOG%
+    echo # Error: Test unsuccessful!!! | tee -a %ERRLOG%
     set ERRORS=1
 :end
