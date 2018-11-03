883750fd8003b7e3f3af2f471bd630ec1568974b
Jiri Malak <malak.jiri@gmail.com>
2018-11-03 18:27:18 +0100

    fix wclass project build

--- bld/wclass/cpp/wstatdlg.cpp
+++ bld/wclass/cpp/wstatdlg.cpp
@@ -94,8 +94,8 @@ void WStatDialog::doDialog( WWindow *parent ) {
     create_info.scroll = GUI_NOSCROLL;
     create_info.style = GUI_NONE;
     create_info.parent = hparent;
-    create_info.num_items = 0;
-    create_info.menu = NULL;
+    create_info.menu.num_items = 0;
+    create_info.menu.menu = NULL;
     create_info.num_attrs = 0;
     create_info.colours = NULL;
     create_info.gui_call_back = DlgGUIEventProc;
