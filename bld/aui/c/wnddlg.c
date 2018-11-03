73d69e4bd357057c2614481feba5c8385c11ab6a
Jiri Malak <malak.jiri@gmail.com>
2018-11-03 21:19:32 +0100

    fix aui project

--- bld/aui/c/wnddlg.c
+++ bld/aui/c/wnddlg.c
@@ -91,7 +91,7 @@ static gui_create_info ResDialog = {
     GUI_NOSCROLL,                       // Scroll Styles
     GUI_VISIBLE | GUI_CLOSEABLE,        // Window Styles
     NULL,                               // Parent
-    0, NULL,                            // Menu array
+    { 0, NULL },                        // Menu array
     0, NULL,                            // Colour attribute array
     dlgOpenGUIEventProc,                // GUI Event Callback function
     NULL,                               // Extra
