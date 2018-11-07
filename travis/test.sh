7d670e9ec66ca72f0f631d06062ca40b3ddb4e1f
Jiri Malak <malak.jiri@gmail.com>
2018-11-07 08:31:52 +0100

    reorganize Travis test jobs

--- travis/test.sh
+++ travis/test.sh
@@ -11,7 +11,22 @@ build_proc()
 {
     if [ "$TRAVIS_BRANCH" = "$OWBRANCH" ]; then
         if [ "$TRAVIS_EVENT_TYPE" = "push" ] && [ "$TRAVIS_OS_NAME" = "linux" ]; then
-            if [ "$1" = "cpp" ]; then
+            if [ "$1" = "wasm" ]; then
+                cd $OWSRCDIR/wasmtest
+                builder -q -i testclean
+                builder -i test
+                cat result.log
+            elif [ "$1" = "c" ]; then
+                cd $OWSRCDIR/ctest
+                builder -q -i testclean
+                builder -i test
+                cat result.log
+            elif [ "$1" = "f77" ]; then
+                cd $OWSRCDIR/f77test
+                builder -q -i testclean
+                builder -i test
+                cat result.log
+            elif [ "$1" = "cpp" ]; then
                 cd $OWSRCDIR/plustest
                 builder -q -i testclean
                 builder -i test
@@ -22,21 +37,24 @@ build_proc()
                 builder -i test
                 cat result.log
             else
-                cd $OWSRCDIR/wasmtest
-                builder -q -i testclean
-                builder -i test
-                cat result.log
-                cd $OWSRCDIR/ctest
-                builder -q -i testclean
-                builder -i test
-                cat result.log
-                cd $OWSRCDIR/f77test
-                builder -q -i testclean
-                builder -i test
-                cat result.log
             fi
         elif [ "$TRAVIS_EVENT_TYPE" = "pull_request" ] && [ "$TRAVIS_OS_NAME" = "linux" ]; then
-            if [ "$1" = "cpp" ]; then
+            if [ "$1" = "wasm" ]; then
+                cd $OWSRCDIR/wasmtest
+                builder -q -i testclean
+                builder -i test
+                cat result.log
+            elif [ "$1" = "c" ]; then
+                cd $OWSRCDIR/ctest
+                builder -q -i testclean
+                builder -i test
+                cat result.log
+            elif [ "$1" = "f77" ]; then
+                cd $OWSRCDIR/f77test
+                builder -q -i testclean
+                builder -i test
+                cat result.log
+            elif [ "$1" = "cpp" ]; then
                 cd $OWSRCDIR/plustest
                 builder -q -i testclean
                 builder -i test
@@ -47,18 +65,6 @@ build_proc()
                 builder -i test
                 cat result.log
             else
-                cd $OWSRCDIR/wasmtest
-                builder -q -i testclean
-                builder -i test
-                cat result.log
-                cd $OWSRCDIR/ctest
-                builder -q -i testclean
-                builder -i test
-                cat result.log
-                cd $OWSRCDIR/f77test
-                builder -q -i testclean
-                builder -i test
-                cat result.log
             fi
         fi
     elif [ "$TRAVIS_BRANCH" = "$OWBRANCH_COVERITY" ]; then
