7d9666820cc1c137b9540aa87d19641db5e81f02
Jiri Malak <malak.jiri@gmail.com>
2018-11-10 00:47:42 +0100

    correct Travis OSX build

--- travis/gitupdf.sh
+++ travis/gitupdf.sh
@@ -19,44 +19,52 @@ gitupdf_proc()
 
     if [ "$TRAVIS_BRANCH" = "$OWBRANCH" ]; then
         if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
-            if [ "$OWTRAVISJOB" = "BOOTSTRAP" ] || [ "$OWTRAVISJOB" = "BUILD" ] || [ "$OWTRAVISJOB" = "DOCPDF" ]; then
-                if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$OWOSXBUILD" != "1" ]; then
-                    return 0
-                fi
-                #
-                # setup client info
-                #
-                git config --global user.email "travis@travis-ci.org"
-                git config --global user.name "Travis CI"
-                git config --global push.default simple
-                #
-                # clone GitHub repository
-                #
-                git clone $GITQUIET --branch=master https://${GITHUB_TOKEN}@github.com/${OWTRAVIS_REPO_SLUG}.git $OWTRAVIS_BUILD_DIR
-                #
-                # copy build log files to git repository tree
-                #
-                if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-                    test -d $OWTRAVIS_BUILD_DIR/logs/osx || mkdir -p $OWTRAVIS_BUILD_DIR/logs/osx
-                    cp $TRAVIS_BUILD_DIR/build/$OWOBJDIR/*.log $OWTRAVIS_BUILD_DIR/logs/osx/
-                else
-                    test -d $OWTRAVIS_BUILD_DIR/logs/linux || mkdir -p $OWTRAVIS_BUILD_DIR/logs/linux
-                    cp $TRAVIS_BUILD_DIR/build/$OWOBJDIR/*.log $OWTRAVIS_BUILD_DIR/logs/linux/
-                fi
-                #
-                # commit new log files to GitHub repository
-                #
-                cd $OWTRAVIS_BUILD_DIR
-                git add -f .
-                if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-                    git commit $GITQUIET -m "Travis CI build $TRAVIS_JOB_NUMBER (failure) - log files (OSX)"
-                else
-                    git commit $GITQUIET -m "Travis CI build $TRAVIS_JOB_NUMBER (failure) - log files (Linux)"
-                fi
-                git push $GITQUIET -f origin
-                cd $TRAVIS_BUILD_DIR
-                echo_msg="gitupdf.sh - done"
-            fi
+            case "$OWTRAVISJOB" in
+                "BOOTSTRAP")
+                "BUILD")
+                "BUILD1")
+                "BUILD2")
+                "DOCPDF")
+                    if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$OWOSXBUILD" != "1" ]; then
+                        return 0
+                    fi
+                    #
+                    # setup client info
+                    #
+                    git config --global user.email "travis@travis-ci.org"
+                    git config --global user.name "Travis CI"
+                    git config --global push.default simple
+                    #
+                    # clone GitHub repository
+                    #
+                    git clone $GITQUIET --branch=master https://${GITHUB_TOKEN}@github.com/${OWTRAVIS_REPO_SLUG}.git $OWTRAVIS_BUILD_DIR
+                    #
+                    # copy build log files to git repository tree
+                    #
+                    if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+                        test -d $OWTRAVIS_BUILD_DIR/logs/osx || mkdir -p $OWTRAVIS_BUILD_DIR/logs/osx
+                        cp $TRAVIS_BUILD_DIR/build/$OWOBJDIR/*.log $OWTRAVIS_BUILD_DIR/logs/osx/
+                    else
+                        test -d $OWTRAVIS_BUILD_DIR/logs/linux || mkdir -p $OWTRAVIS_BUILD_DIR/logs/linux
+                        cp $TRAVIS_BUILD_DIR/build/$OWOBJDIR/*.log $OWTRAVIS_BUILD_DIR/logs/linux/
+                    fi
+                    #
+                    # commit new log files to GitHub repository
+                    #
+                    cd $OWTRAVIS_BUILD_DIR
+                    git add -f .
+                    if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+                        git commit $GITQUIET -m "Travis CI build $TRAVIS_JOB_NUMBER (failure) - log files (OSX)"
+                    else
+                        git commit $GITQUIET -m "Travis CI build $TRAVIS_JOB_NUMBER (failure) - log files (Linux)"
+                    fi
+                    git push $GITQUIET -f origin
+                    cd $TRAVIS_BUILD_DIR
+                    echo_msg="gitupdf.sh - done"
+                    ;;
+                *)
+                    ;;
+            esac
         fi
     fi
 
--- travis/gitupds.sh
+++ travis/gitupds.sh
@@ -19,41 +19,69 @@ gitupds_proc()
 
     if [ "$TRAVIS_BRANCH" = "$OWBRANCH" ]; then
         if [ "$TRAVIS_EVENT_TYPE" = "push" ] && [ "$TRAVIS_OS_NAME" = "linux" ]; then
-            if [ "$OWTRAVISJOB" = "BUILD" ] || [ "$OWTRAVISJOB" = "DOCPDF" ]; then
-                #
-                # setup client info
-                #
-                git config --global user.email "travis@travis-ci.org"
-                git config --global user.name "Travis CI"
-                git config --global push.default simple
-                #
-                # clone GitHub repository
-                #
-                git clone $GITQUIET --branch=master https://${GITHUB_TOKEN}@github.com/${OWTRAVIS_REPO_SLUG}.git $OWTRAVIS_BUILD_DIR
-                #
-                # copy OW build to git tree
-                #
-                export OWRELROOT=$OWTRAVIS_BUILD_DIR
-                cd $OWSRCDIR
-                if [ "$OWTRAVISJOB" = "DOCPDF" ]; then
-                    builder cpdocpdf
-                else
-                    builder cprel
-                fi
-                #
-                # commit updated files to GitHub repository
-                #
-                cd $OWTRAVIS_BUILD_DIR
-                git add -f .
-                if [ "$OWTRAVISJOB" = "DOCPDF" ]; then
-                    git commit $GITQUIET -m "Travis CI build $TRAVIS_JOB_NUMBER - Documentation"
-                else
-                    git commit $GITQUIET -m "Travis CI build $TRAVIS_JOB_NUMBER - OW distribution"
-                fi
-                git push $GITQUIET -f origin
-                cd $TRAVIS_BUILD_DIR
-                echo_msg="gitupds.sh - done"
-            fi
+            case "$OWTRAVISJOB" in
+                "BUILD")
+                "BUILD1")
+                "BUILD2")
+                "DOCPDF")
+                    #
+                    # setup client info
+                    #
+                    git config --global user.email "travis@travis-ci.org"
+                    git config --global user.name "Travis CI"
+                    git config --global push.default simple
+                    #
+                    # clone GitHub repository
+                    #
+                    git clone $GITQUIET --branch=master https://${GITHUB_TOKEN}@github.com/${OWTRAVIS_REPO_SLUG}.git $OWTRAVIS_BUILD_DIR
+                    #
+                    # copy OW build to git tree
+                    #
+                    export OWRELROOT=$OWTRAVIS_BUILD_DIR
+                    cd $OWSRCDIR
+                    case "$OWTRAVISJOB" in
+                        "DOCPDF")
+                            builder cpdocpdf
+                            ;;
+                        "BUILD")
+                            builder cprel
+                            ;;
+                        "BUILD1")
+                            builder cprel1
+                            ;;
+                        "BUILD2")
+                            builder cprel2
+                            ;;
+                        "BOOTSTRAP")
+                        *)
+                            ;;
+                    esac
+                    #
+                    # commit updated files to GitHub repository
+                    #
+                    cd $OWTRAVIS_BUILD_DIR
+                    git add -f .
+                    case "$OWTRAVISJOB" in
+                        "DOCPDF")
+                            git commit $GITQUIET -m "Travis CI build $TRAVIS_JOB_NUMBER - Documentation"
+                            ;;
+                        "BUILD")
+                        "BUILD1")
+                        "BUILD2")
+                            git commit $GITQUIET -m "Travis CI build $TRAVIS_JOB_NUMBER - OW distribution"
+                            ;;
+                        "BOOTSTRAP")
+                        *)
+                            ;;
+                    esac
+                    git push $GITQUIET -f origin
+                    cd $TRAVIS_BUILD_DIR
+                    echo_msg="gitupds.sh - done"
+                    ;;
+                "BOOTSTRAP")
+                *)
+                    ;;
+            esac
         fi
     fi
 
