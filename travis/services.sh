30cf5c041b02f37e6fa3ff203ac95c47923c5585
Jiri Malak <malak.jiri@gmail.com>
2018-11-27 10:15:42 +0100

    update Travis script update services info

--- travis/services.sh
+++ travis/services.sh
@@ -1,16 +1,21 @@
 # Disable services enabled by default
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    sudo service 
     sudo service postgresql stop
     sudo service mysql stop
 fi
 # Display services informations
 if [ "$OWTRAVIS_DEBUG" = "1" ]; then
     if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+        echo "'sudo initctl list'"
         sudo initctl list
+        echo "'service --status-all'"
         service --status-all
     elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
+        echo "'sudo launchctl list'"
         sudo launchctl list
-        sudo top -l 1
+        echo "'launchctl list'"
+        launchctl list
+        echo "'sudo top -l 1 -o MEM'"
+        sudo top -l 1 -o MEM
     fi
 fi
