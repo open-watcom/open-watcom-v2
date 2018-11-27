23fc1748a4e20c9448601682d021fe5ce20e06bf
Jiri Malak <malak.jiri@gmail.com>
2018-11-27 07:01:28 +0100

    Travis add debug info about services

--- travis/services.sh
+++ travis/services.sh
@@ -1,5 +1,11 @@
 # Disable services enabled by default
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    sudo /etc/init.d/postgresql stop
-    sudo /etc/init.d/mysql stop
+    sudo service 
+    sudo service postgresql stop
+    sudo service mysql stop
+fi
+#list status all services
+if [ "$OWTRAVIS_DEBUG" = "1" ]; then
+    sudo initctl list
+    service --status-all
 fi
