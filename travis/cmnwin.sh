dcd0c6b40502de9492e2d3b11d860f8386b7a9e9
Jiri Malak <malak.jiri@gmail.com>
2018-11-28 13:33:38 +0100

    update travis script

--- travis/cmnwin.sh
+++ travis/cmnwin.sh
@@ -1,7 +1,7 @@
 echo "set" >tmp.bat
 echo "ENV1"
-cmd /c tmp.bat
+cmd "/c call tmp.bat"
 echo "call \"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat\" amd64" >tmp.bat
 echo "set" >>tmp.bat
 echo "ENV2"
-cmd /c tmp.bat
+cmd "/c call tmp.bat"
