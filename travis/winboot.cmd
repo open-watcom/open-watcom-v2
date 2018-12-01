46b4b61c5cead4b90df7e910ed4206a256cf9443
Jiri Malak <malak.jiri@gmail.com>
2018-12-01 02:14:28 +0100

    update Travis

--- travis/winboot.cmd
+++ travis/winboot.cmd
@@ -1,24 +1,18 @@
-@echo on
+@echo off
 SETLOCAL EnableExtensions
 REM Script to build the Open Watcom bootstrap tools
 REM By Microsoft Visual Studio
-REM ...
-REM Remove NMAKE macros wrongly defined by Travis
-set CC=
 REM ...
 set OWROOT=%CD%
 REM ...
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 REM ...
-@echo on
-cd %OWROOT%
 call cmnvars.bat
-@echo on
 REM ...
 if "%OWTRAVIS_DEBUG%" == "1" (
-    echo "INCLUDE=%INCLUDE%"
-    echo "LIB=%LIB%"
-    echo "LIBPATH=%LIBPATH%"
+    echo INCLUDE=%INCLUDE%
+    echo LIB=%LIB%
+    echo LIBPATH=%LIBPATH%
 )
 REM ...
 cd %OWSRCDIR%\wmake
--- travis/winbuild.cmd
+++ travis/winbuild.cmd
@@ -3,33 +3,32 @@ SETLOCAL EnableExtensions
 REM Script to build the Open Watcom bootstrap tools
 REM By Microsoft Visual Studio
 REM ...
-REM Remove NMAKE macros wrongly defined by Travis
-set CC=
-REM ...
 set OWROOT=%CD%
 REM ...
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 REM ...
 @echo on
-cd %OWROOT%
 call cmnvars.bat
+@echo on
+REM
+REM setup DOSBOX
+REM
 set OWDOSBOX=%OWROOT%\travis\dosbox\dosbox.exe
 REM ...
 if "%OWTRAVIS_DEBUG%" == "1" (
-    echo "INCLUDE=%INCLUDE%"
-    echo "LIB=%LIB%"
-    echo "LIBPATH=%LIBPATH%"
+    echo INCLUDE=%INCLUDE%
+    echo LIB=%LIB%
+    echo LIBPATH=%LIBPATH%
 )
 REM ...
-@echo on
 cd %OWSRCDIR%
 if "%OWTRAVISJOB%" == "BUILD" (
     if "%TRAVIS_EVENT_TYPE%" == "pull_request" (
         builder build
     ) else (
         builder -q build
-	    set OWRELROOT=%OWROOT%\test
-	    builder -q cprel
+        set OWRELROOT=%OWROOT%\test
+        builder -q cprel
     )
 )
 if "%OWTRAVISJOB%" == "BUILD-1" (
@@ -37,8 +36,8 @@ if "%OWTRAVISJOB%" == "BUILD-1" (
         builder build1
     ) else (
         builder -q build1
-	    set OWRELROOT=%OWROOT%\test
-	    builder -q cprel1
+        set OWRELROOT=%OWROOT%\test
+        builder -q cprel1
     )
 )
 if "%OWTRAVISJOB%" == "BUILD-2" (
@@ -46,7 +45,7 @@ if "%OWTRAVISJOB%" == "BUILD-2" (
         builder build2
     ) else (
         builder -q build2
-	    set OWRELROOT=%OWROOT%\test
-	    builder -q cprel2
+        set OWRELROOT=%OWROOT%\test
+        builder -q cprel2
     )
 )
