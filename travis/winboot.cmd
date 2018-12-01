f8d1561cd56fc314f6b6878771c32305d927d876
Jiri Malak <malak.jiri@gmail.com>
2018-12-01 01:35:40 +0100

    update Travis

--- travis/winboot.cmd
+++ travis/winboot.cmd
@@ -1,4 +1,4 @@
-@echo on off
+@echo on
 SETLOCAL EnableExtensions
 REM Script to build the Open Watcom bootstrap tools
 REM By Microsoft Visual Studio
@@ -10,33 +10,34 @@ set OWROOT=%CD%
 REM ...
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 REM ...
-@echo on off
+@echo on
 cd %OWROOT%
 call cmnvars.bat
-@echo on off
+@echo on
 REM ...
-if not "%OWTRAVIS_DEBUG%" == "1" goto no_env_info
+if "%OWTRAVIS_DEBUG%" == "1" (
     echo "INCLUDE=%INCLUDE%"
     echo "LIB=%LIB%"
     echo "LIBPATH=%LIBPATH%"
-:no_env_info
+)
 REM ...
 cd %OWSRCDIR%\wmake
 mkdir %OWOBJDIR%
 cd %OWOBJDIR%
 nmake -f ..\nmake clean
 nmake -f ..\nmake
-if errorlevel == 1 goto error_exit
+if not errorlevel == 1 (
     cd %OWSRCDIR%\builder
     mkdir %OWOBJDIR%
     cd %OWOBJDIR%
     %OWBINDIR%\wmake -f ..\binmake clean
     %OWBINDIR%\wmake -f ..\binmake bootstrap=1 builder.exe
-    if errorlevel == 1 goto error_exit
-    cd %OWSRCDIR%
-    if "%TRAVIS_EVENT_TYPE%" == "pull_request" (
-        builder boot
-    ) else (
-        builder -q boot
+    if not errorlevel == 1 (
+        cd %OWSRCDIR%
+        if "%TRAVIS_EVENT_TYPE%" == "pull_request" (
+            builder boot
+        ) else (
+            builder -q boot
+        )
     )
-:error_exit
+)
--- travis/winbuild.cmd
+++ travis/winbuild.cmd
@@ -1,4 +1,4 @@
-@echo on off
+@echo on
 SETLOCAL EnableExtensions
 REM Script to build the Open Watcom bootstrap tools
 REM By Microsoft Visual Studio
@@ -10,20 +10,20 @@ set OWROOT=%CD%
 REM ...
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 REM ...
-@echo on off
+@echo on
 cd %OWROOT%
 call cmnvars.bat
 set OWDOSBOX=%OWROOT%\travis\dosbox\dosbox.exe
 REM ...
-if not "%OWTRAVIS_DEBUG%" == "1" goto no_env_info
+if "%OWTRAVIS_DEBUG%" == "1" (
     echo "INCLUDE=%INCLUDE%"
     echo "LIB=%LIB%"
     echo "LIBPATH=%LIBPATH%"
-:no_env_info
+)
 REM ...
-@echo on off
+@echo on
 cd %OWSRCDIR%
-if not "%OWTRAVISJOB%" == "BUILD" goto no_build
+if "%OWTRAVISJOB%" == "BUILD" (
     if "%TRAVIS_EVENT_TYPE%" == "pull_request" (
         builder build
     ) else (
@@ -31,8 +31,8 @@ if not "%OWTRAVISJOB%" == "BUILD" goto no_build
 	    set OWRELROOT=%OWROOT%\test
 	    builder -q cprel
     )
-:no_build
-if not "%OWTRAVISJOB%" == "BUILD-1" goto no_build1
+)
+if "%OWTRAVISJOB%" == "BUILD-1" (
     if "%TRAVIS_EVENT_TYPE%" == "pull_request" (
         builder build1
     ) else (
@@ -40,8 +40,8 @@ if not "%OWTRAVISJOB%" == "BUILD-1" goto no_build1
 	    set OWRELROOT=%OWROOT%\test
 	    builder -q cprel1
     )
-:no_build1
-if not "%OWTRAVISJOB%" == "BUILD-2" goto no_build2
+)
+if "%OWTRAVISJOB%" == "BUILD-2" (
     if "%TRAVIS_EVENT_TYPE%" == "pull_request" (
         builder build2
     ) else (
@@ -49,4 +49,4 @@ if not "%OWTRAVISJOB%" == "BUILD-2" goto no_build2
 	    set OWRELROOT=%OWROOT%\test
 	    builder -q cprel2
     )
-:no_build2
+)
