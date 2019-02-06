d542f05d273abe6d6bdaad6878870be8c0740ad9
Jiri Malak <malak.jiri@gmail.com>
2019-02-06 11:04:44 +0100

    fix memory leaks

--- bld/commonui/c/ctl3d32.c
+++ bld/commonui/c/ctl3d32.c
@@ -34,15 +34,15 @@
 
 #ifdef __WINDOWS_386__
 
-static HANDLE   dllCtl3d;
+static HANDLE   dllCtl3d = (HANDLE)0;
 
-HINDIR DLL32Ctl3dAutoSubclass;
-HINDIR DLL32Ctl3dSubclassDlg;
-HINDIR DLL32Ctl3dRegister;
-HINDIR DLL32Ctl3dUnregister;
-HINDIR DLL32Ctl3dColorChange;
-HINDIR DLL32Ctl3dSubclassCtl;
-HINDIR DLL32Ctl3dCtlColorEx;
+HINDIR DLL32Ctl3dAutoSubclass = NULL;
+HINDIR DLL32Ctl3dSubclassDlg = NULL;
+HINDIR DLL32Ctl3dRegister = NULL;
+HINDIR DLL32Ctl3dUnregister = NULL;
+HINDIR DLL32Ctl3dColorChange = NULL;
+HINDIR DLL32Ctl3dSubclassCtl = NULL;
+HINDIR DLL32Ctl3dCtlColorEx = NULL;
 
 /*
  * Init32Ctl3d - load CTL3D.DLL and get entry
@@ -58,49 +58,49 @@ BOOL Init32Ctl3d( void )
 
     ptr = GetProcAddress( dllCtl3d, "Ctl3dSubclassDlg" );
     if( ptr == NULL ) {
-        FreeLibrary( dllCtl3d );
+        Fini32Ctl3d();
         return( FALSE );
     }
     DLL32Ctl3dSubclassDlg = GetIndirectFunctionHandle( ptr, INDIR_WORD, INDIR_WORD, INDIR_ENDLIST );
 
     ptr = GetProcAddress( dllCtl3d, "Ctl3dAutoSubclass" );
     if( ptr == NULL ) {
-        FreeLibrary( dllCtl3d );
+        Fini32Ctl3d();
         return( FALSE );
     }
     DLL32Ctl3dAutoSubclass = GetIndirectFunctionHandle( ptr, INDIR_WORD, INDIR_ENDLIST );
 
     ptr = GetProcAddress( dllCtl3d, "Ctl3dRegister" );
     if( ptr == NULL ) {
-        FreeLibrary( dllCtl3d );
+        Fini32Ctl3d();
         return( FALSE );
     }
     DLL32Ctl3dRegister = GetIndirectFunctionHandle( ptr, INDIR_WORD, INDIR_ENDLIST );
 
     ptr = GetProcAddress( dllCtl3d, "Ctl3dUnregister" );
     if( ptr == NULL ) {
-        FreeLibrary( dllCtl3d );
+        Fini32Ctl3d();
         return( FALSE );
     }
     DLL32Ctl3dUnregister = GetIndirectFunctionHandle( ptr, INDIR_WORD, INDIR_ENDLIST );
 
     ptr = GetProcAddress( dllCtl3d, "Ctl3dColorChange" );
     if( ptr == NULL ) {
-        FreeLibrary( dllCtl3d );
+        Fini32Ctl3d();
         return( FALSE );
     }
     DLL32Ctl3dColorChange = GetIndirectFunctionHandle( ptr, INDIR_ENDLIST );
 
     ptr = GetProcAddress( dllCtl3d, "Ctl3dSubclassCtl" );
     if( ptr == NULL ) {
-        FreeLibrary( dllCtl3d );
+        Fini32Ctl3d();
         return( FALSE );
     }
     DLL32Ctl3dSubclassCtl = GetIndirectFunctionHandle( ptr, INDIR_WORD, INDIR_ENDLIST );
 
     ptr = GetProcAddress( dllCtl3d, "Ctl3dCtlColorEx" );
     if( ptr == NULL ) {
-        FreeLibrary( dllCtl3d );
+        Fini32Ctl3d();
         return( FALSE );
     }
     DLL32Ctl3dCtlColorEx = GetIndirectFunctionHandle( ptr, INDIR_WORD, INDIR_WORD, INDIR_DWORD, INDIR_ENDLIST );
@@ -116,7 +116,23 @@ BOOL Init32Ctl3d( void )
 void Fini32Ctl3d( void )
 {
     if( dllCtl3d >= (HANDLE)32 ) {
+        if( DLL32Ctl3dSubclassDlg != NULL )
+            FreeIndirectFunctionHandle( DLL32Ctl3dSubclassDlg );
+        if( DLL32Ctl3dAutoSubclass != NULL )
+            FreeIndirectFunctionHandle( DLL32Ctl3dAutoSubclass );
+        if( DLL32Ctl3dRegister != NULL )
+            FreeIndirectFunctionHandle( DLL32Ctl3dRegister );
+        if( DLL32Ctl3dUnregister != NULL )
+            FreeIndirectFunctionHandle( DLL32Ctl3dUnregister );
+        if( DLL32Ctl3dColorChange != NULL )
+            FreeIndirectFunctionHandle( DLL32Ctl3dColorChange );
+        if( DLL32Ctl3dSubclassCtl != NULL )
+            FreeIndirectFunctionHandle( DLL32Ctl3dSubclassCtl );
+        if( DLL32Ctl3dCtlColorEx != NULL )
+            FreeIndirectFunctionHandle( DLL32Ctl3dCtlColorEx );
+
         FreeLibrary( dllCtl3d );
+        dllCtl3d = (HANDLE)0;
     }
 
 } /* Fini32Ctl3d */
