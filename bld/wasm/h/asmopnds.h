06be8528e585a1bc433bf1dfc4541af3a3034be5
Jiri Malak <malak.jiri@gmail.com>
2019-03-30 08:06:36 +0100

    cleanup wasm code use macro for better portability

--- bld/wasm/h/asmopnds.h
+++ bld/wasm/h/asmopnds.h
@@ -32,18 +32,22 @@
 #ifndef ASMOPNDS_H
 #define ASMOPNDS_H
 
-#if defined( __WATCOMC__ )
+#ifdef _M_I86
+#define ENUMU32(x)      x ## UL
+#else
+#define ENUMU32(x)      x ## U
+#endif
 
-enum operand_type {
-    OP_NONE     = 0,
-    OP_R8       = 0x00000001,
-    OP_R16      = 0x00000002,
-    OP_R32      = 0x00000004,
-    OP_MMX      = 0x00000008,
-    OP_XMM      = 0x00000010,
-    OP_A        = 0x00000020,  // AL, AX, EAX registers
-    OP_C        = 0x00000040,  // CL register
-    OP_D        = 0x00000080,  // DX register
+typedef enum operand_type {
+    OP_NONE     = ENUMU32( 0x00000000 ),
+    OP_R8       = ENUMU32( 0x00000001 ),
+    OP_R16      = ENUMU32( 0x00000002 ),
+    OP_R32      = ENUMU32( 0x00000004 ),
+    OP_MMX      = ENUMU32( 0x00000008 ),
+    OP_XMM      = ENUMU32( 0x00000010 ),
+    OP_A        = ENUMU32( 0x00000020 ),  // AL, AX, EAX registers
+    OP_C        = ENUMU32( 0x00000040 ),  // CL register
+    OP_D        = ENUMU32( 0x00000080 ),  // DX register
 
     OP_AL       = ( OP_A | OP_R8 ),
     OP_AX       = ( OP_A | OP_R16 ),
@@ -54,27 +58,27 @@ enum operand_type {
     OP_R        = ( OP_R8 | OP_R16 | OP_R32 ),
     OP_RMX      = ( OP_MMX | OP_XMM ),
 
-    OP_I8       = 0x00000100,
-    OP_I_1      = 0x00000200,
-    OP_I_3      = 0x00000400,
-    OP_U8       = 0x00000800,
-    OP_I16      = 0x00001000,
-    OP_I32      = 0x00002000,
-    OP_J32      = 0x00004000,
-    OP_J48      = 0x00008000,
+    OP_I8       = ENUMU32( 0x00000100 ),
+    OP_I_1      = ENUMU32( 0x00000200 ),
+    OP_I_3      = ENUMU32( 0x00000400 ),
+    OP_U8       = ENUMU32( 0x00000800 ),
+    OP_I16      = ENUMU32( 0x00001000 ),
+    OP_I32      = ENUMU32( 0x00002000 ),
+    OP_J32      = ENUMU32( 0x00004000 ),
+    OP_J48      = ENUMU32( 0x00008000 ),
     OP_I        = ( OP_I8 | OP_I_1 | OP_I_3 | OP_U8 | OP_I16 | OP_I32 ),
     OP_GE_8     = ( OP_I8 | OP_U8 | OP_I16 | OP_I32 ),
     OP_GE_16    = ( OP_I16 | OP_I32 ),
     OP_GE_U8    = ( OP_U8 | OP_I16 | OP_I32 ),
 
-    OP_M_B      = 0x00010000,
-    OP_M_W      = 0x00020000,
-    OP_M_DW     = 0x00040000,
-    OP_M_FW     = 0x00080000,
-    OP_M_QW     = 0x00100000,
-    OP_M_TB     = 0x00200000,
-    OP_M_OW     = 0x00400000,
-    OP_M_DFT    = 0x00800000,
+    OP_M_B      = ENUMU32( 0x00010000 ),
+    OP_M_W      = ENUMU32( 0x00020000 ),
+    OP_M_DW     = ENUMU32( 0x00040000 ),
+    OP_M_FW     = ENUMU32( 0x00080000 ),
+    OP_M_QW     = ENUMU32( 0x00100000 ),
+    OP_M_TB     = ENUMU32( 0x00200000 ),
+    OP_M_OW     = ENUMU32( 0x00400000 ),
+    OP_M_DFT    = ENUMU32( 0x00800000 ),
 
     OP_M8       = ( OP_M_B | OP_M_DFT ),
     OP_M16      = ( OP_M_W | OP_M_DFT ),
@@ -88,100 +92,22 @@ enum operand_type {
     OP_M16_R16  = ( OP_M_W | OP_M_DFT | OP_R16 ),
     OP_M32_R32  = ( OP_M_DW | OP_M_DFT | OP_R32 ),
 
-    OP_CR       = 0x01000000,
-    OP_DR       = 0x02000000,
-    OP_TR       = 0x04000000,
+    OP_CR       = ENUMU32( 0x01000000 ),
+    OP_DR       = ENUMU32( 0x02000000 ),
+    OP_TR       = ENUMU32( 0x04000000 ),
     OP_SPEC_REG = ( OP_CR | OP_DR | OP_TR ),
 
-    OP_SR2      = 0x08000000,
-    OP_SR3      = 0x10000000,
+    OP_SR2      = ENUMU32( 0x08000000 ),
+    OP_SR3      = ENUMU32( 0x10000000 ),
     OP_SR       = ( OP_SR2 | OP_SR3 ),
 
-    OP_ST       = 0x20000000,
-    OP_ST_REG   = 0x40000000,
+    OP_ST       = ENUMU32( 0x20000000 ),
+    OP_ST_REG   = ENUMU32( 0x40000000 ),
     OP_STI      = ( OP_ST | OP_ST_REG ),
 
-    OP_SPECIAL  = 0x80000000      /* this includes the following cases, most
-                                       of which are used in asmscan */
-};
-
-typedef enum operand_type OPNDTYPE;
-
-#else
-
-#define OP_NONE     0
-#define OP_R8       0x00000001
-#define OP_R16      0x00000002
-#define OP_R32      0x00000004
-#define OP_MMX      0x00000008
-#define OP_XMM      0x00000010
-#define OP_A        0x00000020  // AL AX EAX registers
-#define OP_C        0x00000040  // CL register
-#define OP_D        0x00000080  // DX register
-
-#define OP_AL       ( OP_A | OP_R8 )
-#define OP_AX       ( OP_A | OP_R16 )
-#define OP_EAX      ( OP_A | OP_R32 )
-#define OP_CL       ( OP_C | OP_R8 )
-#define OP_DX       ( OP_D | OP_R16 )
-#define OP_R1632    ( OP_R16 | OP_R32 )
-#define OP_R        ( OP_R8 | OP_R16 | OP_R32 )
-#define OP_RMX      ( OP_MMX | OP_XMM )
-
-#define OP_I8       0x00000100
-#define OP_I_1      0x00000200
-#define OP_I_3      0x00000400
-#define OP_U8       0x00000800
-#define OP_I16      0x00001000
-#define OP_I32      0x00002000
-#define OP_J32      0x00004000
-#define OP_J48      0x00008000
-#define OP_I        ( OP_I8 | OP_I_1 | OP_I_3 | OP_U8 | OP_I16 | OP_I32 )
-#define OP_GE_8     ( OP_I8 | OP_U8 | OP_I16 | OP_I32 )
-#define OP_GE_16    ( OP_I16 | OP_I32 )
-#define OP_GE_U8    ( OP_U8 | OP_I16 | OP_I32 )
-
-#define OP_M_B      0x00010000
-#define OP_M_W      0x00020000
-#define OP_M_DW     0x00040000
-#define OP_M_FW     0x00080000
-#define OP_M_QW     0x00100000
-#define OP_M_TB     0x00200000
-#define OP_M_OW     0x00400000
-#define OP_M_DFT    0x00800000
-
-#define OP_M8       ( OP_M_B | OP_M_DFT )
-#define OP_M16      ( OP_M_W | OP_M_DFT )
-#define OP_M32      ( OP_M_DW | OP_M_DFT )
-#define OP_M64      ( OP_M_QW | OP_M_DFT )
-#define OP_M128     ( OP_M_OW | OP_M_DFT )
-
-#define OP_M        ( OP_M_B | OP_M_W | OP_M_DW | OP_M_DFT )
-#define OP_M_ANY    ( OP_M_B | OP_M_W | OP_M_DW | OP_M_FW | OP_M_QW | OP_M_TB | OP_M_OW | OP_M_DFT )
-#define OP_M8_R8    ( OP_M_B | OP_M_DFT | OP_R8 )
-#define OP_M16_R16  ( OP_M_W | OP_M_DFT | OP_R16 )
-#define OP_M32_R32  ( OP_M_DW | OP_M_DFT | OP_R32 )
-
-#define OP_CR       0x01000000
-#define OP_DR       0x02000000
-#define OP_TR       0x04000000
-#define OP_SPEC_REG ( OP_CR | OP_DR | OP_TR )
-
-#define OP_SR2      0x08000000
-#define OP_SR3      0x10000000
-#define OP_SR       ( OP_SR2 | OP_SR3 )
-
-#define OP_ST       0x20000000
-#define OP_ST_REG   0x40000000
-#define OP_STI      ( OP_ST | OP_ST_REG )
-
-#define OP_SPECIAL  0x80000000      /* this includes the following cases, most
-                                       of which are used in asmscan */
-
-typedef unsigned_32 OPNDTYPE;
-
-#endif
-
+    OP_SPECIAL  = ENUMU32( 0x80000000 )     /* this includes the following cases, most
+                                                of which are used in asmscan */
+} OPNDTYPE;
 
 enum operand3_type {
     OP3_NONE = 0x00,
