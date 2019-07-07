a3c1932c8035057d92b295deb4f491375328ec05
Jiri Malak <malak.jiri@gmail.com>
2019-07-07 19:26:34 +0200

    update Travis cache

---
+++ travis/cache2.sh
@@ -0,0 +1,6 @@
+cp $CP_OPTS $OWSRCDIR/fpuemu/i86/stubi86/*.lib buildx/fpuemu/i86/stubi86/
+cp $CP_OPTS $OWSRCDIR/fpuemu/i86/dosi86/*.lib buildx/fpuemu/i86/dosi86/
+cp $CP_OPTS $OWSRCDIR/fpuemu/i86/os2i86/*.lib buildx/fpuemu/i86/os2i86/
+cp $CP_OPTS $OWSRCDIR/fpuemu/i86/wini86/*.lib buildx/fpuemu/i86/wini86/
+cp $CP_OPTS $OWSRCDIR/fpuemu/386/stub386/*.lib buildx/fpuemu/386/stub386/
+cp $CP_OPTS $OWSRCDIR/fpuemu/386/dos386/*.lib buildx/fpuemu/386/dos386/
