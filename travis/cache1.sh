897582b918fa2a84cf9738fb0490a8896e85ba9d
Jiri Malak <malak.jiri@gmail.com>
2019-07-08 12:43:28 +0200

    use cache absolute path

--- travis/cache1.sh
+++ travis/cache1.sh
@@ -4,37 +4,37 @@
 #
 
 echo "save cache1" >$OWBINDIR/cache1.log
-mkdir -p buildx/hdr/dos >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/hdr/dos buildx/hdr/dos/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/hdr/rdos >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/hdr/rdos buildx/hdr/rdos/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/hdr/linux >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/hdr/linux buildx/hdr/linux/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/os2api/os2286/h >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/os2api/os2286/h buildx/os2api/os2286/h/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/os2api/os2386/h >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/os2api/os2386/h buildx/os2api/os2386/h/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/w16api/wini86 >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/w16api/wini86 buildx/w16api/wini86/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/w32api/nt >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/w32api/nt buildx/w32api/nt/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/os2api/os2286/lib >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/os2api/os2286/lib/*.lib buildx/os2api/os2286/lib/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/os2api/os2386/lib >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/os2api/os2386/lib/*.lib buildx/os2api/os2386/lib/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/w32api/nt386 >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/w32api/nt386/*.lib buildx/w32api/nt386/ >>$OWBINDIR/cache1.log
-#mkdir -p buildx/w32api/ntaxp >>$OWBINDIR/cache1.log
-#cp $CP_OPTS $OWSRCDIR/w32api/ntaxp/*.lib buildx/w32api/ntaxp/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/clib/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/clib/library buildx/clib/library/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/clib/rtdll >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/clib/rtdll buildx/clib/rtdll/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/cpplib/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/cpplib/library buildx/cpplib/library/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/cpplib/rtdll >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/cpplib/rtdll buildx/cpplib/rtdll/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/mathlib/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/mathlib/library buildx/mathlib/library/ >>$OWBINDIR/cache1.log
-mkdir -p buildx/mathlib/rtdll >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/mathlib/rtdll buildx/mathlib/rtdll/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/hdr/dos >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/hdr/dos $OWROOT/buildx/hdr/dos/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/hdr/rdos >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/hdr/rdos $OWROOT/buildx/hdr/rdos/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/hdr/linux >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/hdr/linux $OWROOT/buildx/hdr/linux/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/os2api/os2286/h >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/os2api/os2286/h $OWROOT/buildx/os2api/os2286/h/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/os2api/os2386/h >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/os2api/os2386/h $OWROOT/buildx/os2api/os2386/h/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/w16api/wini86 >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/w16api/wini86 $OWROOT/buildx/w16api/wini86/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/w32api/nt >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/w32api/nt $OWROOT/buildx/w32api/nt/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/os2api/os2286/lib >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/os2api/os2286/lib/*.lib $OWROOT/buildx/os2api/os2286/lib/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/os2api/os2386/lib >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/os2api/os2386/lib/*.lib $OWROOT/buildx/os2api/os2386/lib/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/w32api/nt386 >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/w32api/nt386/*.lib $OWROOT/buildx/w32api/nt386/ >>$OWBINDIR/cache1.log
+#mkdir -p $OWROOT/buildx/w32api/ntaxp >>$OWBINDIR/cache1.log
+#cp $CP_OPTS $OWSRCDIR/w32api/ntaxp/*.lib $OWROOT/buildx/w32api/ntaxp/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/clib/library >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/clib/library $OWROOT/buildx/clib/library/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/clib/rtdll >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/clib/rtdll $OWROOT/buildx/clib/rtdll/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/cpplib/library >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/cpplib/library $OWROOT/buildx/cpplib/library/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/cpplib/rtdll >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/cpplib/rtdll $OWROOT/buildx/cpplib/rtdll/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/mathlib/library >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/mathlib/library $OWROOT/buildx/mathlib/library/ >>$OWBINDIR/cache1.log
+mkdir -p $OWROOT/buildx/mathlib/rtdll >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/mathlib/rtdll $OWROOT/buildx/mathlib/rtdll/ >>$OWBINDIR/cache1.log
--- travis/cache2.sh
+++ travis/cache2.sh
@@ -4,15 +4,15 @@
 #
 
 echo "save cache2" >$OWBINDIR/cache2.log
-mkdir -p buildx/fpuemu/i86/stubi86 >>$OWBINDIR/cache2.log
-cp $CP_OPTS $OWSRCDIR/fpuemu/i86/stubi86/*.lib buildx/fpuemu/i86/stubi86/ >>$OWBINDIR/cache2.log
-mkdir -p buildx/fpuemu/i86/dosi86 >>$OWBINDIR/cache2.log
-cp $CP_OPTS $OWSRCDIR/fpuemu/i86/dosi86/*.lib buildx/fpuemu/i86/dosi86/ >>$OWBINDIR/cache2.log
-mkdir -p buildx/fpuemu/i86/os2i86 >>$OWBINDIR/cache2.log
-cp $CP_OPTS $OWSRCDIR/fpuemu/i86/os2i86/*.lib buildx/fpuemu/i86/os2i86/ >>$OWBINDIR/cache2.log
-mkdir -p buildx/fpuemu/i86/wini86 >>$OWBINDIR/cache2.log
-cp $CP_OPTS $OWSRCDIR/fpuemu/i86/wini86/*.lib buildx/fpuemu/i86/wini86/ >>$OWBINDIR/cache2.log
-mkdir -p buildx/fpuemu/386/stub386 >>$OWBINDIR/cache2.log
-cp $CP_OPTS $OWSRCDIR/fpuemu/386/stub386/*.lib buildx/fpuemu/386/stub386/ >>$OWBINDIR/cache2.log
-mkdir -p buildx/fpuemu/386/dos386 >>$OWBINDIR/cache2.log
-cp $CP_OPTS $OWSRCDIR/fpuemu/386/dos386/*.lib buildx/fpuemu/386/dos386/ >>$OWBINDIR/cache2.log
+mkdir -p $OWROOT/buildx/fpuemu/i86/stubi86 >>$OWBINDIR/cache2.log
+cp $CP_OPTS $OWSRCDIR/fpuemu/i86/stubi86/*.lib $OWROOT/buildx/fpuemu/i86/stubi86/ >>$OWBINDIR/cache2.log
+mkdir -p $OWROOT/buildx/fpuemu/i86/dosi86 >>$OWBINDIR/cache2.log
+cp $CP_OPTS $OWSRCDIR/fpuemu/i86/dosi86/*.lib $OWROOT/buildx/fpuemu/i86/dosi86/ >>$OWBINDIR/cache2.log
+mkdir -p $OWROOT/buildx/fpuemu/i86/os2i86 >>$OWBINDIR/cache2.log
+cp $CP_OPTS $OWSRCDIR/fpuemu/i86/os2i86/*.lib $OWROOT/buildx/fpuemu/i86/os2i86/ >>$OWBINDIR/cache2.log
+mkdir -p $OWROOT/buildx/fpuemu/i86/wini86 >>$OWBINDIR/cache2.log
+cp $CP_OPTS $OWSRCDIR/fpuemu/i86/wini86/*.lib $OWROOT/buildx/fpuemu/i86/wini86/ >>$OWBINDIR/cache2.log
+mkdir -p $OWROOT/buildx/fpuemu/386/stub386 >>$OWBINDIR/cache2.log
+cp $CP_OPTS $OWSRCDIR/fpuemu/386/stub386/*.lib $OWROOT/buildx/fpuemu/386/stub386/ >>$OWBINDIR/cache2.log
+mkdir -p $OWROOT/buildx/fpuemu/386/dos386 >>$OWBINDIR/cache2.log
+cp $CP_OPTS $OWSRCDIR/fpuemu/386/dos386/*.lib $OWROOT/buildx/fpuemu/386/dos386/ >>$OWBINDIR/cache2.log
