0b3a2d8cd7572dc5a522165f1f17fda1bd5878c8
Jiri Malak <malak.jiri@gmail.com>
2019-07-09 07:20:21 +0200

    change cp command for Travis cache

--- travis/cache1.sh
+++ travis/cache1.sh
@@ -5,19 +5,19 @@
 
 echo "save cache1" >$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/hdr/dos >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/hdr/dos $OWROOT/buildx/hdr/dos/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/hdr/dos/. $OWROOT/buildx/hdr/dos/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/hdr/rdos >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/hdr/rdos $OWROOT/buildx/hdr/rdos/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/hdr/rdos/. $OWROOT/buildx/hdr/rdos/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/hdr/linux >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/hdr/linux $OWROOT/buildx/hdr/linux/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/hdr/linux/. $OWROOT/buildx/hdr/linux/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/os2api/os2286/h >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/os2api/os2286/h $OWROOT/buildx/os2api/os2286/h/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/os2api/os2286/h/. $OWROOT/buildx/os2api/os2286/h/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/os2api/os2386/h >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/os2api/os2386/h $OWROOT/buildx/os2api/os2386/h/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/os2api/os2386/h/. $OWROOT/buildx/os2api/os2386/h/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/w16api/wini86 >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/w16api/wini86 $OWROOT/buildx/w16api/wini86/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/w16api/wini86/. $OWROOT/buildx/w16api/wini86/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/w32api/nt >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/w32api/nt $OWROOT/buildx/w32api/nt/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/w32api/nt/. $OWROOT/buildx/w32api/nt/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/os2api/os2286/lib >>$OWBINDIR/cache1.log
 cp $CP_OPTS $OWSRCDIR/os2api/os2286/lib/*.lib $OWROOT/buildx/os2api/os2286/lib/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/os2api/os2386/lib >>$OWBINDIR/cache1.log
@@ -27,14 +27,17 @@ cp $CP_OPTS $OWSRCDIR/w32api/nt386/*.lib $OWROOT/buildx/w32api/nt386/ >>$OWBINDI
 #mkdir -p $OWROOT/buildx/w32api/ntaxp >>$OWBINDIR/cache1.log
 #cp $CP_OPTS $OWSRCDIR/w32api/ntaxp/*.lib $OWROOT/buildx/w32api/ntaxp/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/clib/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/clib/library $OWROOT/buildx/clib/library/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/clib/library/*.lib $OWROOT/buildx/clib/library/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/clib/rtdll >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/clib/rtdll $OWROOT/buildx/clib/rtdll/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/clib/rtdll/*.dll $OWROOT/buildx/clib/rtdll/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/clib/rtdll/*.lib $OWROOT/buildx/clib/rtdll/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/cpplib/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/cpplib/library $OWROOT/buildx/cpplib/library/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/cpplib/library/*.lib $OWROOT/buildx/cpplib/library/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/cpplib/rtdll >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/cpplib/rtdll $OWROOT/buildx/cpplib/rtdll/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/cpplib/rtdll/*.dll $OWROOT/buildx/cpplib/rtdll/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/cpplib/rtdll/*.lib $OWROOT/buildx/cpplib/rtdll/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/mathlib/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/mathlib/library $OWROOT/buildx/mathlib/library/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/mathlib/library/*.lib $OWROOT/buildx/mathlib/library/ >>$OWBINDIR/cache1.log
 mkdir -p $OWROOT/buildx/mathlib/rtdll >>$OWBINDIR/cache1.log
-cp $CP_OPTS $OWSRCDIR/mathlib/rtdll $OWROOT/buildx/mathlib/rtdll/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/mathlib/rtdll/*.dll $OWROOT/buildx/mathlib/rtdll/ >>$OWBINDIR/cache1.log
+cp $CP_OPTS $OWSRCDIR/mathlib/rtdll/*.lib $OWROOT/buildx/mathlib/rtdll/ >>$OWBINDIR/cache1.log
