6eb02bccdd04652791fd8f68087dbeac7aa825ec
Jiri Malak <malak.jiri@gmail.com>
2019-07-11 14:06:39 +0200

    use bash script function for travesal file copy

--- travis/cache1.sh
+++ travis/cache1.sh
@@ -3,6 +3,19 @@
 # Script to fill Travis build cache
 #
 
+copy_tree()
+  for x in $2/*; do
+    if [ -d "$x" ]; then
+      copy_tree $1 "$x" $3
+    else 
+      if [ -f "$x" ] && [ "$x" == $3 ]; then
+        mkdir -p `dirname $1/$x`
+        cp $CP_OPTS "$x" "$1/$x" >>$OWBINDIR/cache1.log
+      fi
+    fi
+  done
+}
+
 echo "save cache1" >$OWBINDIR/cache1.log
 #
 cd $OWSRCDIR/hdr/dos
@@ -42,47 +55,39 @@ mkdir -p $OWROOT/buildx/os2api/os2386/lib >>$OWBINDIR/cache1.log
 cp $CP_OPTS *.lib $OWROOT/buildx/os2api/os2386/lib/ >>$OWBINDIR/cache1.log
 #
 cd $OWSRCDIR/w32api
-mkdir -p $OWROOT/buildx/w32api >>$OWBINDIR/cache1.log
-cp $CP_OPTS */*.lib */*/*.lib $OWROOT/buildx/w32api/ >>$OWBINDIR/cache1.log
+copy_tree . $OWROOT/buildx/w32api *.lib
 #
 cd $OWSRCDIR/clib/library
-mkdir -p $OWROOT/buildx/clib/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS */*/*.lib $OWROOT/buildx/clib/library/ >>$OWBINDIR/cache1.log
+copy_tree . $OWROOT/buildx/clib/library *.lib
 #
 cd $OWSRCDIR/clib/doslfn/library
-mkdir -p $OWROOT/buildx/clib/doslfn/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS */*/*.lib $OWROOT/buildx/clib/doslfn/library/ >>$OWBINDIR/cache1.log
+copy_tree . $OWROOT/buildx/clib/doslfn/library *.lib
 #
 cd $OWSRCDIR/clib/startup/library
-mkdir -p $OWROOT/buildx/clib/startup/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS */*/*.obj $OWROOT/buildx/clib/startup/library/ >>$OWBINDIR/cache1.log
+copy_tree . $OWROOT/buildx/clib/startup/library *.obj
 #
 cd $OWSRCDIR/cpplib/library
-mkdir -p $OWROOT/buildx/cpplib/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS */*/*.lib $OWROOT/buildx/cpplib/library/ >>$OWBINDIR/cache1.log
+copy_tree . $OWROOT/buildx/cpplib/library *.lib
 #
 #cd $OWSRCDIR/cpplib/runtime
-#mkdir -p $OWROOT/buildx/cpplib/runtime >>$OWBINDIR/cache1.log
-#cp $CP_OPTS */*.obj $OWROOT/buildx/cpplib/runtime/ >>$OWBINDIR/cache1.log
+#copy_tree . $OWROOT/buildx/cpplib/runtime *.obj
 #
 #cd $OWSRCDIR/cpplib/iostream
-#mkdir -p $OWROOT/buildx/cpplib/iostream >>$OWBINDIR/cache1.log
-#cp $CP_OPTS */*.obj $OWROOT/buildx/cpplib/iostream/ >>$OWBINDIR/cache1.log
+#copy_tree . $OWROOT/buildx/cpplib/iostream *.obj
 #
 cd $OWSRCDIR/mathlib/library
-mkdir -p $OWROOT/buildx/mathlib/library >>$OWBINDIR/cache1.log
-cp $CP_OPTS */*/*.lib $OWROOT/buildx/mathlib/library/ >>$OWBINDIR/cache1.log
+copy_tree . $OWROOT/buildx/mathlib/library *.lib
 #
 cd $OWSRCDIR/clib/rtdll
-mkdir -p $OWROOT/buildx/clib/rtdll >>$OWBINDIR/cache1.log
-cp $CP_OPTS */*/*.lib */*/*.dll $OWROOT/buildx/clib/rtdll/ >>$OWBINDIR/cache1.log
+copy_tree . $OWROOT/buildx/clib/rtdll *.dll
+copy_tree . $OWROOT/buildx/clib/rtdll *.lib
 #
 cd $OWSRCDIR/cpplib/rtdll
-mkdir -p $OWROOT/buildx/cpplib/rtdll >>$OWBINDIR/cache1.log
-cp $CP_OPTS */*/*.lib */*/*.dll $OWROOT/buildx/cpplib/rtdll/ >>$OWBINDIR/cache1.log
+copy_tree . $OWROOT/buildx/cpplib/rtdll *.dll
+copy_tree . $OWROOT/buildx/cpplib/rtdll *.lib
 #
 cd $OWSRCDIR/mathlib/rtdll
-mkdir -p $OWROOT/buildx/mathlib/rtdll >>$OWBINDIR/cache1.log
-cp $CP_OPTS */*/*.lib */*/*.dll $OWROOT/buildx/mathlib/rtdll/ >>$OWBINDIR/cache1.log
+copy_tree . $OWROOT/buildx/mathlib/rtdll *.dll
+copy_tree . $OWROOT/buildx/mathlib/rtdll *.lib
 #
 cd $TRAVIS_BUILD_DIR
