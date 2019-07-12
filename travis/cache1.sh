814648c89a1097c41100fdf94545443c6cf6586d
Jiri Malak <malak.jiri@gmail.com>
2019-07-12 12:30:54 +0200

    correct parameters for Travis cache scripts

--- travis/cache1.sh
+++ travis/cache1.sh
@@ -4,13 +4,13 @@
 #
 set -x
 
-copy_tree1_obj()
+copy_tree1()
 {
-  for x in $2/*; do
+  for x in $1/*; do
     if [ -d "$x" ]; then
-      copy_tree1_obj $1 "$x"
-    elif [ -f "$x" ] && [ "$x" == "*.obj" ]; then
-      f = $1/$x
+      copy_tree1 "$x" $2 $3
+    elif [ -f "$x" ] && [ "$x" == "$3" ]; then
+      f = $2/$x
       d = `dirname "$f"`
       if [ ! -d "$d" ]; then
         mkdir -p "$d" >>$OWBINDIR/cache1.log
@@ -18,54 +18,26 @@ copy_tree1_obj()
       cp $CP_OPTS "$x" "$f" >>$OWBINDIR/cache1.log
     fi
   done
+}
+
+copy_tree1_obj()
+{
+  copy_tree1 . $1 "*.obj"
 }
 
 copy_tree1_lib()
 {
-  for x in $2/*; do
-    if [ -d "$x" ]; then
-      copy_tree1_lib $1 "$x"
-    elif [ -f "$x" ] && [ "$x" == "*.lib" ]; then
-      f = $1/$x
-      d = `dirname "$f"`
-      if [ ! -d "$d" ]; then
-        mkdir -p "$d" >>$OWBINDIR/cache1.log
-      fi
-      cp $CP_OPTS "$x" "$f" >>$OWBINDIR/cache1.log
-    fi
-  done
+  copy_tree1 . $1 "*.lib"
 }
 
 copy_tree1_dll()
 {
-  for x in $2/*; do
-    if [ -d "$x" ]; then
-      copy_tree1_dll $1 "$x"
-    elif [ -f "$x" ] && [ "$x" == "*.dll" ]; then
-      f = $1/$x
-      d = `dirname "$f"`
-      if [ ! -d "$d" ]; then
-        mkdir -p "$d" >>$OWBINDIR/cache1.log
-      fi
-      cp $CP_OPTS "$x" "$f" >>$OWBINDIR/cache1.log
-    fi
-  done
+  copy_tree1 . $1 "*.dll"
 }
 
 copy_tree1_h()
 {
-  for x in $2/*; do
-    if [ -d "$x" ]; then
-      copy_tree1_h $1 "$x"
-    elif [ -f "$x" ] && [ "$x" == "*.h" ]; then
-      f = $1/$x
-      d = `dirname "$f"`
-      if [ ! -d "$d" ]; then
-        mkdir -p "$d" >>$OWBINDIR/cache1.log
-      fi
-      cp $CP_OPTS "$x" "$f" >>$OWBINDIR/cache1.log
-    fi
-  done
+  copy_tree1 . $1 "*.h"
 }
 
 echo "save cache1" >$OWBINDIR/cache1.log
@@ -91,53 +63,53 @@ mkdir -p $OWROOT/buildx/os2api/os2386/h >>$OWBINDIR/cache1.log
 cp $CP_OPTS . $OWROOT/buildx/os2api/os2386/h/ >>$OWBINDIR/cache1.log
 #
 cd $OWSRCDIR/w16api/wini86
-copy_tree1_h . $OWROOT/buildx/w16api/wini86
-copy_tree1_lib . $OWROOT/buildx/w16api/wini86
+copy_tree1_h $OWROOT/buildx/w16api/wini86
+copy_tree1_lib $OWROOT/buildx/w16api/wini86
 #
 cd $OWSRCDIR/w32api/nt
 mkdir -p $OWROOT/buildx/w32api/nt >>$OWBINDIR/cache1.log
 cp $CP_OPTS . $OWROOT/buildx/w32api/nt/ >>$OWBINDIR/cache1.log
 #
 cd $OWSRCDIR/os2api/os2286/lib
-copy_tree1_lib . $OWROOT/buildx/os2api/os2286/lib
+copy_tree1_lib $OWROOT/buildx/os2api/os2286/lib
 #
 cd $OWSRCDIR/os2api/os2386/lib
-copy_tree1_lib . $OWROOT/buildx/os2api/os2386/lib
+copy_tree1_lib $OWROOT/buildx/os2api/os2386/lib
 #
 cd $OWSRCDIR/w32api
-copy_tree1_lib . $OWROOT/buildx/w32api
+copy_tree1_lib $OWROOT/buildx/w32api
 #
 cd $OWSRCDIR/clib/library
-copy_tree1_lib . $OWROOT/buildx/clib/library
+copy_tree1_lib $OWROOT/buildx/clib/library
 #
 cd $OWSRCDIR/clib/doslfn/library
-copy_tree1_lib . $OWROOT/buildx/clib/doslfn/library
+copy_tree1_lib $OWROOT/buildx/clib/doslfn/library
 #
 cd $OWSRCDIR/clib/startup/library
-copy_tree1_obj . $OWROOT/buildx/clib/startup/library
+copy_tree1_obj $OWROOT/buildx/clib/startup/library
 #
 cd $OWSRCDIR/cpplib/library
-copy_tree1_lib . $OWROOT/buildx/cpplib/library
+copy_tree1_lib $OWROOT/buildx/cpplib/library
 #
 #cd $OWSRCDIR/cpplib/runtime
-#copy_tree1_obj . $OWROOT/buildx/cpplib/runtime
+#copy_tree1_obj $OWROOT/buildx/cpplib/runtime
 #
 #cd $OWSRCDIR/cpplib/iostream
-#copy_tree1_obj . $OWROOT/buildx/cpplib/iostream
+#copy_tree1_obj $OWROOT/buildx/cpplib/iostream
 #
 cd $OWSRCDIR/mathlib/library
-copy_tree1_lib . $OWROOT/buildx/mathlib/library
+copy_tree1_lib $OWROOT/buildx/mathlib/library
 #
 cd $OWSRCDIR/clib/rtdll
-copy_tree1_dll . $OWROOT/buildx/clib/rtdll
-copy_tree1_lib . $OWROOT/buildx/clib/rtdll
+copy_tree1_dll $OWROOT/buildx/clib/rtdll
+copy_tree1_lib $OWROOT/buildx/clib/rtdll
 #
 cd $OWSRCDIR/cpplib/rtdll
-copy_tree1_dll . $OWROOT/buildx/cpplib/rtdll
-copy_tree1_lib . $OWROOT/buildx/cpplib/rtdll
+copy_tree1_dll $OWROOT/buildx/cpplib/rtdll
+copy_tree1_lib $OWROOT/buildx/cpplib/rtdll
 #
 cd $OWSRCDIR/mathlib/rtdll
-copy_tree1_dll . $OWROOT/buildx/mathlib/rtdll
-copy_tree1_lib . $OWROOT/buildx/mathlib/rtdll
+copy_tree1_dll $OWROOT/buildx/mathlib/rtdll
+copy_tree1_lib $OWROOT/buildx/mathlib/rtdll
 #
 cd $TRAVIS_BUILD_DIR
--- travis/cache2.sh
+++ travis/cache2.sh
@@ -4,13 +4,13 @@
 #
 set -x
 
-copy_tree2_lib()
+copy_tree2()
 {
-  for x in $2/*; do
+  for x in $1/*; do
     if [ -d "$x" ]; then
-      copy_tree2_lib $1 "$x"
-    elif [ -f "$x" ] && [ "$x" == "*.lib" ]; then
-      f = $1/$x
+      copy_tree2 "$x" $2 $3
+    elif [ -f "$x" ] && [ "$x" == "$3" ]; then
+      f = $2/$x
       d = `dirname "$f"`
       if [ ! -d "$d" ]; then
         mkdir -p "$d" >>$OWBINDIR/cache2.log
@@ -20,30 +20,35 @@ copy_tree2_lib()
   done
 }
 
+copy_tree2_lib()
+{
+  copy_tree2 . $1 "*.lib"
+}
+
 echo "save cache2" >$OWBINDIR/cache2.log
 #
 cd $OWSRCDIR/fpuemu
-copy_tree2_lib . $OWROOT/buildx/fpuemu
+copy_tree2 $OWROOT/buildx/fpuemu
 #
 cd $OWSRCDIR/wres
-copy_tree2_lib . $OWROOT/buildx/wres
+copy_tree2 $OWROOT/buildx/wres
 #
 cd $OWSRCDIR/orl
-copy_tree2_lib . $OWROOT/buildx/orl
+copy_tree2 $OWROOT/buildx/orl
 #
 cd $OWSRCDIR/owl
-copy_tree2_lib . $OWROOT/buildx/owl
+copy_tree2 $OWROOT/buildx/owl
 #
 cd $OWSRCDIR/dwarf
-copy_tree2_lib . $OWROOT/buildx/dwarf
+copy_tree2 $OWROOT/buildx/dwarf
 #
 cd $OWSRCDIR/cfloat
-copy_tree2_lib . $OWROOT/buildx/cfloat
+copy_tree2 $OWROOT/buildx/cfloat
 #
 cd $OWSRCDIR/commonui
-copy_tree2_lib . $OWROOT/buildx/commonui
+copy_tree2 $OWROOT/buildx/commonui
 #
 cd $OWSRCDIR/wpi
-copy_tree2_lib . $OWROOT/buildx/wpi
+copy_tree2 $OWROOT/buildx/wpi
 #
 cd $TRAVIS_BUILD_DIR
