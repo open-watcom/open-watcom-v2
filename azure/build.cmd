7522439f3a22f54a9b4f51cedbfa4d5e8ebd7bf7
Jiri Malak <malak.jiri@gmail.com>
2019-08-10 14:02:01 +0200

    cleanup azure pipelines scripts

--- azure/build.cmd
+++ azure/build.cmd
@@ -1,5 +1,5 @@
 @set OWECHO=off
-@if "%OWAZURE_DEBUG%" == "1" set OWECHO=on
+@if "%OWDEBUG%" == "1" set OWECHO=on
 @echo %OWECHO%
 SETLOCAL EnableExtensions
 REM Script to build the Open Watcom bootstrap tools
@@ -12,117 +12,61 @@ REM ...
 @echo %OWECHO%
 REM ...
 REM setup DOSBOX
-REM ...
+REM
 set OWDOSBOXPATH=%OWROOT%\travis\dosbox
 set OWDOSBOX=dosbox.exe
 set SDL_VIDEODRIVER=dummy
-REM ...
-set OWGHOSTSCRIPTPATH=%OWROOT%\travis\gs927w64
-set OWWIN95HC=%OWROOT%\travis\hcw\hcrtf.exe
-set OWHHC=%OWROOT%\travis\hhc\hhc.exe
-REM ...
-set OWTOOLS=VISUALC
+REM
+if "%OWAZURE_STAGE_NAME%" == "docs" (
+    set OWGHOSTSCRIPTPATH=%OWROOT%\travis\gs927w64
+    set OWWIN95HC=%OWROOT%\travis\hcw\hcrtf.exe
+    set OWHHC=%OWROOT%\travis\hhc\hhc.exe
+)
 REM ...
 call cmnvars.bat
 REM ...
 @echo %OWECHO%
 REM ...
-mkdir %OWBINDIR%
-REM ...
-@echo INCLUDE="%INCLUDE%"
-@echo LIB="%LIB%"
-@echo LIBPATH="%LIBPATH%"
-REM ...
-set RC=0
-REM ...
-REM Bootstrap pre-build wmake
-REM ...
-cd %OWSRCDIR%
-cd wmake
-mkdir %OWOBJDIR%
-cd %OWOBJDIR%
-nmake -f ..\nmake clean >>%OWBINDIR%\bootx.log 2>&1
-nmake -f ..\nmake >>%OWBINDIR%\bootx.log 2>&1
-cd %OWSRCDIR%
-REM ...
-REM Bootstrap pre-build builder
-REM ...
-if not errorlevel == 1 (
-    cd builder
-    mkdir %OWOBJDIR%
-    cd %OWOBJDIR%
-    %OWBINDIR%\wmake -f ..\binmake clean >>%OWBINDIR%\bootx.log 2>&1
-    %OWBINDIR%\wmake -f ..\binmake bootstrap=1 builder.exe >>%OWBINDIR%\bootx.log 2>&1
-    set RC=%ERRORLEVEL%
-    cd %OWSRCDIR%
-    if not %RC% == 1 (
-REM ...
-REM Bootstrap
-REM ...
-	builder -q boot
-    )
+if "%OWDEBUG%" == "1" (
+    echo INCLUDE="%INCLUDE%"
+    echo LIB="%LIB%"
+    echo LIBPATH="%LIBPATH%"
 )
 REM ...
-REM Build
-REM ...
-builder rel
-REM ...
-REM Update Build
-REM ...
-REM Tests
-REM ...
-set OW1PATH="%PATH%"
-set OW1INCLUDE="%INCLUDE%"
-set OW1LIB="%LIB%"
-set OWTOOLS=WATCOM
-set WATCOM=%OWROOT%\test
-set PATH=%WATCOM%\binnt;%WATCOM%\binw;%PATH%
-set INCLUDE=%WATCOM%\h;%WATCOM%\h\nt
-set LIB=
-REM ...
-cd %OWSRCDIR%\wasmtest
-builder -q -i testclean
-builder -i test
-cat result.log
-cd %OWSRCDIR%\ctest
-builder -q -i testclean
-builder -i test
-cat result.log
-cd %OWSRCDIR%\f77test
-builder -q -i testclean
-builder -i test
-cat result.log
-cd %OWSRCDIR%\plustest
-builder -q -i testclean
-builder -i test
-cat result.log
-cd %OWSRCDIR%\clibtest
-builder -q -i testclean
-builder -i test
-cat result.log
 cd %OWSRCDIR%
-REM ...
-REM Documentation
-REM ...
-set OWTOOLS=VISUALC
-set PATH="%OW1PATH%"
-set INCLUDE="%OW1INCLUDE%"
-set LIB="%OW1LIB%"
-set OW1PATH=
-set OW1INCLUDE=
-set OW1LIB=
-REM ...
-REM register all Help Compilers DLL's
-regsvr32 -u -s itcc.dll
-regsvr32 -s %OWROOT%\travis\hhc\itcc.dll
-REM ...
-builder docs
+if "%OWAZURE_STAGE_NAME%" == "boot" (
+    cd wmake
+    mkdir %OWOBJDIR%
+    cd %OWOBJDIR%
+    nmake -f ..\nmake clean >>%OWBINDIR%\bootx.log 2>&1
+    nmake -f ..\nmake >>%OWBINDIR%\bootx.log 2>&1
+    cd %OWSRCDIR%
+    if not errorlevel == 1 (
+        cd builder
+        mkdir %OWOBJDIR%
+        cd %OWOBJDIR%
+        %OWBINDIR%\wmake -f ..\binmake clean >>%OWBINDIR%\bootx.log 2>&1
+        %OWBINDIR%\wmake -f ..\binmake bootstrap=1 builder.exe >>%OWBINDIR%\bootx.log 2>&1
+	cd %OWSRCDIR%
+        if not errorlevel == 1 (
+	    builder boot
+        )
+    )
+)
+if "%OWAZURE_STAGE_NAME%" == "build" (
+    builder rel
+)
+if "%OWAZURE_STAGE_NAME%" == "docs" (
+    REM register all Help Compilers DLL's
+    regsvr32 -u -s itcc.dll
+    regsvr32 -s %OWROOT%\travis\hhc\itcc.dll
+    builder docs
+)
+if "%OWAZURE_STAGE_NAME%" == "inst" (
+    builder missing
+    builder install os_nt cpu_x64
+)
 set RC=%ERRORLEVEL%
-REM ...
-REM Release
-REM ...
-builder missing
-builder install os_nt cpu_x64
-set RC=%ERRORLEVEL%
-cd %OWROOT%
+REM sleep 3
+ping -n 3 127.0.0.1 >NUL
 exit %RC%
---
+++ azure/build.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+#
+# Script to build the Open Watcom tools on Travis
+# using the host platform's native C/C++ compiler.
+#
+# Expects 
+#   - POSIX tools
+#   - correct setup for all OW build environment variables
+#
+
+bootutil_proc()
+{
+    #
+    # build new verison of wmake for host system
+    #
+    if [ ! -d $OWBINDIR ]; then mkdir $OWBINDIR; fi
+    #
+    cd $OWSRCDIR/wmake
+    mkdir $OWOBJDIR
+    cd $OWOBJDIR
+    rm -f $OWBINDIR/wmake
+    case `uname` in
+        Darwin)
+            make -f ../posmake clean
+            make -f ../posmake TARGETDEF=-D__OSX__
+            ;;
+        *)
+            make -f ../posmake clean
+            make -f ../posmake TARGETDEF=-D__LINUX__
+            ;;
+    esac
+    RC=$?
+    if [ $RC -eq 0 ]; then
+        #
+        # build new verison of builder for host system
+        #
+        cd $OWSRCDIR/builder
+        mkdir $OWOBJDIR
+        cd $OWOBJDIR
+        rm -f $OWBINDIR/builder
+        $OWBINDIR/wmake -f ../binmake clean
+        $OWBINDIR/wmake -f ../binmake bootstrap=1 builder.exe
+        RC=$?
+    fi
+}
+
+build_proc()
+{
+    RC=0
+    cd $OWSRCDIR
+    case "$OWAZURE_STAGE_NAME" in
+        "boot")
+            bootutil_proc
+            if [ $RC -eq 0 ]; then
+                cd $OWSRCDIR
+                builder boot
+                RC=$?
+            fi
+            ;;
+        "build")
+            builder rel
+            RC=$?
+            ;;
+        "docs")
+            export OWVERBOSE=1
+            builder docs
+            RC=$?
+            ;;
+        "instal")
+            export OWVERBOSE=1
+            builder missing
+            builder install
+            RC=$?
+            ;;
+        *)
+            ;;
+    esac
+    cd $OWROOT
+    return $RC
+}
+
+build_proc $*
