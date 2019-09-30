2adc4e43a6b811e7caa3ad113b2c188de507efee
Jiri Malak <malak.jiri@gmail.com>
2019-09-30 07:43:07 +0200

    use new _Pragma() operator to use symbol aliases for graph/pgchart library code and save all extra jumps for FORTRAN symbols

--- bld/graphlib/pgchart/h/pgentry.h
+++ bld/graphlib/pgchart/h/pgentry.h
@@ -32,93 +32,11 @@
 
 // Alternate entry points for WATFOR-77
 
-// Functions are in uppercase, do not have underscores on the end, and
-// perform a jump to the appropriate function.
+// Functions aliases are in uppercase, do not have underscores on the end
 
-
-#if defined( VERSION2 )
-  #define _PGCHART_ALT_ENABLED  0
-#elif defined( _M_I86 )
-  #define _PGCHART_ALT_ENABLED  1
-#else
-  #define _PGCHART_ALT_ENABLED  0       // not necessary for 32-bit
-#endif
-
-#if _PGCHART_ALT_ENABLED
-  #define Entry2( f1, f2 )      extern void f1( void ); void f1( void ) { JMP ## f2(); }
+#if defined( _M_I86 ) && !defined( VERSION2 )
+  #define PRAGMA_STRING2(s)     # s
+  #define Entry2( f1, f2 )      _Pragma( PRAGMA_STRING2( alias( # f1 , f2 ) ) )
 #else
   #define Entry2( f1, f2 )
-#endif
-
-#if _PGCHART_ALT_ENABLED
-
-void JMP_pg_analyzechart();
-void JMP_pg_analyzechartms();
-void JMP_pg_analyzepie();
-void JMP_pg_analyzescatter();
-void JMP_pg_analyzescatterms();
-void JMP_pg_chart();
-void JMP_pg_chartms();
-void JMP_pg_chartpie();
-void JMP_pg_chartscatter();
-void JMP_pg_chartscatterms();
-void JMP_pg_defaultchart();
-void JMP_pg_getchardef();
-void JMP_pg_getpalette();
-void JMP_pg_getstyleset();
-void JMP_pg_hlabelchart();
-void JMP_pg_initchart();
-void JMP_pg_resetpalette();
-void JMP_pg_resetstyleset();
-void JMP_pg_setchardef();
-void JMP_pg_setpalette();
-void JMP_pg_setstyleset();
-void JMP_pg_vlabelchart();
-
-#pragma aux JMP_pg_analyzechart         = 0xe9 __offset _pg_analyzechart;
-#pragma aux JMP_pg_analyzechartms       = 0xe9 __offset _pg_analyzechartms;
-#pragma aux JMP_pg_analyzepie           = 0xe9 __offset _pg_analyzepie;
-#pragma aux JMP_pg_analyzescatter       = 0xe9 __offset _pg_analyzescatter;
-#pragma aux JMP_pg_analyzescatterms     = 0xe9 __offset _pg_analyzescatterms;
-#pragma aux JMP_pg_chart                = 0xe9 __offset _pg_chart;
-#pragma aux JMP_pg_chartms              = 0xe9 __offset _pg_chartms;
-#pragma aux JMP_pg_chartpie             = 0xe9 __offset _pg_chartpie;
-#pragma aux JMP_pg_chartscatter         = 0xe9 __offset _pg_chartscatter;
-#pragma aux JMP_pg_chartscatterms       = 0xe9 __offset _pg_chartscatterms;
-#pragma aux JMP_pg_defaultchart         = 0xe9 __offset _pg_defaultchart;
-#pragma aux JMP_pg_getchardef           = 0xe9 __offset _pg_getchardef;
-#pragma aux JMP_pg_getpalette           = 0xe9 __offset _pg_getpalette;
-#pragma aux JMP_pg_getstyleset          = 0xe9 __offset _pg_getstyleset;
-#pragma aux JMP_pg_hlabelchart          = 0xe9 __offset _pg_hlabelchart;
-#pragma aux JMP_pg_initchart            = 0xe9 __offset _pg_initchart;
-#pragma aux JMP_pg_resetpalette         = 0xe9 __offset _pg_resetpalette;
-#pragma aux JMP_pg_resetstyleset        = 0xe9 __offset _pg_resetstyleset;
-#pragma aux JMP_pg_setchardef           = 0xe9 __offset _pg_setchardef;
-#pragma aux JMP_pg_setpalette           = 0xe9 __offset _pg_setpalette;
-#pragma aux JMP_pg_setstyleset          = 0xe9 __offset _pg_setstyleset;
-#pragma aux JMP_pg_vlabelchart          = 0xe9 __offset _pg_vlabelchart;
-
-#pragma aux _PG_ANALYZECHART "*";
-#pragma aux _PG_ANALYZECHARTMS "*";
-#pragma aux _PG_ANALYZEPIE "*";
-#pragma aux _PG_ANALYZESCATTER "*";
-#pragma aux _PG_ANALYZESCATTERMS "*";
-#pragma aux _PG_CHART "*";
-#pragma aux _PG_CHARTMS "*";
-#pragma aux _PG_CHARTPIE "*";
-#pragma aux _PG_CHARTSCATTER "*";
-#pragma aux _PG_CHARTSCATTERMS "*";
-#pragma aux _PG_DEFAULTCHART "*";
-#pragma aux _PG_GETCHARDEF "*";
-#pragma aux _PG_GETPALETTE "*";
-#pragma aux _PG_GETSTYLESET "*";
-#pragma aux _PG_HLABELCHART "*";
-#pragma aux _PG_INITCHART "*";
-#pragma aux _PG_RESETPALETTE "*";
-#pragma aux _PG_RESETSTYLESET "*";
-#pragma aux _PG_SETCHARDEF "*";
-#pragma aux _PG_SETPALETTE "*";
-#pragma aux _PG_SETSTYLESET "*";
-#pragma aux _PG_VLABELCHART "*";
-
 #endif
