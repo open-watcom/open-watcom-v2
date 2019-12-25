23d06d6c8c2d3a513caff523d8180296dfbc7745
Jiri Malak <malak.jiri@gmail.com>
2019-12-25 08:21:00 +0100

    cleanup copyright text in source files

--- bld/clib/posix/c/ptrwlock.c
+++ bld/clib/posix/c/ptrwlock.c
@@ -2,8 +2,7 @@
 *
 *                            Open Watcom Project
 *
-*    Portions Copyright (c) 2016 Open Watcom Contributors.
-*    All Rights Reserved.
+* Copyright (c) 2016-2019 The Open Watcom Contributors. All Rights Reserved.
 *
 *  ========================================================================
 *
