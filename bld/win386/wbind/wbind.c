ec48da083d5f09ffc010cfac59f3cffda42b61be
Jiri Malak <malak.jiri@gmail.com>
2020-01-04 11:22:28 +0100

    fix mistake

--- bld/win386/wbind/wbind.c
+++ bld/win386/wbind/wbind.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2019 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -44,7 +44,7 @@
 #include "exedos.h"
 #include "exeos2.h"
 #include "exephar.h"
-#include "pathgrp.h"
+#include "pathgrp2.h"
 
 #include "clibext.h"
 
