168ef9d077b4cf87082be459a55e4b7f84765d75
Jiri Malak <malak.jiri@gmail.com>
2020-01-05 03:33:36 +0100

    use PGROUP2 structure for _splitpath2 function parameters

--- bld/wsample/c/sampnlm.c
+++ bld/wsample/c/sampnlm.c
@@ -2,6 +2,7 @@
 *
 *                            Open Watcom Project
 *
+* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -42,6 +43,7 @@
 #include "sample.h"
 #include "smpstuff.h"
 #include "wmsg.h"
+#include "pathgrp2.h"
 
 #include <ownwthrd.h>
 
@@ -304,19 +306,20 @@ int InDOS( void )
 
 void GetProg( char *cmd, char *eoc )
 {
-    char    save;
-    char    volume[_MAX_VOLUME];
-    char    dir[_MAX_DIR];
-    char    fname[_MAX_FNAME];
-    char    ext[_MAX_EXT];
-    char    sfname[_MAX_FNAME];
+    char        save;
+    PGROUP2     pg1;
+    PGROUP2     pg2;
 
     save = *eoc;
     *eoc = '\0';
-    _splitpath( cmd, volume, dir, fname, ext );
+    _splitpath2( cmd, pg1.buffer, NULL, NULL, &pg1.fname, NULL );
     *eoc = save;
-    _splitpath( SampName, volume, dir, sfname, ext );
-    _makepath( SampName, volume, dir, sfname[0] == '\0' ? fname : sfname, ext[0] == '\0' ? "SMP" : ext );
+    _splitpath2( SampName, pg2.buffer, &pg2.drive, &pg2.dir, &pg2.fname, &pg2.ext );
+    if( pg2.fname[0] == '\0' )
+        pg2.fname = pg1.fname;
+    if( pg2.ext[0] == '\0' )
+        pg2.ext = "smp";
+    _makepath( SampName, pg2.drive, pg2.dir, pg2.fname, pg2.ext );
 }
 
 
--- bld/wsample/dump/smpdump.c
+++ bld/wsample/dump/smpdump.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2017-2019 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2017-2020 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -40,6 +40,7 @@
 #include "watcom.h"
 #include "sample.h"
 #include "banner.h"
+#include "pathgrp2.h"
 
 #include "clibext.h"
 
