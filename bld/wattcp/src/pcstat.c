a56e59bbf976df88231b239631d5c775e6dd376d
Jiri Malak <malak.jiri@gmail.com>
2020-02-09 11:29:32 +0100

    use sock_type type

--- bld/wattcp/src/pcstat.c
+++ bld/wattcp/src/pcstat.c
@@ -18,12 +18,12 @@
 #include "pcpkt.h"
 #include "pcstat.h"
 
-int sock_stats (tcp_Socket *s, WORD *days, WORD *inactive,
+int sock_stats (const sock_type *s, WORD *days, WORD *inactive,
                 WORD *cwindow, WORD *avg,  WORD *sd)
 {
   time_t now = time (NULL);
 
-  if (s->ip_type != TCP_PROTO)
+  if (s->u.ip_type != TCP_PROTO)
      return (0);
 
   if (days)     *days     = (WORD) (now / (3600*24) - _watt_start_day);
@@ -33,9 +33,9 @@ int sock_stats (tcp_Socket *s, WORD *days, WORD *inactive,
   if (avg)      *avg      = 0;
   if (sd)       *sd       = 0;
 #else
-  if (cwindow)  *cwindow  = s->cwindow;
-  if (avg)      *avg      = s->vj_sa >> 3;
-  if (sd)       *sd       = s->vj_sd >> 2;
+  if (cwindow)  *cwindow  = s->tcp.cwindow;
+  if (avg)      *avg      = s->tcp.vj_sa >> 3;
+  if (sd)       *sd       = s->tcp.vj_sd >> 2;
 #endif
   return (1);
 }
