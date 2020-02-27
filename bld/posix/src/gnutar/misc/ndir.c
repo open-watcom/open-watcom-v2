1094bf8e1a34bdeccb423e63acf0485514a504d5
Jiri Malak <malak.jiri@gmail.com>
2020-02-27 20:48:10 +0100

    remove register keywords

--- bld/posix/src/gnutar/misc/ndir.c
+++ bld/posix/src/gnutar/misc/ndir.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2019 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -75,8 +75,8 @@ char    *dos_errlst[] = {
 */
 DIR *opendir( char *name )
 {
-    register DIR   *dirp;
-    char           *malloc();
+    DIR     *dirp;
+    char    *malloc();
 
     dirp = (DIR *)malloc( sizeof( DIR ) );
     if( dirp == NULL ) {
