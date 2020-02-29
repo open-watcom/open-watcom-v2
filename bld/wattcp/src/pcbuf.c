74c3a750c3d424d97cc9c1789510069fc6167af1
Jiri Malak <malak.jiri@gmail.com>
2020-02-29 03:27:57 +0100

    cleanup code use default names for symbols if possible

--- bld/wattcp/src/pcbuf.c
+++ bld/wattcp/src/pcbuf.c
@@ -12,81 +12,73 @@
 #include "pcbuf.h"
 
 
-int sock_rbsize (const sock_type *s)
+int sock_rbsize (const sock_type *sk)
 {
-  switch (_chk_socket(s))
-  {
+    switch (_chk_socket(sk)) {
     case VALID_IP:
-         return (sizeof(s->raw.ip) + sizeof(s->raw.data));
+        return (sizeof(sk->raw.ip) + sizeof(sk->raw.data));
     case VALID_UDP:
-         return (s->udp.maxrdatalen);
     case VALID_TCP:
-         return (s->tcp.maxrdatalen);
-  }
-  return (0);
+        return (sk->u.maxrdatalen);
+    }
+    return (0);
 }
 
-int sock_rbused (const sock_type *s)
+int sock_rbused (const sock_type *sk)
 {
-  switch (_chk_socket(s))
-  {
+    switch (_chk_socket(sk)) {
     case VALID_IP:
-         return (s->raw.used ? intel16 (s->raw.ip.length) : 0);
+        return (sk->raw.used ? intel16 (sk->raw.ip.length) : 0);
     case VALID_UDP:
-         return (s->udp.rdatalen);
     case VALID_TCP:
-         return (s->tcp.rdatalen);
-  }
-  return (0);
+        return (sk->u.rdatalen);
+    }
+    return (0);
 }
 
-int sock_rbleft (const sock_type *s)
+int sock_rbleft (const sock_type *sk)
 {
-  switch (_chk_socket(s))
-  {
+    switch (_chk_socket(sk)) {
     case VALID_IP:
-         return (s->raw.used ? 0 : sizeof(s->raw.ip) + sizeof(s->raw.data));
+        return (sk->raw.used ? 0 : sizeof(sk->raw.ip) + sizeof(sk->raw.data));
     case VALID_UDP:
-         return (s->tcp.maxrdatalen - s->udp.rdatalen);
     case VALID_TCP:
-         return (s->tcp.maxrdatalen - s->tcp.rdatalen);
-  }
-  return (0);
+        return (sk->u.maxrdatalen - sk->u.rdatalen);
+    }
+    return (0);
 }
 
-int sock_tbsize (const sock_type *s)
+int sock_tbsize (const sock_type *sk)
 {
-  switch (_chk_socket(s))
-  {
+    switch (_chk_socket(sk)) {
     case VALID_IP:
-         return (mtu);
+        return (mtu);
     case VALID_TCP:
-         return (tcp_MaxTxBufSize);
+        return (tcp_MaxTxBufSize);
     case VALID_UDP:
-         return (mtu - sizeof(in_Header) - sizeof(udp_Header));
-  }
-  return (0);
+        return (mtu - sizeof(in_Header) - sizeof(udp_Header));
+    }
+    return (0);
 }
 
-int sock_tbleft (const sock_type *s)
+int sock_tbleft (const sock_type *sk)
 {
-  switch (_chk_socket(s))
-  {
+    switch (_chk_socket(sk)) {
     case VALID_IP:
-         return (mtu);
+        return (mtu);
     case VALID_TCP:
-         return (tcp_MaxTxBufSize - s->tcp.datalen);
+        return (tcp_MaxTxBufSize - sk->tcp.datalen);
     case VALID_UDP:
-         return (mtu - sizeof(in_Header) - sizeof(udp_Header));
-  }
-  return (0);
+        return (mtu - sizeof(in_Header) - sizeof(udp_Header));
+    }
+    return (0);
 }
 
-int sock_tbused (const sock_type *s)
+int sock_tbused (const sock_type *sk)
 {
-  if (_chk_socket(s) == VALID_TCP)
-     return (s->tcp.datalen);
-  return (0);
+    if (_chk_socket(sk) == VALID_TCP)
+        return (sk->tcp.datalen);
+    return (0);
 }
 
 /*
@@ -94,56 +86,59 @@ int sock_tbused (const sock_type *s)
  *  Should be used only when current buffer is empty,
  *  i.e. sock_rbused() returns 0
  */
-int sock_setbuf (sock_type *s, BYTE *rx_buf, unsigned rx_len)
+int sock_setbuf (sock_type *sk, BYTE *rx_buf, unsigned rx_len)
 {
-  int type = _chk_socket (s);
-
-  if (!type || type == VALID_IP)  /* Raw-sockets use fixed buffer */
-     return (0);
-
-  if (!rx_len || !rx_buf)
-  {
-    s->tcp.rdata       = s->tcp.rddata;
-    s->tcp.maxrdatalen = tcp_MaxBufSize;
-  }
-  else
-  {
-    s->tcp.rdata       = rx_buf;
-    s->tcp.maxrdatalen = min (rx_len, USHRT_MAX);
-    memset (rx_buf, 0, s->tcp.maxrdatalen);
-  }
-  return (s->tcp.maxrdatalen);
+    switch (_chk_socket(sk)) {
+    case VALID_TCP:
+    case VALID_UDP:
+        if (rx_len == 0 || rx_buf == NULL) {
+            sk->u.rdata = sk->u.rddata;
+            if (sk->u.ip_type == UDP_PROTO) {
+                sk->tcp.maxrdatalen = udp_MaxBufSize;
+            } else {
+                sk->tcp.maxrdatalen = tcp_MaxBufSize;
+            }
+        } else {
+            sk->u.rdata       = rx_buf;
+            sk->u.maxrdatalen = min (rx_len, USHRT_MAX);
+            memset (rx_buf, 0, sk->u.maxrdatalen);
+        }
+        return (sk->u.maxrdatalen);
+    }
+    return (0);  /* Raw-sockets use fixed buffer */
 }
 
-int sock_preread (const sock_type *s, BYTE *buf, unsigned len)
+int sock_preread (const sock_type *sk, BYTE *buf, unsigned len)
 {
-  int count, type = _chk_socket (s);
+    int count;
 
-  if (!type || type == VALID_IP)  /* Raw-sockets use fixed buffer */
-     return (0);
-
-  count = s->tcp.rdatalen;
-  if (count < 1)
-     return (count);
-
-  if (count > len)
-      count = len;
-  if (buf)
-     memcpy (buf, s->tcp.rdata, count);
-  return (count);
+    switch (_chk_socket(sk)) {
+    case VALID_TCP:
+    case VALID_UDP:
+        count = sk->u.rdatalen;
+        if (count > 0) {
+            if (count > len)
+                count = len;
+            if (buf != NULL) {
+                memcpy (buf, sk->u.rdata, count);
+            }
+        }
+        return (count);
+    }
+    return (0);  /* Raw-sockets use fixed buffer */
 }
 
 /*
  * chk_socket - determine whether a real socket or not
  */
-int _chk_socket (const sock_type *s)
+int _chk_socket (const sock_type *sk)
 {
-    if (s != NULL) {
-        switch (s->u.ip_type) {
+    if (sk != NULL) {
+        switch (sk->u.ip_type) {
         case UDP_PROTO:
             return (VALID_UDP);
         case TCP_PROTO:
-            if (s->tcp.state <= tcp_StateCLOSED)
+            if (sk->tcp.state <= tcp_StateCLOSED)
                 return (VALID_TCP);
             break;
         case IP_TYPE:
@@ -153,14 +148,14 @@ int _chk_socket (const sock_type *s)
     return (0);
 }
 
-const char *sockerr (const sock_type *s)
+const char *sockerr (const sock_type *sk)
 {
-    if (s != NULL) {
-        switch (s->u.ip_type) {
+    if (sk != NULL) {
+        switch (sk->u.ip_type) {
         case UDP_PROTO:
         case TCP_PROTO:
-            if (s->u.err_msg != NULL && s->u.err_msg[0] != '\0') {
-                return (s->u.err_msg);
+            if (sk->u.err_msg != NULL && sk->u.err_msg[0] != '\0') {
+                return (sk->u.err_msg);
             }
             break;
         }
@@ -168,18 +163,15 @@ const char *sockerr (const sock_type *s)
     return (NULL);
 }
 
-const char *sockstate (const sock_type *s)
+const char *sockstate (const sock_type *sk)
 {
-    switch (_chk_socket(s)) {
+    switch (_chk_socket(sk)) {
     case VALID_IP:
         return (_LANG("Raw IP Socket"));
-
     case VALID_UDP:
         return (_LANG("UDP Socket"));
-
     case VALID_TCP:
-        return (_LANG(tcpState[s->tcp.state]));
-
+        return (_LANG(tcpState[sk->tcp.state]));
     default:
         return (_LANG("Not an active socket"));
     }
