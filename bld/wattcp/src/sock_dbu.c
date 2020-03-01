d66a7dcc0db75283bfb28f6a914cbc9fb7cb43cd
Jiri Malak <malak.jiri@gmail.com>
2020-03-01 22:19:47 +0100

    correct 16-bit versus 32-bit code

--- bld/wattcp/src/sock_dbu.c
+++ bld/wattcp/src/sock_dbu.c
@@ -13,12 +13,12 @@ void sock_debugdump (const sock_type *sk)
     if (sk->u.ip_type == IP_TYPE)
         return;
 
-#if defined(__SMALL__)
-    (*_printf) ("next       %04X\r\n",      sk->next);
+#if (DOSX)
+    (*_printf) ("next       %08lX\r\n",     (DWORD)sk->next);
 #elif defined(__LARGE__)
     (*_printf) ("next       %04X:%04X\r\n", FP_SEG(sk->next), FP_OFF(sk->next));
 #else
-    (*_printf) ("next       %08lX\r\n",     (DWORD)sk->next);
+    (*_printf) ("next       %04X\r\n",      sk->next);
 #endif
 
     (*_printf) ("type       %d\r\n", sk->u.ip_type);
