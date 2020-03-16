646782dbc1a274099f309683762d59c6716015c4
Jiri Malak <malak.jiri@gmail.com>
2020-03-16 08:30:14 +0100

    fix for issue #590

--- bld/ndisasm/stand/c/srcmix.c
+++ bld/ndisasm/stand/c/srcmix.c
@@ -40,6 +40,12 @@
 #include "buffer.h"
 #include "disdwarf.h"
 #include "memfuncs.h"
+#if defined( __UNIX__ ) && defined( __WATCOMC__ )
+  #if ( __WATCOMC__ < 1300 )
+    // fix for OW 1.9
+    #include <limits.h>
+  #endif
+#endif
 #include "pathgrp2.h"
 
 #include "clibext.h"
