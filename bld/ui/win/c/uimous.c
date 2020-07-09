f8db280cac68a24b3c3c4db0e6da4ef6a1ebcb1a
Jiri Malak <malak.jiri@gmail.com>
2020-07-09 23:04:07 +0200

    cleanup ui project code

--- bld/ui/win/c/uimous.c
+++ bld/ui/win/c/uimous.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2018 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -33,7 +33,6 @@
 #include <dos.h>
 #include "uidef.h"
 #include <windows.h>
-#include "uidos.h"
 #include "uimouse.h"
 #include "int33.h"
 #include "uiwin.h"
