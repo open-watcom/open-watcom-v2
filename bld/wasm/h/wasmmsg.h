be652e86ba2e774a13c76d105991bd0ad4f48a02
Jiri Malak <malak.jiri@gmail.com>
2020-08-23 15:12:43 +0200

    correct handling of wasm messages

--- bld/wasm/h/wasmmsg.h
+++ bld/wasm/h/wasmmsg.h
@@ -25,45 +25,11 @@
 *
 *  ========================================================================
 *
-* Description:  Assembler message output interface.
+* Description:  Assembler message definitions
 *
 ****************************************************************************/
 
 
-#ifndef _ASMRCMSG_H_INCLUDED
-#define _ASMRCMSG_H_INCLUDED
+#define MAX_MESSAGE_SIZE    128
 
-#define MSG_LANG_SPACING        13000
-
-    #define MAX_MESSAGE_SIZE    128
-
-    #define MSG_SHARE_BASE      1
-    #define MSG_WOMP_BASE       200
-    #define MSG_WASM_BASE       500
-    #define MSG_USAGE_BASE      900
-
-#if defined( INCL_MSGTEXT )
-
-enum {
-#define pick(c,e,j) c,
-    START_BASE1 = MSG_SHARE_BASE - 1,
-#include "../h/asmshare.msg"
-    MSG_SHARE_LAST,
-    START_BASE2 = MSG_WOMP_BASE - 1,
-#include "../h/womp.msg"
-    MSG_WOMP_LAST,
-    START_BASE3 = MSG_WASM_BASE - 1,
-#include "../h/wasm.msg"
-    MSG_WASM_LAST,
-#undef pick
-};
-
-#else
-
-    #include "msg.gh"
-    #include "wmpmsg.gh"
-    #include "wasmmsg.gh"
-
-#endif
-
-#endif
+#include "wasmmsg.rh"
