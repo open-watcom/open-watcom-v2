open-watcom/owp4v1copy

ChangeList: The linker now produces correct entry points (call gates) for exports from IOPL segments in LX modules. The linker now also produces the correct fixups (internal reference via entry table) if a 32-bit non-IOPL segment references such exports.
Implementation note: This is new functionality which makes it possible
to create LX modules with IOPL segments and call into the exported
IOPL entry points. Unlike LINK386, wlink currently does not create
unnamed entry points when the call targets in IOPL segments aren't
explicitly exported. In 16-bit NE modules, IOPL segments do not require
such special treatment.

[git-p4: depot-paths = "//depot/openwatcom/": change = 38215]

bld/wl/h/impexp.h
@@ -24,8 +24,7 @@
 *
 *  ========================================================================
 *
-* Description:  WHEN YOU FIGURE OUT WHAT THIS FILE DOES, PLEASE
-*               DESCRIBE IT HERE!
+* Description:  Declarations related to exports and imports.
 *
 ****************************************************************************/
 
@@ -43,6 +42,7 @@ typedef struct entry_export {
     unsigned_16             isanonymous : 1;
     unsigned_16             isfree : 1;
     unsigned_16             isprivate : 1;
+    unsigned_16             isiopl : 1;
     symbol *                sym;
     char *                  impname;
     targ_addr               addr;

open-watcom/open-watcom-v2

bld/wl/h/impexp.h
@@ -25,8 +25,7 @@
 *
 *  ========================================================================
 *
-* Description:  WHEN YOU FIGURE OUT WHAT THIS FILE DOES, PLEASE
-*               DESCRIBE IT HERE!
+* Description:  Declarations related to exports and imports.
 *
 ****************************************************************************/
 
@@ -42,6 +41,7 @@ typedef struct entry_export {
     boolbit                 isanonymous : 1;
     boolbit                 isfree      : 1;
     boolbit                 isprivate   : 1;
+    boolbit                 isiopl      : 1;
     symbol                  *sym;
     char                    *impname;
     targ_addr               addr;