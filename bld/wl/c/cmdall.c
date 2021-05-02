a4afb46adc5565927ab7eec1a57f40726c9939e9
Jiri Malak <malak.jiri@gmail.com>
2021-05-02 22:10:22 +0200

    don't create traced symbol as defined (SYM_DEFINED flag)

--- bld/wl/c/cmdall.c
+++ bld/wl/c/cmdall.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2021 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -1070,7 +1070,7 @@ static bool AddSymTrace( void )
 {
     symbol      *sym;
 
-    sym = SymOp( ST_REFERENCE_SYM, Token.this, Token.len );
+    sym = SymOp( ST_CREATE, Token.this, Token.len );
     sym->info |= SYM_TRACE;
     return( true );
 }
