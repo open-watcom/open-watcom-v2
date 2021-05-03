2a46fb3c9867fb5b1dd9794259ec834c5264a4d9
Jiri Malak <malak.jiri@gmail.com>
2021-05-03 09:56:32 +0200

    don't resolve unreferenced and undefined traced symbol

--- bld/wl/c/procfile.c
+++ bld/wl/c/procfile.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2021 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -692,10 +692,13 @@ void ResolveUndefined( void )
             }
         }
         for( sym = HeadSym; sym != NULL; sym = sym->link ) {
-            if( ( (sym->info & SYM_DEFINED) == 0 && !IS_SYM_WEAK_REF( sym )
-                 || (FmtData.type & MK_NOVELL) && IS_SYM_IMPORTED( sym )
-                    && (sym->info & (SYM_REFERENCED | SYM_LOCAL_REF)) )
-                && (sym->info & SYM_IS_ALTDEF) == 0 ) {
+            if( ( (sym->info & SYM_DEFINED) == 0
+                  && ( (sym->info & SYM_REFERENCED) || (sym->info & SYM_TRACE) == 0 )
+                  && !IS_SYM_WEAK_REF( sym )
+                || (FmtData.type & MK_NOVELL)
+                  && IS_SYM_IMPORTED( sym )
+                  && (sym->info & (SYM_REFERENCED | SYM_LOCAL_REF)) )
+              && (sym->info & SYM_IS_ALTDEF) == 0 ) {
                 LibFind( sym->name.u.ptr, (sym->info & SYM_CHECKED) != 0 );
             }
             sym->info |= SYM_CHECKED;
