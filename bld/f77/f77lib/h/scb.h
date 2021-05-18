bbd7484f05634e9e38258ed4b993a5dbe7bf0df8
Jiri Malak <malak.jiri@gmail.com>
2021-05-18 08:00:16 +0200

    separate fortran rt-library and compiler code

--- bld/f77/f77lib/h/scb.h
+++ bld/f77/f77lib/h/scb.h
@@ -2,6 +2,7 @@
 *
 *                            Open Watcom Project
 *
+* Copyright (c) 2002-2021 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -34,9 +35,3 @@ typedef struct string {         //  run-time string control block
     char        PGM *strptr;
     uint        len;
 } string;
-
-typedef struct cstring {        //  compile-time string control block
-    char        *strptr;
-    uint        len;
-    char        data;           // used for CHAR( constant )
-} cstring;
--- bld/f77/wfc/h/dattyp.h
+++ bld/f77/wfc/h/dattyp.h
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2021 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -100,7 +100,7 @@ typedef union ftn_type {
     struct cstring      cstring;
     struct string       string;
     struct arr_desc     arr_desc;
-    void                PGM *pgm_ptr;
+    void                *pgm_ptr;
     struct {                            // we don't want the structure chain
         intstar4        field_offset;   // to destroy the field offset during
         void            *struct_chain;  // down-scan.
