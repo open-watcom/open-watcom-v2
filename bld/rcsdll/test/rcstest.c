03baa4be048c9f3da2e4c15e9182e6a8ed38eca2
Jiri Malak <malak.jiri@gmail.com>
2021-06-10 14:04:44 +0200

    fix rcsdll project build

--- bld/rcsdll/test/rcstest.c
+++ bld/rcsdll/test/rcstest.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2021 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -335,7 +335,7 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
 
 ****************************************************************************/
 
-BOOL WINAPI About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+INT_PTR WINAPI About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     lParam = lParam;
     switch (message) {
