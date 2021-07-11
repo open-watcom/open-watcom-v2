2bc237ff8344cbd542e199ec400b72e157c691f4
Jiri Malak <malak.jiri@gmail.com>
2021-07-11 22:14:04 +0200

    cleanup gui project code

--- bld/gui/win/h/guixhot.h
+++ bld/gui/win/h/guixhot.h
@@ -2,6 +2,7 @@
 *
 *                            Open Watcom Project
 *
+* Copyright (c) 2002-2021 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -35,7 +36,7 @@
 
 typedef struct hotspot_info {
     HBITMAP     bitmap;
-    gui_coord   size;
+    guix_coord  size;
 } hotspot_info;
 
 extern void GUIDrawBitmap( int hot_spot, WPI_PRES hdc,
