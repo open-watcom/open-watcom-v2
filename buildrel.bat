e6e6ca31f301e044160170e57e1e4f674d2529ed
Jiri Malak <malak.jiri@gmail.com>
2021-10-16 16:30:28 +0200

    remove useless scripts

--- buildrel.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Script to build the Open Watcom tools and copy to rel tree
-call build.bat rel
--- buildrel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Script to build the Open Watcom tools and copy build to rel tree
-#
-# Expects POSIX or OW tools.
-
-. ./build.sh rel
