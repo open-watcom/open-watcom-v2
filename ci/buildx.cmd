ec52f58195c62ed457148268b57c9366eb75d7a1
Jiri Malak <malak.jiri@gmail.com>
2021-10-27 18:24:49 +0200

    update

--- ci/buildx.cmd
+++ ci/buildx.cmd
@@ -9,7 +9,7 @@ if "%OWTOOLS%" == "WATCOM" (
 )
 if "%OWTOOLS%" == "VISUALC" (
     if "%OWTOOLSV%" == "VS2017" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-    if "%OWTOOLSV%" == "VS2019" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" amd64
+    if "%OWTOOLSV%" == "VS2019" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
     if "%OWTOOLSV%" == "VS2022" call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" amd64
 )
 REM ...
--- ci/coverity.cmd
+++ ci/coverity.cmd
@@ -1,6 +1,6 @@
 echo on
 if "%OWTOOLSV%" == "VS2017" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-if "%OWTOOLSV%" == "VS2019" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" amd64
+if "%OWTOOLSV%" == "VS2019" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 if "%OWTOOLSV%" == "VS2022" call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
 echo on
