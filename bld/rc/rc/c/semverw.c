24a80438ecdfa9f7b6e8eb00ba4ebd4e02abd20f
Jiri Malak <malak.jiri@gmail.com>
2021-11-30 00:01:10 +0100

    fix for issue #768 change behaviour of OW resource compiler to follow Microsoft resource compiler Version info uses default global Language definition instead of neutral language

--- bld/rc/rc/c/semverw.c
+++ bld/rc/rc/c/semverw.c
@@ -409,11 +409,17 @@ void SemWINWriteVerInfo( WResID *name, ResMemFlags flags, VerFixedInfo *info, Fu
 
         loc.len = SemEndResource( loc.start );
 
+/*
+ * MS resource compiler use global Language definition for Version Info resource
+ * it is reason to comment out following code
+ */
+#if 0
         /* version info resources must be language neutral */
 
         lang.lang = DEF_LANG;
         lang.sublang = DEF_SUBLANG;
         SemWINSetResourceLanguage( &lang, false );
+#endif
         SemAddResourceFree( name, WResIDFromNum( RESOURCE2INT( RT_VERSIONINFO ) ), flags, loc );
     } else {
         RESFREE( name );
