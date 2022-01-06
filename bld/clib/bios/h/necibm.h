bafdb88c4f7d4330994fc8c1f27490b1fb9ab555
Jiri Malak <malak.jiri@gmail.com>
2022-01-06 09:20:05 +0100

    restore PC-98 definitions to necibm.h header file

--- bld/clib/bios/h/necibm.h
+++ bld/clib/bios/h/necibm.h
@@ -2,6 +2,7 @@
 *
 *                            Open Watcom Project
 *
+* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -24,13 +25,13 @@
 *
 *  ========================================================================
 *
-* Description:  BIOS access routines and constants.
+* Description:  BIOS access routines and constants for IBM PC and NEC PC-98.
 *
 ****************************************************************************/
 
 
-#ifndef _BIOSIBM_H_INCLUDED
-#define _BIOSIBM_H_INCLUDED
+#ifndef _NECIBM_H_INCLUDED
+#define _NECIBM_H_INCLUDED
 
 /*** Data structures ***/
 #ifndef _BIOS_H_INCLUDED
@@ -50,11 +51,39 @@ struct _ibm_diskinfo_t {
 #pragma pack(__pop);
 #endif
 
+#ifndef _BIOS98_H_INCLUDED
+struct _nec98_diskinfo_t {
+    unsigned        command;
+    unsigned        drive;
+    unsigned        head;
+    unsigned        cylinder;
+    unsigned        data_len;
+    unsigned        sector_len;
+    unsigned        sector;
+    unsigned        nsectors;
+    void _WCI86FAR *buffer;
+    void _WCI86FAR *result;
+};
+
+struct com_t {
+    unsigned short  baud;
+    unsigned short  mode;
+    unsigned short  command;
+    unsigned short  tx_time;
+    unsigned short  rx_time;
+    unsigned short  size;
+    void _WCI86FAR *buffer;
+};
+#endif
+
 
 /*** Handy macros ***/
 #define _IBM_DISK_READ          2
 #define _IBM_DISK_WRITE         3
 #define _IBM_DISK_VERIFY        4
+#define _NEC98_DISK_READ        0x06
+#define _NEC98_DISK_WRITE       0x05
+#define _NEC98_DISK_VERIFY      0x01
 
 #define _KEYBRD_READ            0
 #define _KEYBRD_READY           1
@@ -63,26 +92,48 @@ struct _ibm_diskinfo_t {
 #define _IBM_PRINTER_WRITE      0
 #define _IBM_PRINTER_INIT       1
 #define _IBM_PRINTER_STATUS     2
+#define _NEC98_PRINTER_WRITE    0x11
+#define _NEC98_PRINTER_INIT     0x10
+#define _NEC98_PRINTER_STATUS   0x12
 
 #define _IBM_COM_INIT           0
 #define _IBM_COM_SEND           1
 #define _IBM_COM_RECEIVE        2
 #define _IBM_COM_STATUS         3
+#define _NEC98_COM_INIT         0x00
+#define _NEC98_COM_SEND         0x03
+#define _NEC98_COM_RECEIVE      0x04
+#define _NEC98_COM_STATUS       0x06
+
+#define _NEC98_COM_DEFAULT_BAUD 0xFF
+#define _NEC98_COM_DEFAULT_MODE 0xFF
+#define _NEC98_COM_DEFAULT_CMD  0
+#define _NEC98_COM_DEFAULT_TIME 0
 
 #define _IBM_COM_CH1            0
 #define _IBM_COM_CH2            1
 #define _IBM_COM_CH3            2
+#define _NEC98_COM_CH1          1
+#define _NEC98_COM_CH2          2
+#define _NEC98_COM_CH3          3
 
 #define _IBM_COM_CHR7           2
 #define _IBM_COM_CHR8           3
+#define _NEC98_COM_CHR7         0x08
+#define _NEC98_COM_CHR8         0x0C
 
 #define _IBM_COM_STOP1          0
 #define _IBM_COM_STOP2          4
+#define _NEC98_COM_STOP1        0x40
+#define _NEC98_COM_STOP2        0xC0
 
 #define _IBM_COM_NOPARITY       0
 #define _IBM_COM_ODDPARITY      8
 #define _IBM_COM_SPACEPARITY    16
 #define _IBM_COM_EVENPARITY     24
+#define _NEC98_COM_NOPARITY     0
+#define _NEC98_COM_ODDPARITY    0x10
+#define _NEC98_COM_EVENPARITY   0x30
 
 #define _IBM_COM_110            0
 #define _IBM_COM_150            32
@@ -92,9 +143,21 @@ struct _ibm_diskinfo_t {
 #define _IBM_COM_2400           160
 #define _IBM_COM_4800           192
 #define _IBM_COM_9600           224
+#define _NEC98_COM_DEFAULT      0xFF
+#define _NEC98_COM_75           0x00
+#define _NEC98_COM_150          0x01
+#define _NEC98_COM_300          0x02
+#define _NEC98_COM_600          0x03
+#define _NEC98_COM_1200         0x04
+#define _NEC98_COM_2400         0x05
+#define _NEC98_COM_4800         0x06
+#define _NEC98_COM_9600         0x07
 
 #define _IBM_TIME_GETCLOCK      0
 #define _IBM_TIME_SETCLOCK      1
+#define _NEC98_TIME_GETCLOCK    0
+#define _NEC98_TIME_SETCLOCK    1
+
 
 
 /*** Function prototypes ***/
@@ -105,5 +168,13 @@ extern _WCRTLINK unsigned short __ibm_bios_memsize( void );
 extern _WCRTLINK unsigned short __ibm_bios_printer( unsigned cmd, unsigned port, unsigned data );
 extern _WCRTLINK unsigned short __ibm_bios_serialcom( unsigned cmd, unsigned port, unsigned data );
 extern _WCRTLINK unsigned short __ibm_bios_timeofday( unsigned cmd, long *timeval );
+
+extern _WCRTLINK unsigned short __nec98_bios_disk( unsigned cmd, struct _nec98_diskinfo_t *diskinfo );
+extern _WCRTLINK unsigned short __nec98_bios_equiplist( void );
+extern _WCRTLINK unsigned short __nec98_bios_keybrd( unsigned cmd, unsigned char *buf );
+extern _WCRTLINK unsigned short __nec98_bios_memsize( void );
+extern _WCRTLINK unsigned short __nec98_bios_printer( unsigned cmd, unsigned char *data );
+extern _WCRTLINK unsigned short __nec98_bios_serialcom( unsigned cmd, unsigned port, struct com_t *data );
+extern _WCRTLINK unsigned short __nec98_bios_timeofday( unsigned cmd, char *timeval );
 
 #endif
