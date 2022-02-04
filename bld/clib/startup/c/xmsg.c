c5ca613c69e0d5845c9a9935ba50eb4a03375bcb
Jiri Malak <malak.jiri@gmail.com>
2022-02-04 09:41:27 +0100

    remove dead code

--- bld/clib/startup/c/xmsg.c
+++ bld/clib/startup/c/xmsg.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -186,9 +186,6 @@ _WCRTLINK _WCNORETURN void __exit_with_msg( char _WCI86FAR *msg, int retcode )
   #endif
     EXIT( retcode );
     // never return
-#endif
-#if defined( __RDOSDEV__ )
-    __asm int 3
 #endif
 }
 
