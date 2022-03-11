d4da36f53ee7fa6faaa2656686f184317ff3a018
Jiri Malak <malak.jiri@gmail.com>
2022-03-11 23:36:35 +0100

    fix for issue #841 fix SIZE/SIZEOF if DUP operator is used

--- bld/wasm/c/asmdata.c
+++ bld/wasm/c/asmdata.c
@@ -665,6 +665,8 @@ static token_idx dup_array( asm_sym *sym, asm_sym *struct_sym, token_idx start_p
                 return( INVALID_IDX );
             }
             count = AsmBuffer[cur_pos].u.value;
+            if( sym != NULL )
+                sym->count = 0;
             cur_pos += 2;
             if( AsmBuffer[cur_pos].class != TC_OP_BRACKET ) {
                 AsmError( SYNTAX_ERROR );
