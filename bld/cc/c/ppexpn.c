dbcf1a41890dcdee6da51066934b9897b6c52657
Jiri Malak <malak.jiri@gmail.com>
2022-08-09 01:28:50 +0200

    fix for issue #941 correct wrong message ID (wrong kind, out of value range)

--- bld/cc/c/ppexpn.c
+++ bld/cc/c/ppexpn.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2020 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -432,7 +432,7 @@ static bool COperand( void )
         }
         break;
     default:
-        CErr2p( WARN_UNDEFD_MACRO_IS_ZERO, Buffer );
+        CErr2p( ERR_UNDEFD_MACRO_IS_ZERO, Buffer );
         I64SetZero( p );
         p.no_sign = 0;
         PushOperandCurLocation( p );
