554cac9989aaf15ff05d1520c401ae1340cbc7e6
Jiri Malak <malak.jiri@gmail.com>
2022-09-03 10:36:23 +0200

    fix uninitialized member scale of score_info structure

--- bld/cg/c/scinfo.c
+++ bld/cg/c/scinfo.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2019 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -343,6 +343,7 @@ bool    ScoreLAInfo( score_info *info, name *op )
         info->offset = 0;
         info->index_reg = NO_INDEX;
         info->base = NULL;
+        info->scale = 0;
         return( true );
     default:
         return( false );
