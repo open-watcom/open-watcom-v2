caa30000f2cbad7e64936d99b6fff82cd99890a9
Jiri Malak <malak.jiri@gmail.com>
2022-11-16 13:16:41 +0100

    temporary fix for issue #998 wlink need review of output system to use strictly IDE API interactive mode should be removed, it is a little bit confusing for such tool, it is archaic functionality

--- bld/wl/c/cmdhelp.c
+++ bld/wl/c/cmdhelp.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2021 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -78,11 +78,11 @@ static void doWriteHelp( int first_msg, int last_msg )
                 break;
             }
             previous_null = true;
-        } else if( previous_null ) {
-            WriteStdOutWithNL( msg_buffer );
+            continue;
+        }
+        WriteStdOutInfo( msg_buffer, INF, NULL );
+        if( previous_null ) {
             previous_null = false;
-        } else {
-            WriteStdOutWithNL( msg_buffer );
         }
     }
 }
