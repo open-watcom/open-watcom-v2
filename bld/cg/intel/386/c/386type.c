92d020407141adbee5b16318a2647c7ef863313b
Jiri Malak <malak.jiri@gmail.com>
2022-12-16 09:03:23 +0100

    reformat source code files

--- bld/cg/intel/386/c/386type.c
+++ bld/cg/intel/386/c/386type.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2018 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -35,12 +35,15 @@
 #include "model.h"
 
 
-// type_def THugeCP= {  TY_HUGE_CODE_PTR,6,      TYPE_POINTER + TYPE_CODE };
-type_def TLongCP= {  TY_LONG_CODE_PTR,6,      TYPE_POINTER + TYPE_CODE };
-type_def TNearCP= {  TY_NEAR_CODE_PTR,4,      TYPE_POINTER + TYPE_CODE };
-type_def THugeP = {  TY_HUGE_POINTER, 6,      TYPE_POINTER };
-type_def TLongP = {  TY_LONG_POINTER, 6,      TYPE_POINTER };
-type_def TNearP = {  TY_NEAR_POINTER, 4,      TYPE_POINTER };
+/*                      refno           length  attributes */
+/*                      ======          ======  ========== */
+
+// type_def THugeCP = { TY_HUGE_CODE_PTR, 6,      TYPE_POINTER + TYPE_CODE };
+type_def TLongCP = { TY_LONG_CODE_PTR,  6,      TYPE_POINTER + TYPE_CODE };
+type_def TNearCP = { TY_NEAR_CODE_PTR,  4,      TYPE_POINTER + TYPE_CODE };
+type_def THugeP  = { TY_HUGE_POINTER,   6,      TYPE_POINTER };
+type_def TLongP  = { TY_LONG_POINTER,   6,      TYPE_POINTER };
+type_def TNearP  = { TY_NEAR_POINTER,   4,      TYPE_POINTER };
 
 void    TargTypeInit( void )
 /**************************/
