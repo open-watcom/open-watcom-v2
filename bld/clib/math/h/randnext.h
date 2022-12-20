b5e2d82d310cfbf3e3acc73e1baa6ca0e1c61536
Jiri Malak <malak.jiri@gmail.com>
2022-12-21 00:40:14 +0100

    add new comment

--- bld/clib/math/h/randnext.h
+++ bld/clib/math/h/randnext.h
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2021 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -31,7 +31,7 @@
 ****************************************************************************/
 
 
-#define _INITRANDNEXT(p)
+#define _INITRANDNEXT(p)                /* initialization for Netware/386 */
 #ifdef __SW_BM
     #define _RANDNEXT       (__THREADDATAPTR->__randnext)
 #else
