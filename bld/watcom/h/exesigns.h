fc1f3f52784f703a9f8509375b0f82f1cc3270ac
Jiri Malak <malak.jiri@gmail.com>
2023-03-06 13:09:27 +0100

    add header file with executable format signatures

---
+++ bld/watcom/h/exesigns.h
@@ -0,0 +1,26 @@
+/****************************************************************************
+*
+*                            Open Watcom Project
+*
+* Copyright (c) 2023      The Open Watcom Contributors. All Rights Reserved.
+*
+*  ========================================================================
+*
+* Description:  Executable format signatures.
+*
+****************************************************************************/
+
+
+#ifndef _EXESIGNS_H_INCLUDED
+#define _EXESIGNS_H_INCLUDED
+
+#define EXESIGN_LE      0x454c
+#define EXESIGN_LX      0x584c
+#define EXESIGN_MQ      0x514d
+#define EXESIGN_MZ      0x5a4d
+#define EXESIGN_NE      0x454e
+#define EXESIGN_PE      0x4550
+#define EXESIGN_PL      0x4c50
+#define EXESIGN_PX      0x5850
+
+#endif
