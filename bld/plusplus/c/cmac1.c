1a4b5513d0f0be1b00b31d84bd44558b394fac48
Jiri Malak <malak.jiri@gmail.com>
2023-06-23 02:02:26 +0200

    fix for issue #1104

--- bld/plusplus/c/cmac1.c
+++ bld/plusplus/c/cmac1.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2023 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -303,6 +303,7 @@ TOKEN SpecialMacro(             // EXECUTE A SPECIAL MACRO
         ConstType = TYP_SINT;
         return( T_CONSTANT );
     case MACRO_FILE:
+        TokenLen = 0;
         p = SrcFileNameCurrent();
         WriteBufferEscStr( &p, false );
         return( T_STRING );
