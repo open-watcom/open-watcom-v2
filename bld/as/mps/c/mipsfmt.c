df069f6490cc7a7c2b9282a35322028f3c8064b2
Jiri Malak <malak.jiri@gmail.com>
2023-07-23 11:14:13 +0200

    correct mistake in MIPS Assembler

--- bld/as/mps/c/mipsfmt.c
+++ bld/as/mps/c/mipsfmt.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2023 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -649,6 +649,7 @@ static int doDelaySlotNOP( uint_32 *buffer )
     int     inc = 0;
 
     if( _DirIsSet( REORDER ) ) {
+        buffer++;
         *buffer = OPCODE_NOP;
         inc = 1;
     }
