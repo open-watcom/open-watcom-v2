09ee2ee26d7a42a7c329c4eed41e28b196ee462e
Jiri Malak <malak.jiri@gmail.com>
2024-01-04 22:31:57 +0100

    fix for issue #1202 change -d option data type to behave similar way as old parser

--- bld/rc/rc/c/param.c
+++ bld/rc/rc/c/param.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2023 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2024 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -623,7 +623,7 @@ static bool scanDefine( OPT_STRING **h )
     char        *m;
     char        *p;
 
-    if( OPT_GET_OPTION( h ) ) {
+    if( OPT_GET_FILE( h ) ) {
         m = (*h)->data;
         p = strchr( m, '=' );
         if( p != NULL ) {
