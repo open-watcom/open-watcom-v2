9f2159c54eb787a73541e31a9ec35b12300fdd1d
Jiri Malak <malak.jiri@gmail.com>
2024-01-22 08:12:20 +0100

    cleanup code

--- bld/fe_misc/c/cmdscan.c
+++ bld/fe_misc/c/cmdscan.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2023 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2024 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -67,7 +67,7 @@ char const *CmdScanAddr(        // RETURN COMMAND-LINE SCAN ADDRESS
 int CmdScanChar(                // SCAN THE NEXT CHARACTER
     void )
 {
-    return( (unsigned char)*cmd.curr_ptr++ );
+    return( *(unsigned char *)cmd.curr_ptr++ );
 }
 
 
