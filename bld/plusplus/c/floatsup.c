802bab9606d94947a1d1ba6a26c20649a49c015f
Jiri Malak <malak.jiri@gmail.com>
2024-04-01 23:44:30 +0200

    plusplus: use new cfloat functions to simplify compiler code

--- bld/plusplus/c/floatsup.c
+++ bld/plusplus/c/floatsup.c
@@ -40,16 +40,7 @@
 #include "floatsup.h"
 
 
-cfstruct                cxxh;
-
-static float_handle     MaxPosFloat;
-static float_handle     MinPosFloat;
-static float_handle     MaxPosDbl;
-static float_handle     MinPosDbl;
-static float_handle     MaxNegFloat;
-static float_handle     MinNegFloat;
-static float_handle     MaxNegDbl;
-static float_handle     MinNegDbl;
+cfstruct            cxxh;
 
 void    FloatSupportInit( void )
 /******************************/
@@ -57,71 +48,35 @@ void    FloatSupportInit( void )
     cxxh.alloc = CMemAlloc;
     cxxh.free = CMemFree;
     CFInit( &cxxh );
-    /*
-     * constants are extracted from ISO C standard 2.2.4.2.2
-     */
-    MaxPosFloat = CFCnvSF( &cxxh, "3.40282347e+38" );
-    MinPosFloat = CFCnvSF( &cxxh, "1.17549435e-38" );
-    MaxPosDbl   = CFCnvSF( &cxxh, "1.7976931348623157e+308" );
-    MinPosDbl   = CFCnvSF( &cxxh, "2.2250738585072014e-308" );
-    MaxNegFloat = CFCnvSF( &cxxh, "-3.40282347e+38" );
-    MinNegFloat = CFCnvSF( &cxxh, "-1.17549435e-38" );
-    MaxNegDbl   = CFCnvSF( &cxxh, "-1.7976931348623157e+308" );
-    MinNegDbl   = CFCnvSF( &cxxh, "-2.2250738585072014e-308" );
 }
 
 void    FloatSupportFini( void )
 /******************************/
 {
-    CFFree( &cxxh, MaxPosFloat );
-    CFFree( &cxxh, MinPosFloat );
-    CFFree( &cxxh, MaxPosDbl );
-    CFFree( &cxxh, MinPosDbl );
-    CFFree( &cxxh, MaxNegFloat );
-    CFFree( &cxxh, MinNegFloat );
-    CFFree( &cxxh, MaxNegDbl );
-    CFFree( &cxxh, MinNegDbl );
-
     CFFini( &cxxh );
 }
 
 static float_handle makeOK( float_handle f )
 /******************************************/
 {
+    if( CFExp( f ) > 0 ) {
+        CErr1( ERR_FLOATING_CONSTANT_OVERFLOW );
+    } else {
+        CErr1( ERR_FLOATING_CONSTANT_UNDERFLOW );
+    }
     CFFree( &cxxh, f );
     f = CFCnvUF( &cxxh, 1 );
     return( f );
 }
 
-
 float_handle CFCheckFloatLimit( float_handle f )
 /**********************************************/
 {
     int sign;
-    bool err;
 
-    err = false;
-    sign = CFTest( f );
-    if( sign > 0 ) {
-        if( CFCompare( f, MaxPosFloat ) > 0 ) {
-            err = true;
-            CErr1( ERR_FLOATING_CONSTANT_OVERFLOW );
-        } else if( CFCompare( f, MinPosFloat ) < 0 ) {
-            err = true;
-            CErr1( ERR_FLOATING_CONSTANT_UNDERFLOW );
-        }
-    } else if( sign < 0 ) {
-        if( CFCompare( f, MaxNegFloat ) < 0 ) {
-            err = true;
-            CErr1( ERR_FLOATING_CONSTANT_OVERFLOW );
-        } else if( CFCompare( f, MinNegFloat ) > 0 ) {
-            err = true;
-            CErr1( ERR_FLOATING_CONSTANT_UNDERFLOW );
-        }
-    }
-    if( err ) {
-        f = makeOK( f );
-    }
+    if( CFIsFloat( f ) )
+        return( f );
+    f = makeOK( f );
     return( f );
 }
 
@@ -129,34 +84,12 @@ float_handle CFCheckDoubleLimit( float_handle f )
 /***********************************************/
 {
     int sign;
-    bool err;
 
-    err = false;
-    sign = CFTest( f );
-    if( sign > 0 ) {
-        if( CFCompare( f, MaxPosDbl ) > 0 ) {
-            err = true;
-            CErr1( ERR_FLOATING_CONSTANT_OVERFLOW );
-        } else if( CFCompare( f, MinPosDbl ) == -1 ) {
-            err = true;
-            CErr1( ERR_FLOATING_CONSTANT_UNDERFLOW );
-
-        }
-    } else if( sign < 0 ) {
-        if( CFCompare( f, MaxNegDbl ) < 0 ) {
-            err = true;
-            CErr1( ERR_FLOATING_CONSTANT_OVERFLOW );
-        } else if( CFCompare( f, MinNegDbl ) > 0 ) {
-            err = true;
-            CErr1( ERR_FLOATING_CONSTANT_UNDERFLOW );
-        }
-    }
-    if( err ) {
-        f = makeOK( f );
-    }
+    if( CFIsDouble( f ) )
+        return( f );
+    f = makeOK( f );
     return( f );
 }
-
 
 target_long CFFloat2Long( float_handle *f )
 /*****************************************/
