e4a92d79e35c81996aeab82be0d243c3ca2d392f
Jiri Malak <malak.jiri@gmail.com>
2024-04-01 11:40:38 +0200

    cfloat: consolidate source files and remove unused files

--- bld/cfloat/c/cfconv.c
+++ bld/cfloat/c/cfconv.c
@@ -34,11 +34,98 @@
 #include <string.h>
 #include <stddef.h>
 #include "cfloati.h"
-#include "machine.h"
 #include "i64.h"
 
 
 #define _HiBitOn( x )   ( ( (x) & 0x80000000 ) != 0 )
+#define I16DIGITS       5
+#define I32DIGITS       10
+#define I64DIGITS       20
+
+static struct STRUCT_cfloat( 4 )     MaxNegI8   = {
+    3,              /* exponent ten */
+    3,              /* mantissa length ten */
+    0,              /* allocation length */
+    -1,             /* negative */
+    { '1','2','8',0 }
+};
+
+static struct STRUCT_cfloat( 4 )     MaxU8   = {
+    3,              /* exponent ten */
+    3,              /* mantissa length ten */
+    0,              /* allocation length */
+    1,              /* positive */
+    { '2','5','5',0 }
+};
+
+static struct STRUCT_cfloat( 6 )     MaxNegI16  = {
+    5,              /* exponent ten */
+    5,              /* mantissa length ten */
+    0,              /* allocation length */
+    -1,             /* negative */
+    { '3','2','7','6','8',0 }
+};
+
+static struct STRUCT_cfloat( 6 )     MaxU16  = {
+    5,              /* exponent ten */
+    5,              /* mantissa length ten */
+    0,              /* allocation length */
+    1,              /* positive */
+    { '6','5','5','3','5',0 }
+};
+
+static struct STRUCT_cfloat( 11 )     MaxNegI32  = {
+    10,             /* exponent ten */
+    10,             /* mantissa length ten */
+    0,              /* allocation length */
+    -1,             /* negative */
+    { '2','1','4','7','4','8','3','6','4','8',0 }
+};
+
+static struct STRUCT_cfloat( 11 )     MaxU32  = {
+    10,             /* exponent ten */
+    10,             /* mantissa length ten */
+    0,              /* allocation length */
+    1,              /* positive */
+    { '4','2','9','4','9','6','7','2','9','5',0 }
+};
+
+static struct STRUCT_cfloat( 20 )     MaxNegI64  = {
+    20,             /* exponent ten */
+    20,             /* mantissa length ten */
+    0,              /* allocation length */
+    -1,             /* negative */
+    { '9','2','2','3','3','7','2','9','3','6','8','5','4','7','7','5','8','0','7',0 }
+};
+
+static struct STRUCT_cfloat( 21 )     MaxU64  = {
+    20,             /* exponent ten */
+    20,             /* mantissa length ten */
+    0,              /* allocation length */
+    1,              /* positive */
+    { '1','8','4','4','6','7','4','4','0','7','3','7','0','9','5','5','1','6','1','5',0 }
+};
+
+/*
+ * this is MaxU32 + 1 - just don't want to calc it at runtime
+ */
+static struct STRUCT_cfloat( 11 )     High64Mult  = {
+    10,             /* exponent ten */
+    10,             /* mantissa length ten */
+    0,              /* allocation length */
+    1,              /* positive */
+    { '4','2','9','4','9','6','7','2','9','6',0 }
+};
+
+#if 0
+static struct STRUCT_cfloat( 4 )      MaxFS = {
+    39,             /* exponent ten */
+    3,              /* mantissa length ten */
+    0,              /* allocation length */
+    1,              /* positive */
+    { '3','3','7',0 }
+};
+#endif
 
 static  signed_64   CFGetDec64( const char *str )
 /***********************************************/
@@ -409,7 +496,7 @@ static  bool CFIsType( cfloat *f, cfloat *maxval )
 bool CFIsI8( cfloat *f )
 /**********************/
 {
-    return( CFIsType( f, (cfloat *)&MaxI8 ) );
+    return( CFIsType( f, (cfloat *)&MaxNegI8 ) );
 }
 
 bool CFIsI16( cfloat *f )
