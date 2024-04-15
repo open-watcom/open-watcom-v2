4769b08376bcc05300f77b58db59b9220cbe474a
Jiri Malak <malak.jiri@gmail.com>
2024-04-15 09:26:04 +0200

    cg: do code more transparent

--- bld/cg/h/score.h
+++ bld/cg/h/score.h
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2023 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2024 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -48,11 +48,15 @@ typedef enum {
 } ins_mod;
 
 typedef enum {
-    #define SCOREBOARD
     #define pick(e,s)       SC_ ## e,
     #include "nclass.h"
+    /*
+     * the next 3 used only by the scoreboarder
+     */
+    pick( N_INITIAL,    0   )
+    pick( N_VOLATILE,   0   )
+    pick( N_ADDRESS,    0   )
     #undef pick
-    #undef SCOREBOARD
 } score_name_class_def;
 
 typedef struct score_reg {
@@ -69,7 +73,7 @@ typedef struct score_reg {
 
 typedef struct score_info {
     int_32                  offset;
-    union  name             *base;      /*  indexed names only */
+    union name              *base;      /*  indexed names only */
     union {
         struct temp_name    *t;
         struct var_name     *v;
