3311a407b8157a79519569a945ced52391f4eeb1
Jiri Malak <malak.jiri@gmail.com>
2024-05-21 13:14:26 +0200

    gui: enable build of sample code for gui project

--- bld/gui/sample/c/pmsample.c
+++ bld/gui/sample/c/pmsample.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2018-2021 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2018-2024 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -31,14 +31,14 @@
 ****************************************************************************/
 
 
-#include "gui.h"
+#include "sample.h"
 #include "guitypes.h"
 
 static  gui_rect        Scale           = { 0, 0, 1000, 1000 };
 
 bool MainWndGUIEventProc( gui_window *wnd, gui_event gui_ev, void *param )
 {
-    /* unused parameters */ (void)gui; (void)gui_ev; (void)param;
+    /* unused parameters */ (void)wnd; (void)gui_ev; (void)param;
 
     return( true );
 }
