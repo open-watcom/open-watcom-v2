68e23eaa73dc171422c202ee9d3020d2c4dbbfdc
Jiri Malak <malak.jiri@gmail.com>
2024-09-24 02:38:24 +0200

    clib: add new macro for directory structures buffer size

--- bld/clib/posix/h/dirstrea.h
+++ bld/clib/posix/h/dirstrea.h
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2015-2016 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2015-2024 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -28,6 +28,9 @@
 * Description:  __dirstream structure
 *
 ****************************************************************************/
+
+
+#define _DIRBUF  8  /*  Max number of dir entries buffered  */
 
 struct __dirstream
 {
