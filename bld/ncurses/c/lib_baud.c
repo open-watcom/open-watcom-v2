6e10d200d553d1964faf473f392037827c2e74f4
SASANO Takayoshi <uaa@uaa.org.uk>
2025-02-05 23:01:02 +0900

    only NetBSD supports <sys/ttydev.h>

--- bld/ncurses/c/lib_baud.c
+++ bld/ncurses/c/lib_baud.c
@@ -46,7 +46,7 @@
  * of the indices up to B115200 fit nicely in a 'short', allowing us to retain
  * ospeed's type for compatibility.
  */
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+#if defined(__NetBSD__)
 #undef B0
 #undef B50
 #undef B75
