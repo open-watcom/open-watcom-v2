163a0fbcaaf8ba72e488a85d79940ca9dd6b66e3
Jiri Malak <malak.jiri@gmail.com>
2025-03-31 19:56:13 +0200

    gui/aui: cleanup build make files and restructure source files

--- bld/aui/h/_aui.h
+++ bld/aui/h/_aui.h
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2024 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2025 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -35,6 +35,7 @@
 
 #include "aui.h"
 #include "aui.rh"
+#include "auimsgs.grh"
 
 #define WNDEVENT( w, e, p )     (w)->info->event( w, e, p )
 
