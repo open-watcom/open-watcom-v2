0f0a73595ee7b82d1b09fa2ab12d0b728454c201
Jiri Malak <malak.jiri@gmail.com>
2025-05-09 18:29:05 +0200

    docs: add prebuild device/driver/font files for new wgml tool

---
+++ docs/gml/owhelp/build.bat
@@ -0,0 +1,11 @@
+..\dos\gendev ..\syslib\mono01.fon
+..\dos\gendev ..\syslib\whelp.pcd
+..\dos\gendev ..\syslib\whelpdrv.pcd
+..\dos\gendev ..\syslib\ps.pcd
+..\dos\gendev ..\syslib\psdrv.pcd
+ren WGMLST.COP wgmlst.cop
+ren MONO01.COP mono01.cop
+ren WHELP.COP whelp.cop
+ren WHELPDRV.COP whelpdrv.cop
+ren PS.COP ps.cop
+ren PSDRV.COP psdrv.cop
