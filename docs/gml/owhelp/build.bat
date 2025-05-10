9d3d98e05bc378b590be0c68ea75593121d96efe
Jiri Malak <malak.jiri@gmail.com>
2025-05-10 09:02:44 +0200

    wgml: add missing PS font for ow documentation build

--- docs/gml/owhelp/build.bat
+++ docs/gml/owhelp/build.bat
@@ -1,4 +1,5 @@
 ..\dos\gendev ..\syslib\mono01.fon
+..\dos\gendev ..\syslib\pstr.fon
 ..\dos\gendev ..\syslib\whelp.pcd
 ..\dos\gendev ..\syslib\whelpdrv.pcd
 ..\dos\gendev ..\syslib\ps.pcd
@@ -9,3 +10,4 @@ ren WHELP.COP whelp.cop
 ren WHELPDRV.COP whelpdrv.cop
 ren PS.COP ps.cop
 ren PSDRV.COP psdrv.cop
+ren PSTR.COP pstr.cop
