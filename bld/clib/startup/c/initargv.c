62a588b7cdd11fced1edf155a83d2864c0803a50
Jiri Malak <malak.jiri@gmail.com>
2025-06-02 00:07:15 +0200

    wlink: use predefined rounding macros

--- bld/clib/startup/c/initargv.c
+++ bld/clib/startup/c/initargv.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2025 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -59,11 +59,11 @@
  * This doesn't work for far pointer's
  */
 #if defined( _M_I86 )
-    #define ALIGN_SIZE( __x ) __ROUND_UP_SIZE( __x, 2 )
+    #define ALIGN_SIZE( __x ) __ROUND_UP_SIZE_WORD( __x )
 #elif defined( _M_IX86 )
-    #define ALIGN_SIZE( __x ) __ROUND_UP_SIZE( __x, 4 )
+    #define ALIGN_SIZE( __x ) __ROUND_UP_SIZE_DWORD( __x )
 #else
-    #define ALIGN_SIZE( __x ) __ROUND_UP_SIZE( __x, 8 )
+    #define ALIGN_SIZE( __x ) __ROUND_UP_SIZE_QWORD( __x )
 #endif
 
 _WCRTDATA static CHAR_TYPE  *__F_NAME(__CmdLine,__wCmdLine);    /* cmdline buffer */
