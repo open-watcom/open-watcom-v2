8e8ca03414080a6f3dcfc6d5cbff4ce7e1416700
Jiri Malak <malak.jiri@gmail.com>
2025-06-26 16:31:57 +0200

    trap: fix type mismatches, improve code portability

--- bld/trap/common/nto/ntotrio.c
+++ bld/trap/common/nto/ntotrio.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2023 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2025 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -86,7 +86,7 @@ int KeyGet( void )
 {
     struct termios  old;
     struct termios  new;
-    char            key;
+    unsigned char   key;
 
     tcgetattr( 0, &old );
     new = old;
