bd86627b13e880bdd451fba2120df5f22e372f1b
Jiri Malak <malak.jiri@gmail.com>
2025-09-03 19:48:45 +0200

    clib: add missing header reference

--- bld/clib/handleio/c/chsizlnx.c
+++ bld/clib/handleio/c/chsizlnx.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2025 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -40,6 +40,7 @@
 #include "iomode.h"
 #include "rtcheck.h"
 #include "thread.h"
+#include "lseek.h"
 
 
 _WCRTLINK int _chsize( int handle, long size )
