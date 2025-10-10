f7a53e57173cd4eafd85c1cd27c6244f29940fac
Jiri Malak <malak.jiri@gmail.com>
2025-10-10 21:32:38 +0200

    watcom: reformat source file

--- bld/watcom/c/i64.c
+++ bld/watcom/c/i64.c
@@ -57,7 +57,9 @@ void U64Neg( const unsigned_64 *a, unsigned_64 *res )
 {
     res->u._32[L] = ~a->u._32[L];
     res->u._32[H] = ~a->u._32[H];
-    if( ++res->u._32[L] == 0 ) ++res->u._32[H];
+    if( ++res->u._32[L] == 0 ) {
+        ++res->u._32[H];
+    }
 }
 
 void U64Add( const unsigned_64 *a, const unsigned_64 *b, unsigned_64 *res )
@@ -66,7 +68,8 @@ void U64Add( const unsigned_64 *a, const unsigned_64 *b, unsigned_64 *res )
 
     res->u._32[H] = a->u._32[H] + b->u._32[H];
     new_value = a->u._32[L] + b->u._32[L];
-    if( new_value < a->u._32[L] ) ++res->u._32[H];
+    if( new_value < a->u._32[L] )
+        ++res->u._32[H];
     res->u._32[L] = new_value;
 }
 
@@ -76,9 +79,13 @@ void U64IncDec( unsigned_64 *a, signed_32 i )
 
     new_value = a->u._32[L] + i;
     if( i >= 0 ) {
-        if( new_value < a->u._32[L] ) ++a->u._32[H];
+        if( new_value < a->u._32[L] ) {
+            ++a->u._32[H];
+        }
     } else {
-        if( new_value > a->u._32[L] ) --a->u._32[H];
+        if( new_value > a->u._32[L] ) {
+            --a->u._32[H];
+        }
     }
     a->u._32[L] = new_value;
 }
@@ -101,8 +108,9 @@ void U64Mul( const unsigned_64 *a, const unsigned_64 *b, unsigned_64 *res )
 
     res->u._32[L] = 0;
     res->u._32[H] = 0;
-    while( tmp_b.u._32[L] != 0 || tmp_b.u._32[H] != 0 ) {
-        if( tmp_b.u._32[L] & 1 ) U64Add( &tmp_a, res, res );
+    while( U64Test( tmp_b ) ) {
+        if( tmp_b.u._32[L] & 1 )
+            U64Add( &tmp_a, res, res );
         U64ShiftL( &tmp_a, 1, &tmp_a );
         U64ShiftR( &tmp_b, 1, &tmp_b );
     }
@@ -116,7 +124,8 @@ void U64Div( const unsigned_64 *a, const unsigned_64 *b,
     int         count;
     unsigned_32 tmp;
 
-    if( a->u._32[H] == 0 && b->u._32[H] == 0 ) {
+    if( a->u._32[H] == 0
+      && b->u._32[H] == 0 ) {
         /* both fit in 32-bit, use hardware divide */
         tmp = a->u._32[L] / b->u._32[L];
         if( rem != NULL ) {
@@ -128,15 +137,17 @@ void U64Div( const unsigned_64 *a, const unsigned_64 *b,
     } else {
         tmp_a = *a;
         tmp_b = *b;
-        div->u._32[L] = 0;
-        div->u._32[H] = 0;
         count = 0;
         for( ;; ) {
-            if( tmp_b.u.sign.v ) break;
-            if( U64Cmp( &tmp_a, &tmp_b ) <= 0 ) break;
+            if( tmp_b.u.sign.v )
+                break;
+            if( U64Cmp( &tmp_a, &tmp_b ) <= 0 )
+                break;
             U64ShiftL( &tmp_b, 1, &tmp_b );
             ++count;
         }
+        div->u._32[L] = 0;
+        div->u._32[H] = 0;
         while( count >= 0 ) {
             U64ShiftL( div, 1, div );
             if( U64Cmp( &tmp_a, &tmp_b ) >= 0 ) {
@@ -146,7 +157,9 @@ void U64Div( const unsigned_64 *a, const unsigned_64 *b,
             U64Shift( &tmp_b, 1, &tmp_b );
             --count;
         }
-        if( rem != NULL ) *rem = tmp_a;
+        if( rem != NULL ) {
+            *rem = tmp_a;
+        }
     }
 }
 
@@ -160,34 +173,39 @@ void I64Div( const signed_64 *a, const signed_64 *b,
 
     neg = 0;
     remneg = 0;
-    if( a->u.sign.v ){
+    if( a->u.sign.v ) {
         U64Neg( a, &tmp_a );
         neg ^= 1;
         remneg = 1;
-    }else{
+    } else {
         tmp_a = *a;
     }
-    if( b->u.sign.v ){
+    if( b->u.sign.v ) {
         U64Neg( b, &tmp_b );
         neg ^= 1;
-    }else{
+    } else {
         tmp_b = *b;
     }
     U64Div( &tmp_a, &tmp_b, div, rem );
-    if( neg ){
+    if( neg ) {
         U64Neg( div, div );
     }
-    if( remneg && (rem != NULL) ){
+    if( remneg
+      && (rem != NULL) ) {
         U64Neg( rem, rem );
     }
 }
 
 int U64Cmp( const unsigned_64 *a, const unsigned_64 *b )
 {
-    if( a->u._32[H] > b->u._32[H] ) return( +1 );
-    if( a->u._32[H] < b->u._32[H] ) return( -1 );
-    if( a->u._32[L] > b->u._32[L] ) return( +1 );
-    if( a->u._32[L] < b->u._32[L] ) return( -1 );
+    if( a->u._32[H] > b->u._32[H] )
+        return( +1 );
+    if( a->u._32[H] < b->u._32[H] )
+        return( -1 );
+    if( a->u._32[L] > b->u._32[L] )
+        return( +1 );
+    if( a->u._32[L] < b->u._32[L] )
+        return( -1 );
     return( 0 );
 }
 
@@ -198,10 +216,14 @@ int I64Cmp( const signed_64 *a, const signed_64 *b )
 
     ah = a->u._32[H];
     bh = b->u._32[H];
-    if( ah > bh ) return( +1 );
-    if( ah < bh ) return( -1 );
-    if( a->u._32[L] > b->u._32[L] ) return( +1 );
-    if( a->u._32[L] < b->u._32[L] ) return( -1 );
+    if( ah > bh )
+        return( +1 );
+    if( ah < bh )
+        return( -1 );
+    if( a->u._32[L] > b->u._32[L] )
+        return( +1 );
+    if( a->u._32[L] < b->u._32[L] )
+        return( -1 );
     return( 0 );
 }
 
@@ -211,9 +233,12 @@ int _I64Test( const signed_64 *a )
 
     ah = a->u._32[H];
 
-    if( ah > 0 ) return( +1 );
-    if( ah < 0 ) return( -1 );
-    if( a->u._32[L] > 0 ) return( +1 );
+    if( ah > 0 )
+        return( +1 );
+    if( ah < 0 )
+        return( -1 );
+    if( a->u._32[L] > 0 )
+        return( +1 );
     return( 0 );
 }
 
@@ -224,14 +249,14 @@ void I64ShiftR( const signed_64 *a, unsigned shift, signed_64 *res )
     signed_32         tmp;
 
     shift &= 0x3f;
-    if( shift < 32 ){
+    if( shift < 32 ) {
         tmp = a->u._32[H];
         save = tmp;
         save <<= 1;  // incase shift == 0
         res->u._32[H] = tmp >> shift;          //arithmetic shift
         res->u._32[L] = a->u._32[L] >> shift;
-        res->u._32[L] |= save << (31-shift);
-    }else{
+        res->u._32[L] |= save << (31 - shift);
+    } else {
         shift -= 32;
         tmp = a->u._32[H];
         res->u._32[H] = (tmp < 0) ? -1L : 0L;//sign extend
@@ -244,16 +269,16 @@ void U64ShiftR( const unsigned_64 *a, unsigned shift, unsigned_64 *res )
     unsigned_32       save;
 
     shift &= 0x3f;
-    if( shift < 32 ){
+    if( shift < 32 ) {
         save = a->u._32[H];
         save <<= 1;  // incase shift == 0
         res->u._32[H] = a->u._32[H] >> shift;
         res->u._32[L] = a->u._32[L] >> shift;
-        res->u._32[L] |= save << (31-shift);
-    }else{
+        res->u._32[L] |= save << (31 - shift);
+    } else {
         shift -= 32;
         save = a->u._32[H];
-        res->u._32[H] = 0L;//vacate
+        res->u._32[H] = 0L; // vacate
         res->u._32[L] = save >> shift;
     }
 }
@@ -263,16 +288,16 @@ void U64ShiftL( const unsigned_64 *a, unsigned shift, unsigned_64 *res )
     unsigned_32       save;
 
     shift &= 0x3f;
-    if( shift < 32 ){
+    if( shift < 32 ) {
         save = a->u._32[L];
         save >>= 1;  // incase shift == 0
         res->u._32[L] = a->u._32[L] << shift;
         res->u._32[H] = a->u._32[H] << shift;
-        res->u._32[H] |= save >> (31-shift);
-    }else{
+        res->u._32[H] |= save >> (31 - shift);
+    } else {
         shift -= 32;
         save = a->u._32[L];
-        res->u._32[L] = 0L;//vacate
+        res->u._32[L] = 0L; // vacate
         res->u._32[H] = save << shift;
     }
 }
@@ -291,7 +316,10 @@ void U64Shift( const unsigned_64 *a, int shift, unsigned_64 *res )
 
 #ifdef _U64_C_ROUTINES
 int U64Cnv10( unsigned_64 *res, char c )
-{ //res = res*10+c res source/dest c value <= 9
+/***************************************
+ * res = res * 10 + c res source/dest c value <= 9
+ */
+{
     unsigned_64 tmp8;
     unsigned_64 tmp2;
     unsigned_32 save;
@@ -300,38 +328,38 @@ int U64Cnv10( unsigned_64 *res, char c )
     tmp8 = *res;
     tmp2 = tmp8;
     over = 0;
-    if( tmp8.u._32[H] & 0xE0000000 ){ // shift by 3 will overflow
+    if( tmp8.u._32[H] & 0xE0000000 ) { // shift by 3 will overflow
         over = 1;
     }
-    if( tmp8.u._32[H] & 0x80000000 ){ // shift by 1 will overflow
+    if( tmp8.u._32[H] & 0x80000000 ) { // shift by 1 will overflow
         over = 1;
     }
     save = tmp8.u._32[L];
     tmp8.u._32[L] <<= 3;
     tmp8.u._32[H] <<= 3;
-    tmp8.u._32[H] |= save >> (32-3);
+    tmp8.u._32[H] |= save >> (32 - 3);
     save = tmp2.u._32[L];
     tmp2.u._32[L] <<= 1;
     tmp2.u._32[H] <<= 1;
-    tmp2.u._32[H] |= save >> (32-1);
+    tmp2.u._32[H] |= save >> (32 - 1);
     save = tmp8.u._32[L] + tmp2.u._32[L];
     tmp2.u._32[L] = save;
-    if( save < tmp8.u._32[L] ){ // add carry;
+    if( save < tmp8.u._32[L] ) { // add carry;
         ++tmp8.u._32[H];
-        if( tmp8.u._32[H]  == 0 ){
+        if( tmp8.u._32[H] == 0 ) {
             over = 1;
         }
     }
     save = tmp8.u._32[H] + tmp2.u._32[H];
     tmp2.u._32[H] = save;
-    if( save < tmp8.u._32[H] ){
+    if( save < tmp8.u._32[H] ) {
         over = 1;
     }
     save = tmp2.u._32[L] + c;
     tmp2.u._32[L] = save;
-    if( save < (unsigned)c ){ // add carry;
+    if( save < (unsigned)c ) { // add carry;
         ++tmp2.u._32[H];
-        if( tmp2.u._32[H]  == 0 ){
+        if( tmp2.u._32[H] == 0 ) {
             over = 1;
         }
     }
@@ -340,20 +368,23 @@ int U64Cnv10( unsigned_64 *res, char c )
 }
 
 int U64Cnv8( unsigned_64 *res, char c )
-{ //res = res*8+c res source/dest c value <= 7
+/*
+ * res = res * 8 + c res source/dest c value <= 7
+ */
+{
     unsigned_64 tmp8;
     unsigned_32 save;
     int         over;
 
     over = 0;
     tmp8 = *res;
-    if( tmp8.u._32[H] & 0xE0000000 ){ // shift by 3 will overflow
+    if( tmp8.u._32[H] & 0xE0000000 ) { // shift by 3 will overflow
         over = 1;
     }
     save = tmp8.u._32[L];
     tmp8.u._32[L] <<= 3;
     tmp8.u._32[H] <<= 3;
-    tmp8.u._32[H] |= save >> (32-3);
+    tmp8.u._32[H] |= save >> (32 - 3);
     tmp8.u._32[L] |= c;
     *res = tmp8;
     return( over );
@@ -368,13 +399,13 @@ int U64Cnv2( unsigned_64 *res, char c )
 
     over = 0;
     tmp2 = *res;
-    if( tmp2.u._32[H] & 0x80000000 ){ // shift by 1 will overflow
+    if( tmp2.u._32[H] & 0x80000000 ) { // shift by 1 will overflow
         over = 1;
     }
     save = tmp2.u._32[L];
     tmp2.u._32[L] <<= 1;
     tmp2.u._32[H] <<= 1;
-    tmp2.u._32[H] |= save >> (32-1);
+    tmp2.u._32[H] |= save >> (32 - 1);
     tmp2.u._32[L] |= c;
     *res = tmp2;
     return( over );
@@ -388,13 +419,13 @@ int U64Cnv16( unsigned_64 *res, char c )
 
     over = 0;
     tmp16 = *res;
-    if( tmp16.u._32[H] & 0xF0000000 ){ // shift by 4 will overflow
+    if( tmp16.u._32[H] & 0xF0000000 ) { // shift by 4 will overflow
         over = 1;
     }
     save = tmp16.u._32[L];
     tmp16.u._32[L] <<= 4;
     tmp16.u._32[H] <<= 4;
-    tmp16.u._32[H] |= save >> (32-4);
+    tmp16.u._32[H] |= save >> (32 - 4);
     tmp16.u._32[L] |= c;
     *res = tmp16;
     return( over );
