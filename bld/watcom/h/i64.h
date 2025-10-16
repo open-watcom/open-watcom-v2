bfada6e0597ee09f9a5d28661204d5feb7ab2a05
Jiri Malak <malak.jiri@gmail.com>
2025-10-17 00:01:55 +0200

    watcom: correct mistake in 64-bit conversion macros

--- bld/watcom/h/i64.h
+++ bld/watcom/h/i64.h
@@ -166,11 +166,11 @@ int  U64Cnv16( unsigned_64 *res, char c );
 #define I8FetchTrunc( x )       ((signed_8)(x).u._8[I64LO8])
 
 #define U64ConvU32( x )         ((x).u._32[I64HI32]=0)
-#define U64ConvI32( x )         ((x).u._32[I64HI32]=((signed_32)(x).u._32[I64LO32])?-1:0)
-#define U64ConvU16( x )         ((x).u._32[I64HI32]=(x).u._16[I64MI16W1]=0)
-#define U64ConvI16( x )         ((x).u._32[I64MI16W1]=(x).u._32[I64HI32]=((signed_16)(x).u._32[I64MI16W0])?-1:0)
-#define U64ConvU8( x )          ((x).u._32[I64HI32]=(x).u._16[I64MI16W1]=(x).u._16[I64MI8B1]=0)
-#define U64ConvI8( x )          ((x).u._32[I64MI8B1]=(x).u._32[I64MI16W1]=(x).u._32[I64HI32]=((signed_8)(x).u._32[I64MI8B0])?-1:0)
+#define U64ConvI32( x )         ((x).u._32[I64HI32]=((signed_32)(x).u._32[I64LO32]<0)?-1:0)
+#define U64ConvU16( x )         ((x).u._16[I64MI16W1]=(x).u._32[I64HI32]=0)
+#define U64ConvI16( x )         ((x).u._16[I64MI16W1]=(x).u._32[I64HI32]=((signed_16)(x).u._16[I64MI16W0]<0)?-1:0)
+#define U64ConvU8( x )          ((x).u._8[I64MI8B1]=(x).u._16[I64MI16W1]=(x).u._32[I64HI32]=0)
+#define U64ConvI8( x )          ((x).u._8[I64MI8B1]=(x).u._16[I64MI16W1]=(x).u._32[I64HI32]=((signed_8)(x).u._8[I64MI8B0]<0)?-1:0)
 
 /* Note about the FetchNative macros: These assume that the value is stored
  * in memory as a non-64bit type, starting at the lowest address. That is,
