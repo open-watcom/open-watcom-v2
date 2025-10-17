87001c45d6979986d5b55e0d52b9a861adb84e19
Jiri Malak <malak.jiri@gmail.com>
2025-10-17 13:50:05 +0200

    wl-ms2wlink: fix symbol case sensitive/non-sensitive linking handling
    In the past OW linker was changed to use case sensitive symbol linking instead of non-sensitive, but it was not propagate to ms2wlink tool
    now ms2wlink behave as before wlink change
    all regression test were fixed too

--- bld/wl/ms2wlink/c/keyword.c
+++ bld/wl/ms2wlink/c/keyword.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2022 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2025 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -1386,7 +1386,7 @@ static void ProcNoIgnoreCase( const char *arg )
 {
     /* unused parameters */ (void)arg;
 
-    AddOption( "caseexact" );
+    NoIgnoreCase = true;
 }
 
 static void ProcNoLogo( const char *arg )
