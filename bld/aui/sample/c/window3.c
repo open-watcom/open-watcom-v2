7ff6b097312154b4a5e69a83207104486737a3e7
Jiri Malak <malak.jiri@gmail.com>
2025-10-21 01:11:13 +0200

    aui: add comment about UTF-8 encoding

--- bld/aui/sample/c/window3.c
+++ bld/aui/sample/c/window3.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2024 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2025 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -27,6 +27,8 @@
 *
 * Description:  WHEN YOU FIGURE OUT WHAT THIS MODULE DOES, PLEASE
 *               DESCRIBE IT HERE!
+*
+* UTF-8 encoding, ¥
 *
 ****************************************************************************/
 
