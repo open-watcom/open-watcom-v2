2871b453cb9732b3a79323c678f9c77980e9be05
Jiri Malak <malak.jiri@gmail.com>
2025-10-27 19:25:43 +0100

    wmake: remove problematic code
    the MAX_RESOURCE_SIZE is always smaller then _MAX_PATH

--- bld/wmake/c/mupdate.c
+++ bld/wmake/c/mupdate.c
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2024 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2025 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -207,7 +207,7 @@ STATIC RET_T carryOut( TARGET *targ, CLIST *clist, time_t max_time )
     CLIST               *err;
     int                 i;
     struct utimbuf      times;
-    char                msg[max( MAX_RESOURCE_SIZE, _MAX_PATH )];
+    char                msg[_MAX_PATH];
 
     assert( targ != NULL
         && clist != NULL );
