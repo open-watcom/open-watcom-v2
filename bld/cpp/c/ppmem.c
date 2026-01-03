308e2d2c5931d2b6f22de2823937fa49c78ec73b
Jiří Malák <malak.jiri@gmail.com>
2026-01-03 23:47:42 +0100

    cpp: correct type mismatch (#1565)

--- bld/cpp/c/ppmem.c
+++ bld/cpp/c/ppmem.c
@@ -2,6 +2,7 @@
 *
 *                            Open Watcom Project
 *
+* Copyright (c) 2026      The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -52,7 +53,7 @@ void PPENTRY PP_Free( void *p )
     free( p );
 }
 
-void PPENTRY PP_OutOfMemory()
+void PPENTRY PP_OutOfMemory( void )
 {
     printf( "Out of memory\n" );
     exit( 1 );
