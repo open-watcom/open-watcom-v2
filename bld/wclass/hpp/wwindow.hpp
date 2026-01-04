9c9a83233775b907bdb3c949129aa2ad54d374ec
Jiří Malák <malak.jiri@gmail.com>
2026-01-04 19:29:20 +0100

    wclass: use transparent cast (#1567)

--- bld/wclass/hpp/wwindow.hpp
+++ bld/wclass/hpp/wwindow.hpp
@@ -2,7 +2,7 @@
 *
 *                            Open Watcom Project
 *
-* Copyright (c) 2002-2024 The Open Watcom Contributors. All Rights Reserved.
+* Copyright (c) 2002-2026 The Open Watcom Contributors. All Rights Reserved.
 *    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
 *
 *  ========================================================================
@@ -314,7 +314,7 @@ public:
         GUIWndDirty( _handle );
     }
     void WEXPORT invalidateRow( int row ) {
-        GUIWndDirtyRow( _handle, row );
+        GUIWndDirtyRow( _handle, (gui_text_ord)row );
     }
     void WEXPORT setSystemFont( bool fixed ) {
         GUISetSystemFont( _handle, fixed );
