326dd8d95cb85bab4dc35b7f3334d6a904ea4434
Jiri Malak <malak.jiri@gmail.com>
2026-02-10 20:26:55 +0100

    as: use local memory tracking routines instead of default cover functions

--- bld/as/c/asmem.c
+++ bld/as/c/asmem.c
@@ -33,59 +33,121 @@
 
 #include "as.h"
 #ifdef _STANDALONE_
-#include "trmemcvr.h"
-#include "wresmem.h"
+    #include "wresmem.h"
+  #if defined( TRMEM )
+    #include "trmem.h"
+  #endif
 #else
-#include "asalloc.h"
+    #include "asalloc.h"
 #endif
+
+
+#if defined( _STANDALONE_ ) && defined( TRMEM )
+
+static _trmem_hdl   memHandle;
+static FILE         *memFile;       /* file handle we'll write() to */
+
+static void memPrintLine( void *file, const char *buf, size_t len )
+/*****************************************************************/
+{
+    /* unused parameters */ (void)file; (void)len;
+
+    fprintf( stderr, "***%s\n", buf );
+    if( memFile != NULL ) {
+        fprintf( memFile, "%s\n", buf );
+    }
+}
+
+#endif /* _STANDALONE_ && TRMEM */
 
 #ifdef _STANDALONE_
 void MemInit( void )
 //******************
 {
-    TRMemOpen();
+  #ifdef TRMEM
+    memFile = fopen( "mem.trk", "w" );
+    memHandle = _trmem_open( malloc, free, realloc, NULL, NULL, memPrintLine, _TRMEM_ALL );
+    if( memHandle == NULL ) {
+        exit( EXIT_FAILURE );
+    }
+  #endif
 }
 #endif
 
+#if defined( TRMEM ) && defined( _M_IX86 )
+#pragma aux (WFRM) MemAlloc
+#endif
 pointer MemAlloc( size_t size )
 //*****************************
 {
 #ifdef _STANDALONE_
-    return( TRMemAlloc( size ) );
+  #ifdef TRMEM
+    return( _trmem_alloc( size, _trmem_guess_who(), memHandle ) );
+  #else
+    return( malloc( size ) );
+  #endif
 #else
     return( AsmAlloc( size ) );
 #endif
 }
 
 #ifdef _STANDALONE_
+#if defined( TRMEM ) && defined( _M_IX86 )
+#pragma aux (WFRM) wres_alloc
+#endif
 void *wres_alloc( size_t size )
 {
-    return( TRMemAlloc( size ) );
+  #ifdef TRMEM
+    return( _trmem_alloc( size, _trmem_guess_who(), memHandle ) );
+  #else
+    return( malloc( size ) );
+  #endif
 }
 #endif
 
 #ifdef _STANDALONE_
+#if defined( TRMEM ) && defined( _M_IX86 )
+#pragma aux (WFRM) MemRealloc
+#endif
 pointer MemRealloc( pointer ptr, size_t size )
 //********************************************
 {
-    return( TRMemRealloc( ptr, size ) );
+  #ifdef TRMEM
+    return( _trmem_realloc( ptr, size, _trmem_guess_who(), memHandle ) );
+  #else
+    return( realloc( ptr, size ) );
+  #endif
 }
 #endif
 
+#if defined( TRMEM ) && defined( _M_IX86 )
+#pragma aux (WFRM) MemFree
+#endif
 void MemFree( pointer ptr )
 //*************************
 {
 #ifdef _STANDALONE_
-    TRMemFree( ptr );
+  #ifdef TRMEM
+    _trmem_free( ptr, _trmem_guess_who(), memHandle );
+  #else
+    free( ptr );
+  #endif
 #else
     AsmFree( ptr );
 #endif
 }
 
 #ifdef _STANDALONE_
+#if defined( TRMEM ) && defined( _M_IX86 )
+#pragma aux (WFRM) wres_free
+#endif
 void wres_free( void *ptr )
 {
-    TRMemFree( ptr );
+  #ifdef TRMEM
+    _trmem_free( ptr, _trmem_guess_who(), memHandle );
+  #else
+    free( ptr );
+  #endif
 }
 #endif
 
@@ -93,9 +155,16 @@ void wres_free( void *ptr )
 void MemFini( void )
 //******************
 {
-#ifdef TRMEM
-    TRMemPrtList();
-#endif
-    TRMemClose();
+  #ifdef TRMEM
+    if( memHandle != NULL ) {
+        _trmem_prt_list_ex( memHandle, 100 );
+        _trmem_close( memHandle );
+        if( memFile != NULL ) {
+            fclose( memFile );
+            memFile = NULL;
+        }
+        memHandle = NULL;
+    }
+  #endif
 }
 #endif
