9c78e3e9322113257587fc760838353f65d1c538
Jiri Malak <malak.jiri@gmail.com>
2026-03-13 00:20:37 +0100

    wv: remove useless type casts

--- bld/wv/rfx/rfx.c
+++ bld/wv/rfx/rfx.c
@@ -1300,7 +1300,7 @@ static dir_handle      *DirOpenf( const char *fspec, object_loc fnloc )
     error_handle    errh;
     file_parse      parse;
 
-    dh = (dir_handle *)MemAlloc( sizeof( dir_handle ) );
+    dh = MemAlloc( sizeof( dir_handle ) );
     if( dh == NULL ) {
         NoMemory();
         /* never return */
