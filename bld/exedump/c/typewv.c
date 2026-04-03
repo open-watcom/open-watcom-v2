e7ca917107d003d255cedd01f53b991ca007800b
Cam Sinclair <csinkers@gmail.com>
2026-04-03 14:12:58 +1000

    exedump: fix incorrect 'high bound' value for arrays with >256 elements (watcom debug format)

--- bld/exedump/c/typewv.c
+++ bld/exedump/c/typewv.c
@@ -175,7 +175,13 @@ static void array_index( unsigned_8 *ptr, unsigned_8 size )
 /*********************************************************/
 {
     Wdputs( "          high bound = " );
-    Puthex( *ptr, 2*size );
+    switch (size)
+    {
+    case 1: Puthex( *ptr, 2 ); break;
+    case 2: Puthex( *(unsigned_16*)ptr, 4 ); break;
+    default: Puthex( *(unsigned_32*)ptr, 8 ); break;
+    }
+
     base_type_index( ptr+size );
 }
 
