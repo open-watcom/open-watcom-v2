e724d4a0e7cc1593ae2430e07a62352b7852dd68
Jiri Malak <malak.jiri@gmail.com>
2026-04-11 11:34:26 +0200

    ui: add source code OW header text

--- bld/ui/nov/h/uinlm.h
+++ bld/ui/nov/h/uinlm.h
@@ -1,3 +1,39 @@
+/****************************************************************************
+*
+*                            Open Watcom Project
+*
+* Copyright (c) 2026      The Open Watcom Contributors. All Rights Reserved.
+*    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
+*    Portions Copyright (c) 1989-2002 Novell, Inc.  All Rights Reserved.
+*
+*  ========================================================================
+*
+*    This file contains Original Code and/or Modifications of Original
+*    Code as defined in and that are subject to the Sybase Open Watcom
+*    Public License version 1.0 (the 'License'). You may not use this file
+*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
+*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
+*    provided with the Original Code and Modifications, and is also
+*    available at www.sybase.com/developer/opensource.
+*
+*    The Original Code and all software distributed under the License are
+*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
+*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
+*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
+*    NON-INFRINGEMENT. Please see the License for the specific language
+*    governing rights and limitations under the License.
+*
+*  ========================================================================
+*
+*   Any problems or updates required, please either contact the author or
+*   the OpenWatcom contributors forums.
+*       http://www.openwatcom.com/
+*
+* Description:
+*
+****************************************************************************/
+
 #define DONT_CHECK_CTRL_CHARS 0x10 /* turns off ^C and ^S processing        */
 #define AUTO_DESTROY_SCREEN   0x20 /* prevents "Press any key to close" msg */
 
